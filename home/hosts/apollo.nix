cd6bd2811427f43f7a4cb1cdb473ede97f1a9628
Malte Brandy <malte.brandy@maralorn.de>
2018-02-08 23:55:06 +0100

    Ergänze apollo config

--- home/hosts/apollo.nix
+++ home/hosts/apollo.nix
@@ -6,4 +6,38 @@ in {
     ../snippets/everywhere.nix
     ../snippets/my-systems.nix
   ];
+  home.packages = with pkgs; [
+    # config
+    gnome3.gnome_session
+    gnome3.gnome-dictionary
+
+    # web
+    unstable.firefox
+    chromium
+
+    # tools & office
+    gimp
+    imagemagick
+    libreoffice-fresh
+    pandoc
+    xournal
+    musescore
+    handbrake
+    octave
+
+    # look & feel
+    arc-theme
+    arc-icon-theme
+    libertine
+    nerdfonts
+
+    # media
+    ncmpcpp
+    pavucontrol
+    deluge
+    mpd
+    gmpc
+    calibre
+    gnome-mpv
+  ];
 }
