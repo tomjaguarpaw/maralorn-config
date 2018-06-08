835abf7a42d5ad37be9d58f080fadbd6f76cf4f3
Malte Brandy <malte.brandy@maralorn.de>
2018-06-08 22:15:25 +0200

    Push to rust 1.26.2

--- home-manager/modules/rustdev.nix
+++ home-manager/modules/rustdev.nix
@@ -1,15 +1,17 @@
 { lib, pkgs, config, ...}:
 with lib;
-{
+let
+  rustPkgs = import <rust126> {};
+in {
 
 options.m-0.rustdev.enable = mkEnableOption "Rust Dev";
 
 config = mkIf config.m-0.rustdev.enable {
   home.packages = with pkgs; [
 #    gnome3.glade
-    rustracer
-    cargo
-    rustc
+    rustPkgs.rustracer
+    rustPkgs.cargo
+    rustPkgs.rustc
     gcc
     binutils-unwrapped
     rustfmt
