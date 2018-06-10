10e4fb328c12502b1f724b5a8d9462841fcad68d
Malte Brandy <malte.brandy@maralorn.de>
2018-06-10 14:13:59 +0200

    Show more state in headstatus

--- home-manager/modules/graphical/i3.nix
+++ home-manager/modules/graphical/i3.nix
@@ -8,7 +8,7 @@ let
     do
       echo \
       $(date "+%Y-%m-%d %a %H:%M") "|" \
-      $(cat ~/.kassandra_state | tail -n3 | sed "s/$/ | /") \
+      $(cat ~/.kassandra_state | tail -n4 | sed "s/$/ | /") \
       =inbox= $(task +PENDING -BLOCKED -TAGGED count) "|" \
       =active task= $(task rc.verbose=nothing active || echo "none") "|" \
       =tags= $(task +PENDING -BLOCKED -project -optional -later rc.verbose=nothing tags | sed "s/\(.\)$/\1 |/" )
