5121b7de6d2476d55e07afb05f633413c38ae003
Malte Brandy <malte.brandy@maralorn.de>
2018-06-30 04:30:09 +0200

    Try bugwarrior

--- home-manager/modules/update_tasks.nix
+++ home-manager/modules/update_tasks.nix
@@ -1,10 +1,24 @@
 { config, lib, pkgs , ... }:
 with lib;
 let
-
+  inherit (config.m-0.private) me gitlab;
 in {
 options.m-0.update_tasks.enable = mkEnableOption "Update Tasks";
 config = mkIf config.m-0.update_tasks.enable {
+  home.file.".config/bugwarrior/bugwarriorrc".text = ''
+    [general]
+    targets=cda_gitlab
+
+    [cda_gitlab]
+    service=gitlab
+    gitlab.password=@oracle:eval:pass de/darmstadt/ccc/ldap
+    gitlab.login=${me.user}
+    gitlab.host=${gitlab.host}
+    gitlab.token=${gitlab.token}
+    gitlab.only_if_assigned=${me.user}
+    gitlab.only_if_author=${me.user}
+  '';
+  home.packages = [ pkgs.python2Packages.bugwarrior ];
   systemd.user = {
     services.update_tasks = {
       Unit = {
