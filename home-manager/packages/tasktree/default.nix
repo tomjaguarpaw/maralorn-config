0c94ccae4d151f4fbd5fdb5f101bd9c47c14e423
Malte Brandy <malte.brandy@maralorn.de>
2018-06-30 04:16:03 +0200

    Build tasktree with carnix

--- home-manager/packages/tasktree/default.nix
+++ home-manager/packages/tasktree/default.nix
@@ -1,39 +1,48 @@
-{ rustPlatform, gnome3, atk, cairo, gdk_pixbuf, glib, pango, makeDesktopItem, fetchFromGitHub }:
-let
-   desktopItem = makeDesktopItem {
-    name = "Tasktree";
-    exec = "tasktree";
-    icon = "tasktree";
-    comment = "A taskwarrior UI";
-    desktopName = "Tasktree";
-    genericName = "Tasktree";
-    categories = "Office;";
-  };
-in
-with rustPlatform; buildRustPackage rec {
-  name = "tasktree";
-  version = "abb312f";
-  src = fetchFromGitHub {
-    rev = version;
-    owner = "maralorn";
-    repo = "tasktree";
-    sha256 = "139xjvi7b62k3075b4md9hdkb1xafhhiyz2yhbb96d73j1gkqs77";
-  };
-  depsSha256 = "14acvigygrrqyvxra2n01vpadc3mcf8981jrggpvwfbz58jrsa7h";
-  cargoSha256 = "14acvigygrrqyvxra2n01vpadc3mcf8981jrggpvwfbz58jrsa7h";
+{ pkgs, defaultCrateOverrides, makeDesktopItem, atk , pango, gnome3, cairo, gdk_pixbuf, glib, ... }:
+((pkgs.callPackage ./Cargo.nix {}).tasktree_0_1_0 {}).override {
+  crateOverrides = defaultCrateOverrides // {
+    atk-sys = attr: { buildInputs = [ atk ]; };
+    pango-sys = attr: { buildInputs = [ pango glib ]; };
+    gio = attr: { buildInputs = [ glib ]; };
+    gdk-sys = attr: { buildInputs = [ gdk_pixbuf glib cairo pango ]; };
+    gtk-sys = attr: { buildInputs = [ gdk_pixbuf glib cairo pango atk gnome3.gtk ]; };
+    gdk = attr: { buildInputs = [ cairo gnome3.gtk gdk_pixbuf pango ]; };
+    gtk = attr: { buildInputs = [ cairo atk gnome3.gtk gdk_pixbuf pango ]; };
+    tasktree = attrs:
+		let
+			desktopItem = makeDesktopItem {
+			 name = "Tasktree";
+			 exec = "tasktree";
+			 icon = "tasktree";
+			 comment = "A taskwarrior UI";
+			 desktopName = "Tasktree";
+			 genericName = "Tasktree";
+			 categories = "Office;";
+		  };
+		in {
 
-  propagatedBuildInputs = [ gnome3.gtk atk cairo gdk_pixbuf glib pango ];
-  postInstall = ''
-    function installIcon () {
-        mkdir -p $out/share/icons/hicolor/$1/apps/
-        cp icons/$1.png $out/share/icons/hicolor/$1/apps/tasktree.png
-    }
-    installIcon "16x16"
-    installIcon "32x32"
-    installIcon "64x64"
-
-    mkdir -p $out/share/applications
-    ln -s ${desktopItem}/share/applications/* $out/share/applications/
-  '';
-  doCheck = false;
+		  postInstall = ''
+			mkdir -p $out/share/applications
+			ln -s ${desktopItem}/share/applications/* $out/share/applications/
+			rm $out/lib/link
+		  '';
+		};
+  };
 }
+
+
+  #propagatedBuildInputs = [ gnome3.gtk atk cairo gdk_pixbuf glib pango ];
+  #postInstall = ''
+    #function installIcon () {
+        #mkdir -p $out/share/icons/hicolor/$1/apps/
+        #cp icons/$1.png $out/share/icons/hicolor/$1/apps/tasktree.png
+    #}
+    #installIcon "16x16"
+    #installIcon "32x32"
+    #installIcon "64x64"
+
+    #mkdir -p $out/share/applications
+    #ln -s ${desktopItem}/share/applications/* $out/share/applications/
+  #'';
+  #doCheck = false;
+#}
