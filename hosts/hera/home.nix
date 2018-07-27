ee1247f54120922a668bb70b25b149ad8b04df98
Malte Brandy <malte.brandy@maralorn.de>
2018-07-27 18:40:35 +0200

    Activate taskwarrior on hera

--- hosts/hera/home.nix
+++ hosts/hera/home.nix
@@ -6,12 +6,15 @@ imports = [
   ./secret
 ];
 
-m-0.hostName = "hera";
-m-0.weechat = {
-  enable = true;
-  user = config.m-0.private.hackint.user;
-  pw = config.m-0.private.hackint.hackint_pw;
-  channels = config.m-0.private.hackint.channels;
+m-0 = {
+  hostName = "hera";
+  taskwarrior.enable = true;
+  weechat = {
+    enable = true;
+    user = config.m-0.private.hackint.user;
+    pw = config.m-0.private.hackint.hackint_pw;
+    channels = config.m-0.private.hackint.channels;
+  };
 };
 
 }
