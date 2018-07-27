15632dc83e3686dd0bbb1a5b4428328cc331c91f
Malte Brandy <malte.brandy@maralorn.de>
2018-07-27 14:57:20 +0200

    Update vpn ips

--- common/default.nix
+++ common/default.nix
@@ -13,20 +13,26 @@ with lib;
     };
     m-0.hosts = mkOption {
       type = types.attrs;
-      default = let p = config.m-0.prefix; in {
+      default = let
+        p = config.m-0.prefix;
+        hera-p = "${p}::3";
+        apollo-p = "${p}::1";
+        wg-p = "${p}::100";
+      in {
+        hera-wg = "${wg-p}::1";
+        apollo-wg = "${wg-p}::2";
+
         hera-v4 = "213.136.94.190";
         hera = "${p}::1";
-        hera-wg = "${p}::1:0:1";
-        hera-intern = "${p}::3:1";
-        git = "${p}::3:2";
-        borg = "${p}::3:3";
-        dav = "${p}::3:5";
-        blog = "${p}::3:6";
-        chor = "${p}::3:7";
-        matrix = "${p}::3:8";
-        apollo-wg = "${p}::1:1:1";
-        apollo = "${p}::1:1";
-        athene = "${p}::2:1";
+        hera-intern = "${hera-p}:1";
+        git = "${hera-p}:2";
+        borg = "${hera-p}:3";
+        dav = "${hera-p}:5";
+        blog = "${hera-p}:6";
+        chor = "${hera-p}:7";
+        matrix = "${hera-p}:8";
+
+        apollo = apollo-wg;
       };
     };
   };
