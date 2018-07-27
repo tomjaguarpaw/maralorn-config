a9d4cd5813adce1b696e595457344402a2ee14ce
Malte Brandy <malte.brandy@maralorn.de>
2018-07-27 19:21:37 +0200

    Add https-git

--- hosts/hera/git.nix
+++ hosts/hera/git.nix
@@ -23,12 +23,37 @@ in {
         };
         inherit (config.networking) nameservers;
         defaultGateway6 = { address = config.m-0.hosts.hera-intern; interface = "eth0"; };
+		  firewall.allowedTCPPorts = [ 80 443 ];
       };
 
       environment.systemPackages = [ pkgs.git ];
 
+
       services = {
         sshd.enable = true;
+        fcgiwrap.enable = true;
+        nginx = {
+          enable = true;
+          virtualHosts."git.m-0.eu" = {
+            forceSSL = true;
+            enableACME = true;
+            default = true;
+            locations = {
+              "~ (/.*)" = {
+                extraConfig = ''
+						# fcgiwrap is set up to listen on this host:port
+						fastcgi_pass  unix:/run/fcgiwrap.sock;
+						include       ${pkgs.nginx}/conf/fastcgi_params;
+						fastcgi_param SCRIPT_FILENAME     ${pkgs.git}/bin/git-http-backend;
+						# export all repositories under GIT_PROJECT_ROOT
+						fastcgi_param GIT_HTTP_EXPORT_ALL "";
+						fastcgi_param GIT_PROJECT_ROOT    /home/git;
+						fastcgi_param PATH_INFO           $1;
+                '';
+              };
+            };
+          };
+        };
       };
     };
   };
