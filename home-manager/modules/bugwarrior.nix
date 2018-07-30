39b3b3eb0cc1411e33ec0a651d8acab1893127ab
Malte Brandy <malte.brandy@maralorn.de>
2018-07-30 21:41:01 +0200

    Readd missing grep in bugwarrior path

--- home-manager/modules/bugwarrior.nix
+++ home-manager/modules/bugwarrior.nix
@@ -43,7 +43,7 @@ config = mkIf config.m-0.bugwarrior.enable {
       };
       Service = {
         Type = "oneshot";
-        Environment=''PATH=${pkgs.taskwarrior}/bin:${pkgs.eventd}/bin OTRS_USER=${me.user} OTRS_PASSWORD=${otrs.password} OTRS_QUEUES="${otrs.queues}" OTRS_OWNERS="${otrs.owners}" OTRS_HOST=${otrs.host}'';
+        Environment=''PATH=${pkgs.taskwarrior}/bin:${pkgs.eventd}/bin:${pkgs.gnugrep}/bin OTRS_USER=${me.user} OTRS_PASSWORD=${otrs.password} OTRS_QUEUES="${otrs.queues}" OTRS_OWNERS="${otrs.owners}" OTRS_HOST=${otrs.host}'';
         ExecStart= let
           update = pkgs.writeShellScriptBin "update" ''
             ${pkgs.bugwarrior}/bin/bugwarrior-pull
