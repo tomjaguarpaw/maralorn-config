b076f8dc503991292106b628e8631c9c3c6c6cb1
Malte Brandy <malte.brandy@maralorn.de>
2018-10-10 11:43:39 +0200

    Fix v4 problems with hera-unlock

--- hosts/apollo/home.nix
+++ hosts/apollo/home.nix
@@ -26,7 +26,7 @@ m-0 = {
   pythia.enable = true;
   unlocker = [ {
     name = "hera";
-    hostName = "hera.m-0.eu";
+    hostName = "hera-v4.m-0.eu";
     pubKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCHkqWlFLtmIlTSKahr2PcL++K75YgfsSU6jwVYW5df3JCkowu/M16SIBxABxYSQrKej5uIz/OFCjqSxHJQ8D5wSYBvn2gYr/BbBcz4rfIJmZ55Od2jckaqlj/M8TtkuPPhsQG7S730vXxK5hbMT8iW5WWv8sIKY/WtaRbZOFMX/53WCLEHtnMu5zFJFWf92+mjIHSLyW8ggl1m525RUiaAfCge2vnuzIFq4kUqJxaWzxIvEWIncKWN10K/HMvdI+yOtbSen41uKedwSFhUFs3xHy1mJddYOrlcJQPt5zuuffZ/nTDVXMZoh5QNwg8ZlkkueVChaS1Y5STjb7cem1Mt";
     passPath = "eu/m-0/hera/disk";
   } ];
