1037c1e7cef870fd7e494805eabab5e0611d89c3
Malte Brandy <malte.brandy@maralorn.de>
2019-02-06 00:45:38 +0100

    Switch to urxvtc

--- home-manager/modules/graphical/default.nix
+++ home-manager/modules/graphical/default.nix
@@ -9,8 +9,17 @@ imports = [
   ./rofi
   ./ssh-agent.nix
 ];
-
-config = mkIf config.m-0.graphical.enable {
+config = let
+  urxvt = pkgs.rxvt_unicode-with-plugins;
+  terminal = pkgs.writeShellScriptBin "terminal" ''
+      ${urxvt}/bin/urxvtc "$@"
+      if [ $? -eq 2 ]; then
+         ${urxvt}/bin/urxvtd -q -o -f
+         ${urxvt}/bin/urxvtc "$@"
+      fi
+    '';
+in
+mkIf config.m-0.graphical.enable {
   m-0 = {
     workspaces = [
       "tasks"
@@ -24,7 +33,7 @@ config = mkIf config.m-0.graphical.enable {
       "leisure"
       "config"
     ];
-    terminal = "${pkgs.my-st}/bin/st";
+    terminal = "${terminal}/bin/terminal";
     colors = {
       "foreground" = "#dddbff";
       "background" = "#05004a";
@@ -48,6 +57,7 @@ config = mkIf config.m-0.graphical.enable {
   };
   home = {
     packages = with pkgs; [
+      terminal
       tasktree
       my-st
       shutter
@@ -58,6 +68,47 @@ config = mkIf config.m-0.graphical.enable {
       variant = "neo";
       options = [ "altwin:swap_lalt_lwin" ];
     };
+  };
+  programs.urxvt = {
+    enable = true;
+    package = pkgs.rxvt_unicode-with-plugins;
+    fonts = [ "6x13" ];
+    keybindings = {
+      "C-1" = "command:\\033]710;6x13\\007";
+      "C-2" = "command:\\033]710;10x20\\007";
+      "C-3" = "command:\\033]710;xft:Roboto Mono Nerd Font:size=16\\007";
+      "C-4" = "command:\\033]710;xft:Roboto Mono Nerd Font:size=24\\007";
+      "C-f" = "matcher:select";
+      "C-g" = "matcher:last";
+    };
+    extraConfig = {
+      tintColor = config.m-0.colors.background;
+      perl-ext = "default,matcher,clipboard-osc";
+      url-launcher = "firefox";
+      foreground = config.m-0.colors.foreground;
+      background = config.m-0.colors.background;
+      color0 = config.m-0.colors.black;
+      color1 = config.m-0.colors.red;
+      color2 = config.m-0.colors.green;
+      color3 = config.m-0.colors.yellow;
+      color4 = config.m-0.colors.blue;
+      color5 = config.m-0.colors.magenta;
+      color6 = config.m-0.colors.cyan;
+      color7 = config.m-0.colors.white;
+      color8 = config.m-0.colors.brightBlack;
+      color9= config.m-0.colors.brightRed;
+      color10 = config.m-0.colors.brightGreen;
+      color11 = config.m-0.colors.brightYellow;
+      color12 = config.m-0.colors.brightBlue;
+      color13 = config.m-0.colors.brightMagenta;
+      color14 = config.m-0.colors.brightCyan;
+      color15 = config.m-0.colors.brightWhite;
+    };
+    scroll = {
+      bar.enable = false;
+      lines = 0;
+    };
+    transparent = true;
   };
   gtk = {
     enable = true;
