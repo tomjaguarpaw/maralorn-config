f45fa2dbcde562a6d10f027124c352939ea67e2e
Malte Brandy <malte.brandy@maralorn.de>
2019-04-04 13:12:20 +0200

    Ergänze ncmpcpp config

--- home-manager/modules/laptop.nix
+++ home-manager/modules/laptop.nix
@@ -11,7 +11,16 @@ config = let
     nmcli r wifi on;
   '';
 in
-  mkIf config.m-0.laptop.enable {
+mkIf config.m-0.laptop.enable {
+  home.file.".ncmpcpp/config".text = ''
+    ask_before_clearing_playlists=no
+    mouse_support = yes
+    song_columns_list_format = "(24)[red]{a} $R(48)[blue]{t} (24)[green]{b} (4)[magenta]{l}"
+    playlist_display_mode = columns
+    search_engine_display_mode = columns
+    browser_display_mode = columns
+    user_interface = alternative
+  '';
   programs = {
     firefox = {
       enable = true;
