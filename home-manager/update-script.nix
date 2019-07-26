5ae3cfa7a2252e44c480ac0ee2a59d4dabb5bd07
Malte Brandy <malte.brandy@maralorn.de>
2019-07-26 15:54:36 +0200

    Fixes

---
+++ home-manager/update-script.nix
@@ -0,0 +1,22 @@
+{ pkgs, config, lib,  ... }:
+let
+  inherit (import ../common/lib.nix) writeHaskellScript get-niv-path gcRetentionDays;
+  inherit (import ./lib.nix) update-home;
+  configPath = "/home/${config.home.username}/git/nixos/config";
+  home-maintenance = writeHaskellScript
+    { name = "user-maintenance"; imports = [ ]; bins = [ (update-home configPath) pkgs.nix pkgs.git];} ''
+    main = do
+      git "-C" "${configPath}" "pull"
+      update_home
+      nix_collect_garbage "--delete-older-than" "${toString gcRetentionDays}d"
+      nix "optimise-store"
+  '';
+in
+{
+  home = {
+    packages = builtins.attrValues {
+      inherit home-maintenance get-niv-path;
+      update-home = update-home configPath;
+    };
+  };
+}
