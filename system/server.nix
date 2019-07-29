f2de1bfe08b19f707fcfbd71345dc85bf4dd91c9
Malte Brandy <malte.brandy@maralorn.de>
2019-07-29 03:50:11 +0200

    Fix system-maintenance unit

--- system/server.nix
+++ system/server.nix
@@ -6,6 +6,7 @@ imports = [ ./init_ssh.nix ];
 config = {
   systemd.services."system-maintenance" = {
     startAt = "2:45";
+    environment.NIX_PATH = "/etc/nix-path:nixos-config=/etc/nixos/configuration.nix";
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${(import ./update-lib.nix config.system.build.nixos-rebuild).system-maintenance}/bin/system-maintenance";
