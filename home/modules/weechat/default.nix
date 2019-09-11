cbfdb8ee8da0c29732bfae4278b78f24e2da82f0
Malte Brandy <malte.brandy@maralorn.de>
2019-09-11 18:00:56 +0200

    Add freenode channels

--- home/modules/weechat/default.nix
+++ home/modules/weechat/default.nix
@@ -63,7 +63,7 @@ in {
           freenode.sasl_password = "${config.m-0.weechat.freenode_pw}"
           freenode.autoconnect = on
           freenode.username = "${config.m-0.weechat.user}"
-          freenode.autojoin = "#nixos,#matrix,#haskell,#card10badge,#ghc,#home-manager,#krebs,#nixos-de"
+          freenode.autojoin = "#nixos,#matrix,#haskell,#reflex-frp,#card10badge,#ghc,#home-manager,#krebs,#nixos-de"
 
           [server]
           hackint.addresses = "irc.hackint.org/6697"
