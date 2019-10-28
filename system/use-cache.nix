d628525c3b34b3a8745af8cc720a2444bed56e2a
Malte Brandy <malte.brandy@maralorn.de>
2019-10-28 23:20:58 +0100

    Set priority of hera substituter lower

--- system/use-cache.nix
+++ system/use-cache.nix
@@ -1,6 +1,6 @@
-{ ... }: {
+{ lib, ... }: {
   nix = {
-    binaryCaches = [ "http://hera:5001" ];
+    binaryCaches = lib.mkAfter [ "http://hera:5001" ];
     binaryCachePublicKeys =
       [ "hera:dPBUx652Yr9s2pQDeHRQ5dl5zSuo+j94AyvJGZJaMFw=" ];
   };
