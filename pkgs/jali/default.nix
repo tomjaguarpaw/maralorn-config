4ba90c1f6e45e235034877a4a49b751802742588
Malte Brandy <malte.brandy@maralorn.de>
2019-10-29 03:24:02 +0100

    Update jali

--- pkgs/jali/default.nix
+++ pkgs/jali/default.nix
@@ -1,15 +1,19 @@
 { aqbanking, python36Packages, fetchgit }:
-with python36Packages;
+let
+  andir = import (builtins.fetchTarball {
+    url = "https://github.com/andir/nixpkgs/archive/aqbanking.tar.gz";
+    sha256 = "06m7risi68jfqgv001ip77lzz6bylvvdn5j8sn845c54zpm6qp36";
+  }) { };
+in with python36Packages;
 buildPythonApplication rec {
   name = "${pname}-${version}";
   pname = "jali";
   doCheck = false;
-  version = "2c1dfa1a";
+  version = "3a884c7eb3b294c5aaf2191408e1ac2befbceaaf";
   src = fetchgit {
     url = "https://git.darmstadt.ccc.de/jali/jali.git";
     rev = version;
-    sha256 = "1xac5rs848nyxshgrv52i1w1y8bgbawv7spr3489p8f22crvawra";
+    sha256 = "0fkb4vyf570d7zcwi3mix9g44wqpy128gd3li0grkbml7aflzxfr";
   };
-  #src = /home/maralorn/git/jali;
-  propagatedBuildInputs = [ jinja2 pendulum GitPython aqbanking ];
+  propagatedBuildInputs = [ jinja2 pendulum GitPython andir.aqbanking ];
 }
