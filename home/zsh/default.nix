73a6dd6d62d55371e81084d1dda49299e78c6465
Malte Brandy <malte.brandy@maralorn.de>
2020-03-01 17:27:38 +0100

    Clean dead code

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -24,11 +24,6 @@
       oh-my-zsh = {
         enable = true;
         plugins = [ "colored-man-pages" ];
-        #        theme = "powerlevel10k";
-        #        custom = "${pkgs.runCommand "oh-my-zsh-custom" { } ''
-        #          mkdir -p $out/themes
-        #          ln -s ${my-pkgs.zsh-powerlevel10k}/share/powerlevel10k/powerlevel10k.zsh-theme $out/themes/.
-        #        ''}";
       };
     };
   };
