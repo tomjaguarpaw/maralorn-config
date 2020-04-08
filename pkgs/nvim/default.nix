bdd13f7e77d2917460741d2394bf229c118473b4
Malte Brandy <malte.brandy@maralorn.de>
2020-04-08 18:55:46 +0200

    Change tabnine plugin

--- pkgs/nvim/default.nix
+++ pkgs/nvim/default.nix
@@ -2,22 +2,23 @@ n:
 n.override (let
   pkgs = import <nixpkgs> { };
   inherit (import ../../lib) sources colors;
-  tabnine = sources.tabnine-vim;
 in {
   configure = {
     customRC = ''
-      set rtp+=${tabnine}
       ${builtins.readFile ./vimrc}
       hi Normal ctermbg=NONE
     '';
     packages.myVimPackage = {
       start = builtins.attrValues {
         inherit (pkgs.vimPlugins)
-          vim-nix vimtex airline rust-vim fugitive vim-trailing-whitespace
-          vim-airline-themes vim-pandoc vim-pandoc-syntax haskell-vim
-          vim-autoformat vim-ledger papercolor-theme nerdcommenter vundle
-          coc-nvim coc-python coc-rls coc-yaml coc-vimtex coc-css coc-json
-          coc-html coc-git coc-tabnine;
+        # rust-vim vimtex
+        # coc-tabnine (TODO: Why doesn‘t it work?)
+        # TODO: tabnine config in home-manager
+        # TODO: tabnine lsp: nix, rust, pandoc/latex lsp? was noch?
+          vim-pandoc vim-nix vimtex airline fugitive vim-trailing-whitespace
+          haskell-vim vim-airline-themes vim-autoformat vim-ledger
+          papercolor-theme nerdcommenter coc-nvim coc-python coc-rls coc-yaml
+          coc-vimtex coc-css coc-json coc-html coc-git;
       };
     };
   };
