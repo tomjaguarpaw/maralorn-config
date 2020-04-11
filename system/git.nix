97480d592ad2ac0d7fe828d5659b0791e5c44cc0
Malte Brandy <malte.brandy@maralorn.de>
2020-04-11 17:51:18 +0200

    Another git buildfix

--- system/git.nix
+++ system/git.nix
@@ -30,7 +30,7 @@ let
         git "push" "--all" "-f" mirror
       deploy <- lookupEnv "GL_OPTION_WEB_DEPLOY"
       whenJust deploy $ \deploy -> do
-        (maybe [] ("-A":) -> target) <- lookupEnv "GL_OPTION_WEB_DEPLOY_NIX_TARGET"
+        (maybe [] (\x -> ["-A",x]) -> target) <- lookupEnv "GL_OPTION_WEB_DEPLOY_NIX_TARGET"
         (decodeUtf8 -> path) <- pwd |> captureTrim
         echo ([i|Deploying build to /var/www/#{deploy}|] :: String)
         bracket (checkout path) (rm "-rf") $ \dir -> withCurrentDirectory dir $ nix-build "-o" ([i|/var/www/#{deploy}|] :: String) target
