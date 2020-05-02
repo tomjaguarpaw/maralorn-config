bee79c03d3c0d3e7fe2e71bfb2980ef1cb39f24c
Malte Brandy <malte.brandy@maralorn.de>
2020-05-02 22:00:24 +0200

    Install grafana

--- system/monitoring/default.nix
+++ system/monitoring/default.nix
@@ -52,7 +52,34 @@ in {
         forceSSL = true;
         locations."/" = { proxyPass = "http://localhost:9093"; };
       };
+      virtualHosts."stats.maralorn.de" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/".proxyPass = "http://localhost:3000/";
+      };
     };
+    grafana = {
+      enable = true;
+      auth.anonymous.enable = true;
+      users = {
+        allowOrgCreate = false;
+        allowSignUp = false;
+      };
+      provision = {
+        enable = true;
+        datasources = [{
+          access = "proxy";
+          name = "prometheus";
+          type = "prometheus";
+          url = "http://localhost:9090";
+        }];
+        dashboards = [{
+          name = "Static dashboards";
+          options.path = ./grafana-dashboards;
+        }];
+      };
+    };
+
     prometheus = {
       exporters = {
         blackbox = {
