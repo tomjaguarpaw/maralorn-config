33ee4fb85baba8a128882a8df56fdc5f3a44ef45
Malte Brandy <malte.brandy@maralorn.de>
2020-05-06 13:00:52 +0200

    Make weechat mode dependent

--- home/chat.nix
+++ home/chat.nix
@@ -4,5 +4,8 @@ in {
   home.packages = builtins.attrValues {
     inherit (pkgs) signal-desktop tdesktop dino mumble;
     inherit (unstable) riot-desktop;
+    weechat = pkgs.writeShellScriptBin "weechat" ''
+      ssh -t hera "tmux -L weechat attach"
+    '';
   };
 }
