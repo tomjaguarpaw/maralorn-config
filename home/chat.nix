e323bb601961a727b48599a42a1abfbd89dd1c1d
Malte Brandy <malte.brandy@maralorn.de>
2020-05-14 02:11:01 +0200

    Move discord

--- home/chat.nix
+++ home/chat.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let inherit (import ../lib) unfreePkgs;
+in {
   home.packages = builtins.attrValues {
+    inherit (unfreePkgs) discord;
     inherit (pkgs) signal-desktop tdesktop dino riot-desktop;
     weechat = pkgs.writeShellScriptBin "weechat" ''
       ssh -t hera "tmux -L weechat attach"
