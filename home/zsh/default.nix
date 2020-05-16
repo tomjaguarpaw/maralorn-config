ce7690e9d11ebd003ce9090ea49cd8aa2aa6e195
Malte Brandy <malte.brandy@maralorn.de>
2020-05-16 15:59:27 +0200

    Simplify zsh config

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,35 +1,20 @@
 { pkgs, ... }: {
-
-  programs = let my-pkgs = import ../../pkgs;
-  in {
-    zsh = {
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    history = {
+      save = 100000;
+      size = 100000;
+    };
+    initExtra = ''
+      ${builtins.readFile ./zshrc}
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      source ${./p10k.zsh}
+    '';
+    oh-my-zsh = {
       enable = true;
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      history = {
-        save = 100000;
-        size = 100000;
-      };
-      initExtra = ''
-        # If running from tty1 start sway
-        if [ "$(tty)" = "/dev/tty1" ]; then
-           . ${my-pkgs.start-ssh-agent}/bin/start-ssh-agent
-           while true; do
-             select-mode
-             echo Launching sway at $(date) >> ~/tmp/sway.log
-             ${pkgs.sway}/bin/sway >> ~/tmp/sway.log
-           done
-        fi
-
-        ${builtins.readFile ./zshrc}
-        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        source ${./p10k.zsh}
-      '';
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "colored-man-pages" ];
-      };
+      plugins = [ "colored-man-pages" ];
     };
   };
-
 }
