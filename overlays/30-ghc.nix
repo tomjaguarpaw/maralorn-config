8608cbb943f38a5dbdeb502b3520a71e365815ce
Malte Brandy <malte.brandy@maralorn.de>
2020-06-05 23:48:33 +0200

    Install a few more haskell packages

--- overlays/30-ghc.nix
+++ overlays/30-ghc.nix
@@ -1,12 +1,15 @@
 self: super:
-let master = import super.sources.nixpkgs-master { };
+let
+  master = import super.sources.nixpkgs-master { };
+  pkgs = master;
+  inherit (master.haskell.lib) overrideCabal unmarkBroken;
 in {
   myHaskellPackages = {
     inherit (master.haskellPackages)
       brittany ormolu releaser cabal-fmt stack ghcide cabal-install dhall aeson
       unordered-containers shh string-interpolate relude replace-megaparsec
       async say cmdargs megaparsec fdo-notify these neuron taskwarrior pandoc
-      hlint;
+      hlint Cabal cabal2nix;
   };
   ghc = master.ghc.withPackages (_: builtins.attrValues self.myHaskellPackages);
 }
