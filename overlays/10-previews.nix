737d4ffdc17d33391c2027fbc0eec9902c04454c
Malte Brandy <malte.brandy@maralorn.de>
2020-06-06 16:44:29 +0200

    Adapt previews

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -1,12 +1,10 @@
 self: super:
 let
   unstable = import self.sources.unstable { };
-  master = import super.sources.nixpkgs-master { };
 in {
   gnome3 = super.gnome3 // {
     inherit (unstable.gnome3) gnome-keyring seahorse gdm;
   };
   inherit (unstable)
-    neovim vimPlugins syncthing nerdfonts; # riot-desktop;
-  inherit (master) go-neb;
+    neovim vimPlugins syncthing nerdfonts ormolu go-neb; # riot-desktop;
 }
