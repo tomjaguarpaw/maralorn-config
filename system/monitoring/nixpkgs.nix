8cbc880b0a7ee43b0555c3cc070a77d210efbb92
Malte Brandy <malte.brandy@maralorn.de>
2020-06-14 03:11:50 +0200

    Fix prometheus expression

--- system/monitoring/nixpkgs.nix
+++ system/monitoring/nixpkgs.nix
@@ -1,21 +1,23 @@
 { pkgs, ... }:
 let
-  watchNixpkgsPackage = name: branch: path: let name = "nixpkgs: ${name} on ${branch}";in {
-    job_name = name;
-    metrics_path = "/job/${path}/prometheus";
-    scheme = "https";
-    scrape_interval = "1h";
-    scrape_timeout = "60s";
-    static_configs = [{
-      labels = {
-        inherit name;
-        packageName = name;
-        url = "https://hydra.nixos.org/job/${path}";
-        alert_type = "nixpkgs";
-      };
-      targets = [ "hydra.nixos.org" ];
-    }];
-  };
+  watchNixpkgsPackage = name: branch: path:
+    let job_name = "nixpkgs: ${name} on ${branch}";
+    in {
+      inherit job_name;
+      metrics_path = "/job/${path}/prometheus";
+      scheme = "https";
+      scrape_interval = "1h";
+      scrape_timeout = "60s";
+      static_configs = [{
+        labels = {
+          name = job_name;
+          packageName = name;
+          url = "https://hydra.nixos.org/job/${path}";
+          alert_type = "nixpkgs";
+        };
+        targets = [ "hydra.nixos.org" ];
+      }];
+    };
   watchHaskellUnstable = name:
     watchNixpkgsPackage name "haskell-updates"
     "nixpkgs/haskell-updates/haskellPackages.${name}.x86_64-linux";
