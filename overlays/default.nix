394d8fcec67bafeab3c7ed1437ffdc1f476fffaf
Malte Brandy <malte.brandy@maralorn.de>
2020-06-27 17:02:29 +0200

    Fix overlays

---
+++ overlays/default.nix
@@ -0,0 +1,12 @@
+{ lib }:
+let
+  overlayPath = ./.;
+  candidates = lib.attrNames (builtins.readDir overlayPath);
+  pathToOverlay = n: overlayPath + ("/" + n);
+  isNixFile = n: builtins.match ".*\\.nix" n != null;
+  isNixDir = n: builtins.pathExists (pathToOverlay n + "/default.nix");
+  notDefault = n: n != "default.nix";
+  isOverlay = n: (isNixDir n || isNixFile n) && notDefault n;
+  overlays = builtins.filter isOverlay candidates;
+  importOverlay = n: import (pathToOverlay n);
+in map importOverlay overlays
