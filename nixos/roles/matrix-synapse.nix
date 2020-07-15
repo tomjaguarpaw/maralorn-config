b4937f539dfb712ca737f6863116682632024ef0
Malte Brandy <malte.brandy@maralorn.de>
2020-07-15 20:51:56 +0200

    Another shot

--- nixos/roles/matrix-synapse.nix
+++ nixos/roles/matrix-synapse.nix
@@ -16,7 +16,7 @@ in {
           '';
           "/.well-known/matrix/client".extraConfig = ''
             default_type application/json;
-            return 200 "{\"m.homeserver\": \"https://matrix.maralorn.de\"}";
+            return 200 "{\"m.homeserver\": { \"base_url\":\"https://matrix.maralorn.de\"} }";
           '';
         };
         extraConfig = "
