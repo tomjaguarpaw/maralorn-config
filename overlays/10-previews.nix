3c08e2759e397a079e57d55d255652f79649fa7e
Malte Brandy <malte.brandy@maralorn.de>
2020-07-17 13:17:17 +0200

    Fix element

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -1,11 +1,13 @@
 self: super:
 let
   unstable = import self.sources.unstable { };
+  nixpkgs-master = import self.sources.nixpkgs-master { };
 in {
   systemd-next = unstable.systemd;
   gnome3 = super.gnome3 // {
     inherit (unstable.gnome3) gnome-keyring seahorse gdm;
   };
   inherit (unstable)
-    aqbanking neovim vimPlugins syncthing nerdfonts ormolu ghcid go-neb element-web element-desktop;
+    aqbanking neovim vimPlugins syncthing nerdfonts ormolu ghcid go-neb;
+  inherit (nixpkgs-master) element-web element-desktop;
 }
