3645826294895834cb5141567b5c395fc32247cb
Malte Brandy <malte.brandy@maralorn.de>
2020-08-15 00:18:57 +0200

    Bumping versions

--- overlays/30-ghc.nix
+++ overlays/30-ghc.nix
@@ -2,6 +2,12 @@ self: super:
 let
   master = import super.sources.nixpkgs-master { };
   inherit (master.haskell.lib) overrideCabal unmarkBroken;
+  myOverrides = self: super: {
+    optics = super.optics_0_3;
+    optics-th = super.optics-th_0_3_0_1;
+    optics-core = super.optics-core_0_3_0_1;
+    optics-extra = super.optics-extra_0_3;
+  };
   makeHaskellScriptPackages = p: {
     inherit (p)
       aeson shh string-interpolate relude replace-megaparsec async say
