5f0dc9fa678b12593915cc17a5314695e3c9b534
Malte Brandy <malte.brandy@maralorn.de>
2020-09-16 23:59:47 +0200

    Fix email2matrix

--- overlays/email2matrix/package.nix
+++ overlays/email2matrix/package.nix
@@ -1,12 +1,12 @@
 { buildGoModule, fetchFromGitHub, ... }:
 buildGoModule rec {
   name = "email2matrix";
-  version = "782bcfdd67983ff27f0b9cec5c81cf1a20796ab0";
+  version = "0c3ec3c7f0066dd7b7b8a30a846d024c759f8176";
   src = fetchFromGitHub {
     owner = "devture";
     repo = "email2matrix";
     rev = version;
-    sha256 = "0nx99iab2y10m4jh4jl9c4y7j4iy8zlyfcn42v4y4mlk1507czlj";
+    sha256 = "0mydj41kbjh4d4j6yql2qq8a18rqjgcdm3wlcr4gm3x70lg3lyll";
   };
-  vendorSha256 = "0nrl1d1628isd6183a9rj4qmsmzpbsf656cm75vw0lz2x0s4x7dg";
+  vendorSha256 = "00h2234f913ksbm175xsd72359v9g7f31y08nr5a555wn8sk4cgd";
 }
