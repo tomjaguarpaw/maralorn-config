334a87dfb8ff6e1a8dc1216151b8f880ff8ad6bc
Malte Brandy <malte.brandy@maralorn.de>
2020-09-16 19:34:51 +0200

    Disable previews

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -3,11 +3,4 @@ let
   unstable = import self.sources.unstable { };
   nixpkgs-master = import self.sources.nixpkgs-master { };
 in {
-  systemd-next = unstable.systemd;
-  gnome3 = super.gnome3 // {
-    inherit (unstable.gnome3) gnome-keyring seahorse gdm;
-  };
-  inherit (unstable)
-    aqbanking neovim vimPlugins syncthing nerdfonts ormolu ghcid go-neb fzf nixpkgs-fmt;
-  inherit (nixpkgs-master) element-web element-desktop;
 }
