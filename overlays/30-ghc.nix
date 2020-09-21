19a0edd610dd00c6393d344fbc6cbaa552f0f3bb
Malte Brandy <malte.brandy@maralorn.de>
2020-09-21 16:43:05 +0200

    Remove obsolete pandoc version spec

--- overlays/30-ghc.nix
+++ overlays/30-ghc.nix
@@ -17,13 +17,16 @@ let
   makeHaskellPackages = p:
     {
       inherit (p)
-        brittany ormolu releaser cabal-fmt stack ghcid ghcide haskell-language-server cabal-install dhall
-        taskwarrior pandoc_2_10_1 hlint
-        cabal2nix weeder reflex-dom password optics shh-extras neuron hspec-discover cabal-edit paths;
+        brittany ormolu releaser cabal-fmt stack ghcid ghcide
+        haskell-language-server cabal-install dhall taskwarrior pandoc hlint
+        cabal2nix weeder reflex-dom password optics shh-extras neuron
+        hspec-discover cabal-edit paths;
     } // makeHaskellScriptPackages p;
 in {
   myHaskellPackages = makeHaskellPackages master.haskellPackages;
   scriptGhc = master.ghc.withPackages
     (p: builtins.attrValues (makeHaskellScriptPackages p));
-  ghc = (master.haskellPackages.override { overrides = myOverrides; }).ghc.withHoogle (p: builtins.attrValues (makeHaskellPackages p));
+  ghc = (master.haskellPackages.override {
+    overrides = myOverrides;
+  }).ghc.withHoogle (p: builtins.attrValues (makeHaskellPackages p));
 }
