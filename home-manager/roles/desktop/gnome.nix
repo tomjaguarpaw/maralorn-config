40ee2192e302fc281e586d58e66c948f1660e824
Malte Brandy <malte.brandy@maralorn.de>
2020-09-28 13:35:45 +0200

    Fix neo-layout in gnome

--- home-manager/roles/desktop/gnome.nix
+++ home-manager/roles/desktop/gnome.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   services.gpg-agent.pinentryFlavor = "gnome3";
   dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      sources =  [(lib.hm.gvariant.mkTuple ["xkb" "de+neo"])]; # use neo
+      xkb-options = [
+        "altwin:swap_lalt_lwin" # swap alt and win
+        "lv3:menu_switch" # So that gnome-settings does not set it to ralt
+      ];
+    };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" =
       {
         binding = "<Super>Return";
