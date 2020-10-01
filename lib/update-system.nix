ec43143933fcda0d4138120bc1e2ddb774b071e5
Malte Brandy <malte.brandy@maralorn.de>
2020-10-01 08:05:00 +0200

    Fix update-script

--- lib/update-system.nix
+++ lib/update-system.nix
@@ -11,8 +11,9 @@ in {
     main = do
         paths <- myNixPath "${configPath}"
         args <- getArgs
-        bracket (rm canaryPath) (\() -> exe "/run/wrappers/bin/sudo" "-u" "maralorn" "git" "-C" privatePath "restore" canaryPath) $ \() ->
-           nixos_rebuild (paths ++ ["build", "--no-out-link"] ++ remoteBuildParams ++ fmap toString args)
+        bracket (rm canaryPath) (\() -> exe "/run/wrappers/bin/sudo" "-u" "maralorn" "git" "-C" privatePath "restore" canaryPath) $ \() -> do
+           nixos_rebuild (paths ++ ["build"] ++ remoteBuildParams ++ fmap toString args)
+           rm "result"
         nixos_rebuild (paths ++ ["switch"] ++ fmap toString args)
   '';
 }
