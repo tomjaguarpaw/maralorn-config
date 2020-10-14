3aed39b739515b33fa8078132d3ee76a89642bba
Malte Brandy <malte.brandy@maralorn.de>
2020-10-14 03:01:22 +0200

    Add simple desktop files

---
+++ home-manager/roles/desktop/desktop-items.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+let
+  simpleDesktopItem = name: command:
+    pkgs.makeDesktopItem {
+      name = name;
+      desktopName = name;
+      exec = command;
+    };
+  namedTerminalDesktopItem = name: command:
+    pkgs.makeDesktopItem {
+      name = name;
+      desktopName = name;
+      exec = command;
+      terminal = "true";
+    };
+  superSimpleDesktopItem = name: simpleDesktopItem name name;
+  terminalDesktopItem = name: namedTerminalDesktopItem name name;
+
+in {
+  home.packages = map superSimpleDesktopItem [ "kassandra2" ]
+    ++ map terminalDesktopItem [ "maintenance" "ncmpcpp" "kassandra" "hotkeys" "vim" ];
+}
