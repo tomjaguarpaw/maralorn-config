27cfb6cdb17caf843057c60d9e070330b690bdda
Malte Brandy <malte.brandy@maralorn.de>
2020-10-27 01:24:14 +0100

    Install more latex

--- home-manager/roles/research.nix
+++ home-manager/roles/research.nix
@@ -2,11 +2,6 @@
   home.packages = [ pkgs.zotero ];
   programs.texlive = {
     enable = true;
-    extraPackages = tpkgs: {
-      inherit (tpkgs)
-        scheme-medium pdfjam latexmk collection-latexextra
-        collection-bibtexextra collection-luatex collection-mathscience
-        collection-fontsextra dvipng;
-    };
+    extraPackages = p: { inherit (p) scheme-full; };
   };
 }
