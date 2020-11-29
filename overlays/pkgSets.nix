21f593d30b6faaba10682581bde76409e9f4fc28
Malte Brandy <malte.brandy@maralorn.de>
2020-11-29 18:17:09 +0100

    Install audio-software

--- overlays/pkgSets.nix
+++ overlays/pkgSets.nix
@@ -33,7 +33,7 @@ self: super: {
     '';
   };
   desktop-pkgs = {
-    inherit (self) lm_sensors xwayland xdg_utils libnotify kassandra kassandra2 shotcut;
+    inherit (self) lm_sensors xwayland xdg_utils libnotify kassandra kassandra2 shotcut mlt audacity;
     inherit (self.gnomeExtensions) appindicator system-monitor;
     inherit (self.gnome3)
       dconf dconf-editor gnome-tweaks gnome-shell-extensions adwaita-icon-theme
