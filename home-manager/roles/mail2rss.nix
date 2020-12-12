08c6d200270dadbce2183005a911f1962e5f1ee5
Malte Brandy <malte.brandy@maralorn.de>
2020-12-13 00:03:06 +0100

    Release the mail2rss script

--- home-manager/roles/mail2rss.nix
+++ home-manager/roles/mail2rss.nix
@@ -1,14 +1,15 @@
 { lib, pkgs, config, ... }: {
   systemd.user = {
     timers.logfeed = {
-      Timer.OnCalendar = "19:55";
+      Timer.OnCalendar = "19:57";
       Install.WantedBy = [ "timers.target" ];
     };
     services = {
       mail2rss = {
         Unit.Description = "Mail to rss exporter";
         Service = {
-          ExecStart = pkgs.writeScript "mail2rss" "${pkgs.logfeed}/bin/mail2rss ${config.accounts.email.maildirBasePath} hera/Move/readlater > /var/www/rss/mails2.xml ";
+          ExecStart = toString (pkgs.writeShellScript "mail2rss"
+            "${pkgs.logfeed}/bin/mail2rss ${config.accounts.email.maildirBasePath} hera/Move/readlater > /var/www/rss/mails2.xml");
           Type = "oneshot";
         };
       };
