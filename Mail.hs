5de2118aeb0eb4be09a2a825d052a705378d9cc8
Malte Brandy <malte.brandy@maralorn.de>
2020-12-13 19:43:09 +0100

    Improve html body handling

--- Mail.hs
+++ Mail.hs
@@ -34,6 +34,7 @@ import qualified Data.Map                      as Map
 import qualified Options.Applicative           as O
 import           Text.Atom.Feed.Export          ( textFeed )
 import           Text.Atom.Feed
+import           Text.HTML.TagSoup
 
 data Options = Options
   { dbPath :: String
@@ -53,9 +54,9 @@ type Error = Text
 data Body = HTMLBody Text | TextBody Text
 
 data Message = Message
-  { date   :: UTCTime
-  , header :: [(Text, Text)]
-  , body   :: Body
+  { date    :: UTCTime
+  , headers :: [(Text, Text)]
+  , body    :: Body
   }
 
 main :: IO ()
@@ -89,16 +90,16 @@ main = do
         fst
         (msgsByThread <> thrdsByThread)
     now <- lift getCurrentTime
-    let entries = threadToEntry <$> sortOn (date :: Thread -> UTCTime) (rights result)
-        feed    = nullFeed [i|read-later-e-mails-#{timestamp now}|]
-                           (TextString "Readlater-E-Mail")
-                           (timestamp now)
+    let entries =
+          threadToEntry <$> sortOn (date :: Thread -> UTCTime) (rights result)
+        feed = nullFeed [i|read-later-e-mails-#{timestamp now}|]
+                        (TextString "Readlater-E-Mail")
+                        (timestamp now)
         errors = lefts result
-    feedText <-
-      tryJust [i|Failed to generate feed.|] . textFeed $ feed
-        { feedEntries =
-          (if null errors then id else (errorsToEntry now errors :)) entries
-        }
+    feedText <- tryJust [i|Failed to generate feed.|] . textFeed $ feed
+      { feedEntries = (if null errors then id else (errorsToEntry now errors :))
+                        entries
+      }
     say $ toStrict feedText
   either
     (\(er :: Text) ->
@@ -108,13 +109,13 @@ main = do
     res
 
 threadToEntry :: Thread -> Entry
-threadToEntry Thread { subject, messages, threadid, totalCount, date, authors } =
-  (nullEntry threadUrl threadTitle (timestamp date))
-    { entryContent = Just . HTMLContent $ content,
-      entryAuthors = (\x -> nullPerson { personName = x }) <$> authors
+threadToEntry Thread { subject, messages, threadid, totalCount, date, authors }
+  = (nullEntry threadUrl threadTitle (timestamp date))
+    { entryContent = Just . HTMLContent $ content
+    , entryAuthors = (\x -> nullPerson { personName = x }) <$> authors
     }
  where
-  threadUrl  = [i|thread-#{threadid}-#{timestamp date}|]
+  threadUrl   = [i|thread-#{threadid}-#{timestamp date}|]
   threadTitle = TextString [i|#{subject} (#{length messages}/#{totalCount})|]
   content     = T.intercalate [i|<br>\n<hr>\n|] (messageToHtml <$> messages)
 
@@ -152,16 +153,20 @@ processThread (threadid, toList -> thrdAndMsgs) =
     let messages = sortOn (date :: Message -> UTCTime) results
     subject    <- decodeUtf8 <$> Notmuch.threadSubject thread
     totalCount <- Notmuch.threadTotalMessages thread
-    authors <- (^. Notmuch.matchedAuthors) <$> Notmuch.threadAuthors thread
-    date <- Notmuch.threadNewestDate thread
+    authors    <- (^. Notmuch.matchedAuthors) <$> Notmuch.threadAuthors thread
+    date       <- Notmuch.threadNewestDate thread
     pure (Thread { subject, threadid, messages, totalCount, authors, date })
 
 
 messageToHtml :: Message -> Text
-messageToHtml Message { header, body } = T.intercalate "<br>\n" $ ((\(name, content) -> [i|<b>#{name}:</b> #{content}|]) <$> header) <> ["<br>\n", bodyToHtml body]
+messageToHtml Message { headers, body } =
+  T.intercalate "<br>\n"
+    $  ((\(name, content) -> [i|<b>#{name}:</b> #{content}|]) <$> headers)
+    <> one (bodyToHtml body)
 
 bodyToHtml :: Body -> Text
-bodyToHtml (HTMLBody x) = x
+bodyToHtml (HTMLBody x) = fromMaybe x onlyBody
+   where onlyBody = renderTags . takeWhile (not . isTagCloseName "body") <$> (viaNonEmpty tail . dropWhile (not . isTagOpenName "body") . parseTags $ x)
 bodyToHtml (TextBody x) = T.intercalate "<br>\n" . T.splitOn "\n" $ x
 
 processMessage :: (MonadIO m, MonadCatch m) => Notmuch.Message n a -> m Message
@@ -194,7 +199,7 @@ processMessage msg = do
         parseResult
       (if isHtml textPart then HTMLBody else TextBody)
         <$> tryJust [i|Could not decode message|] (decode textPart)
-  pure $ Message { date, header = hdrs, body = either TextBody id msgEither }
+  pure $ Message { date, headers = hdrs, body = either TextBody id msgEither }
 
 tryHdr :: MonadIO m => ByteString -> Notmuch.Message n a -> m (Maybe Text)
 tryHdr h msg =
