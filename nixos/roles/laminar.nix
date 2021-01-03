ee874ff44d7345457e0de97ee9a29e020e25a100
Malte Brandy <malte.brandy@maralorn.de>
2021-01-03 18:44:01 +0100

    Fix commit id

--- nixos/roles/laminar.nix
+++ nixos/roles/laminar.nix
@@ -2,6 +2,6 @@
 let
   laminarPkgs = builtins.fetchGit {
     url = "https://github.com/maralorn/nixpkgs.git";
-    rev = "e37cd38";
+    rev = "e37cd38effca0310cfe4d27583102175b4a456c9";
   };
 in { systemd.packages = [ laminarPkgs.laminar ]; }
