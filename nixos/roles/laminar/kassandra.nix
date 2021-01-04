dc31655d649ef49c853a86c6c98c011cf325897d
Malte Brandy <malte.brandy@maralorn.de>
2021-01-04 11:38:31 +0100

    Simply kassandra meta job

--- nixos/roles/laminar/kassandra.nix
+++ nixos/roles/laminar/kassandra.nix
@@ -2,10 +2,10 @@
 let
   path = [ pkgs.git pkgs.nix pkgs.gnutar pkgs.gzip pkgs.openssh pkgs.laminar ];
   setup = ''
-    set -ex
     export PATH=${lib.makeBinPath path}:$PATH
   '';
   target = name: ''
+    set -ex
     ${setup}
     export HOME=$PWD
     git clone git@localhost:kassandra2 kassandra
@@ -15,15 +15,8 @@ in {
   services.laminar.jobs = {
     "kassandra.run" = pkgs.writeShellScript "kassandra" ''
       ${setup}
-      laminarc run kassandra-lib &
-      P1=$!
-      laminarc run kassandra-android &
-      P2=$!
-      laminarc run kassandra-app &
-      P3=$!
-      laminarc run kassandra-server &
-      P4=$!
-      wait $P1 $P2 $P3 $P4
+      echo Launching and waiting for jobs lib, app, android and server
+      laminarc run kassandra-lib kassandra-android kassandra-app kassandra-server
     '';
     "kassandra-lib.run" = pkgs.writeShellScript "kassandra-lib" (target "lib");
     "kassandra-app.run" = pkgs.writeShellScript "kassandra-app" (target "app");
