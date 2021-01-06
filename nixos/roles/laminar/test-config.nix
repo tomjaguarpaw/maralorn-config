d2f2dd009573d63f3f4cd5349b85a723dd2346f7
Malte Brandy <malte.brandy@maralorn.de>
2021-01-06 02:14:31 +0100

    Fix test-config job

--- nixos/roles/laminar/test-config.nix
+++ nixos/roles/laminar/test-config.nix
@@ -1,14 +1,15 @@
 { pkgs, lib, config, ... }:
-{
+let
+  path = [ pkgs.git pkgs.nix pkgs.gnutar pkgs.gzip pkgs.openssh pkgs.laminar ];
+in {
   services.laminar.cfgFiles.jobs = {
-    "test-config.run" = pkgs.writeShellScript "test-config"
-  ''
-    set -ex
-    export PATH=${lib.makeBinPath path}:$PATH
-    export NIX_PATH="/etc/nix-path:nixos-config=/etc/nixos/configuration.nix"
-    cd /var/cache/gc-links
-    ${pkgs.test-config}/bin/test-config
-    ${pkgs.systemd}/bin/systemctl start --no-block update-config
-  '';
+    "test-config.run" = pkgs.writeShellScript "test-config" ''
+      set -ex
+      export PATH=${lib.makeBinPath path}:$PATH
+      export NIX_PATH="/etc/nix-path:nixos-config=/etc/nixos/configuration.nix"
+      cd /var/cache/gc-links
+      ${pkgs.test-config}/bin/test-config
+      ${pkgs.systemd}/bin/systemctl start --no-block update-config
+    '';
   };
 }
