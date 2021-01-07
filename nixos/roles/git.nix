327abd798c07bb93b44f3f0d2ddffa8e7a003714
Malte Brandy <malte.brandy@maralorn.de>
2021-01-07 06:18:30 +0100

    Smoothing push-hook

--- nixos/roles/git.nix
+++ nixos/roles/git.nix
@@ -22,15 +22,15 @@ let
       pure repoDir
 
     main = do
-      mirrorMay <- lookupEnv "GL_OPTION_MIRROR"
-      whenJust mirrorMay $ \mirror -> do
-        say [i|Force pushing all branches to #{mirror}|]
-        git "push" "--all" "-f" mirror
       jobMay <- lookupEnv "GL_OPTION_CI_JOB"
       whenJust jobMay $ \job -> do
         args <- toString . Text.intercalate " " . fmap toText <$> getArgs
         jobName <- decodeUtf8 <$> (laminarc ["queue", job, [i|BRANCH=#{args}|]] |> captureTrim)
         say [i|Queued job #{jobName}.\nSee https://ci.maralorn.de/jobs/#{Text.replace ":" "/" jobName}|]
+      mirrorMay <- lookupEnv "GL_OPTION_MIRROR"
+      whenJust mirrorMay $ \mirror -> do
+        say [i|Force pushing all branches to #{mirror}|]
+        git "push" "--all" "-f" mirror
       deployMay <- lookupEnv "GL_OPTION_WEB_DEPLOY"
       whenJust deployMay $ \deploy -> do
         (maybe [] (\x -> ["-A", x]) -> target) <- lookupEnv "GL_OPTION_WEB_DEPLOY_NIX_TARGET"
@@ -38,26 +38,15 @@ let
         say [i|Building default.nix #{show target} to /var/www/#{deploy}|]
         bracket (checkout path) (rm "-rf") $ \repoDir -> withCurrentDirectory repoDir $ nix_build "-o" ([i|/var/www/#{deploy}|] :: String) target
         say "Done"
-      testFlag <- lookupEnv "GL_OPTION_TEST"
-      whenJust testFlag $ \_ -> do
-        say "Starting test-config.service."
-        exe "sudo" ${pkgs.haskellList update-command};
   '';
 in {
   systemd.tmpfiles.rules = let cfg = config.services.gitolite;
   in lib.mkAfter
   [ "z ${cfg.dataDir}/.ssh/id_ed25519 0600 ${cfg.user} ${cfg.group} - -" ];
-  security.sudo.extraRules = [{
-    commands = [{
-      command = builtins.concatStringsSep " " update-command;
-      options = [ "NOPASSWD" ];
-    }];
-    users = [ "git" ];
-  }];
   services.gitolite = {
     enable = true;
     user = "git";
-    adminPubkey = builtins.elemAt (pkgs.privateValue [""] "ssh-keys" )0;
+    adminPubkey = builtins.elemAt (pkgs.privateValue [ "" ] "ssh-keys") 0;
     commonHooks = [ "${post-update}/bin/post-update" ];
   };
 }
