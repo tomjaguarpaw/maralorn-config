b4a7e155b49a5c0cb3692fe91add7187d4c22953
Malte Brandy <malte.brandy@maralorn.de>
2021-02-16 21:00:47 +0100

    Install some Haskell packages

--- overlays/30-ghc.nix
+++ overlays/30-ghc.nix
@@ -1,12 +1,13 @@
 self: super:
 let
   inherit (super) fetchFromGitHub;
-  master = import super.sources.nixpkgs-master { };
-  inherit (master.haskell.lib) overrideCabal unmarkBroken;
+  master = import super.sources.nixpkgs-master {};
+  inherit (master.haskell.lib) overrideCabal unmarkBroken doJailbreak;
   makeHaskellScriptPackages = p: {
     inherit (p)
       aeson shh string-interpolate relude replace-megaparsec async say
-      megaparsec fdo-notify these fsnotify;
+      megaparsec fdo-notify these fsnotify
+      ;
   };
   makeHaskellPackages = p:
     {
@@ -14,10 +15,15 @@ let
         brittany ormolu releaser cabal-fmt stack ghcid ghcide
         haskell-language-server cabal-install dhall taskwarrior pandoc hlint
         cabal2nix weeder reflex-dom password optics shh-extras neuron
-        hspec-discover cabal-edit paths hmatrix postgresql-simple;
+        hspec-discover cabal-edit paths hmatrix postgresql-simple snap
+        ;
+      iCalendar = overrideCabal (doJailbreak (unmarkBroken p.iCalendar)) {
+        preConfigure = ''substituteInPlace iCalendar.cabal --replace "network >=2.6 && <2.7" "network -any"'';
+      };
     } // makeHaskellScriptPackages p;
-    inherit (master) ghc haskellPackages;
-in {
+  inherit (master) ghc haskellPackages;
+in
+{
   inherit ghc haskellPackages;
   nix-output-monitor = master.nix-output-monitor;
   myHaskellPackages = makeHaskellPackages haskellPackages;
