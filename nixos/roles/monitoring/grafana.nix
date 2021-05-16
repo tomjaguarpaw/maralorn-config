35956709b0f2a458d9d808a5940d4f77df259b51
Malte Brandy <malte.brandy@maralorn.de>
2021-05-17 01:48:46 +0200

    Improve badges

--- nixos/roles/monitoring/grafana.nix
+++ nixos/roles/monitoring/grafana.nix
@@ -6,33 +6,19 @@ let
     badge "https://ci.maralorn.de/badge/${name}.svg"
       "https://ci.maralorn.de/jobs/${name}";
   badges = lib.concatStringsSep "\\n" [
+
     (heading "ci.maralorn.de" "https://ci.maralorn.de")
     (job "kassandra")
     (job "test-config")
     (job "logfeed")
-    (
-      heading "haskell-taskwarrior"
-        "https://hackage.haskell.org/package/taskwarrior"
-    )
-    (badge "https://github.com/maralorn/haskell-taskwarrior/actions/workflows/haskell.yml/badge.svg" "https://github.com/maralorn/taskwarrior/actions")
-    (
-      badge
-        "https://repology.org/badge/vertical-allrepos/haskell:taskwarrior.svg?columns=3&header="
-        "https://repology.org/project/haskell:taskwarrior/versions"
-    )
-    (
-      badge "https://img.shields.io/hackage-deps/v/taskwarrior.svg"
-        "http://packdeps.haskellers.com/reverse/taskwarrior"
-    )
-    (
-      heading "nix-output-monitor"
-        "https://github.com/maralorn/nix-output-monitor"
-    )
-    (
-      badge
-        "https://repology.org/badge/vertical-allrepos/nix-output-monitor.svg?columns=3&header="
-        "https://repology.org/project/nix-output-monitor/versions"
-    )
+
+    (heading "haskell-taskwarrior" "https://hackage.haskell.org/package/taskwarrior")
+    (badge "https://github.com/maralorn/haskell-taskwarrior/actions/workflows/haskell.yml/badge.svg" "https://github.com/maralorn/haskell-taskwarrior/actions")
+    (badge "https://img.shields.io/hackage-deps/v/taskwarrior.svg" "http://packdeps.haskellers.com/reverse/taskwarrior")
+    (badge "https://repology.org/badge/vertical-allrepos/haskell:taskwarrior.svg?columns=3&header=" "https://repology.org/project/haskell:taskwarrior/versions")
+
+    (heading "nix-output-monitor" "https://github.com/maralorn/nix-output-monitor")
+    (badge "https://repology.org/badge/vertical-allrepos/nix-output-monitor.svg?columns=3&header=" "https://repology.org/project/nix-output-monitor/versions")
   ];
   dashboards = pkgs.runCommand "dashboards" {} ''
     mkdir -p $out
