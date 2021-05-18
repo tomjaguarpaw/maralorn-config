5ed04c2201ea9241bcb8613c5b354d3a961769c8
Malte Brandy <malte.brandy@maralorn.de>
2021-05-18 18:33:01 +0200

    Fix test-config PATH

--- nixos/roles/laminar/test-config.nix
+++ nixos/roles/laminar/test-config.nix
@@ -101,7 +101,7 @@ in
           (builtins.readFile ./test-config.hs);
       in
       pkgs.writeShellScript "test-config" ''
-        PATH=${pkgs.gnutar}/bin:$PATH ${test-config}
+        PATH=${lib.makeBinPath [ pkgs.gnutar pkgs.nix pkgs.laminar]}:$PATH ${test-config}
       '';
     "bump-config.run" = pkgs.writeHaskell "bump-config"
       {
