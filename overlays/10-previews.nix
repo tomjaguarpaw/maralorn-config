09754e429da7913d2ccea10133f7d9ba8b7aa95c
Malte Brandy <malte.brandy@maralorn.de>
2021-05-20 23:54:50 +0200

    Update gomuks

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -3,8 +3,6 @@ let
   unstable = import super.sources.nixos-unstable { };
 in
 {
-  inherit (unstable)
-    laminar upterm syncthing vimPlugins dhallPackages fzf
-    ;
+  inherit (unstable) laminar upterm syncthing vimPlugins dhallPackages fzf gomuks;
   matrix-synapse-tools.rust-synapse-compress-state = unstable.matrix-synapse-tools.rust-synapse-compress-state;
 }
