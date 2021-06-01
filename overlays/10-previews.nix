c6b3e88210e4f4ca61bce9cc01febf0f329e825e
Malte Brandy <malte.brandy@maralorn.de>
2021-06-01 23:59:37 +0200

    Remove previews

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -3,5 +3,5 @@ let
   unstable = import super.sources.nixos-unstable { };
 in
 {
-  inherit (unstable) laminar upterm syncthing vimPlugins dhallPackages fzf gomuks nheko haskellPackages ghc cachix nix-output-monitor matrix-synapse-tools;
+  inherit (unstable) haskellPackages ghc cachix nix-output-monitor;
 }
