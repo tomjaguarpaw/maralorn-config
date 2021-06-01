56793acd7d59e5f794409dac5f4459af7b802d3c
Malte Brandy <malte.brandy@maralorn.de>
2021-06-02 00:01:43 +0200

    Unbump steam

--- overlays/20-unfree.nix
+++ overlays/20-unfree.nix
@@ -1,10 +1,8 @@
 self: super:
 let
   unfree = import self.sources."${self.nixpkgs-channel}" { config.allowUnfree = true; };
-  unstableUnfree =
-    import self.sources.nixos-unstable { config.allowUnfree = true; };
+  # unstableUnfree = import self.sources.nixos-unstable { config.allowUnfree = true; };
 in
 {
-  inherit (unfree) discord factorio zoom-us skypeforlinux google-chrome minecraft teamviewer;
-  inherit (unstableUnfree) steam;
+  inherit (unfree) discord factorio zoom-us skypeforlinux google-chrome minecraft teamviewer steam;
 }
