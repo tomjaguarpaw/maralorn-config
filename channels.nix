5b8cfd7d50071c0e825e97199c85f796701da670
Malte Brandy <malte.brandy@maralorn.de>
2021-06-01 23:57:42 +0200

    Introduce nixos-21.05

--- channels.nix
+++ channels.nix
@@ -1,4 +1,8 @@
 let
+  nixos-21-05 = {
+    nixpkgs-channel = "nixos-21.05";
+    home-manager-channel = "home-manager-21.05";
+  };
   nixos-20-09 = {
     nixpkgs-channel = "nixos-20.09";
     home-manager-channel = "home-manager-20.09";
@@ -10,6 +14,6 @@ let
 in
 rec {
   hera = nixos-20-09;
-  apollo = nixos-20-09;
+  apollo = nixos-21-05;
   chor-cloud = hera;
 }
