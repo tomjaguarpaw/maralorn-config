96f4337e51e25fdc0c1a7f15fd2af36e53ddeaff
Malte Brandy <malte.brandy@maralorn.de>
2021-06-06 19:33:30 +0200

    Update mailman

--- nixos/roles/mailman.nix
+++ nixos/roles/mailman.nix
@@ -1,9 +1,22 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 let
   hostname = "lists.maralorn.de";
   admin = "malte.brandy@maralorn.de";
+  cfg = config.services.mailman;
+  lists = pkgs.privateValue { } "mail/lists";
 in
 {
+  systemd.services.mailman.postStart = lib.concatStringsSep "\n" (
+    map
+      (
+        x: ''
+          ${cfg.package}/bin/mailman syncmembers -W -G - "${x}" << EOF
+          ${lib.concatStringsSep "\n" lists.${x}}
+          EOF
+        ''
+      )
+      (builtins.attrNames lists)
+  );
   services = {
     mailman = {
       enable = true;
@@ -13,26 +26,28 @@ in
       siteOwner = admin;
       settings = {
         mailman.default_language = "de";
-        "paths.fhs".template_dir = lib.mkForce (pkgs.setToDirectories {
-          site.de = {
-            "list:user:notice:goodbye.txt" = builtins.toFile "goodbye" ''
-              Du erhältst nun keine E-Mails mehr über diese Mailingliste.
+        "paths.fhs".template_dir = lib.mkForce (
+          pkgs.setToDirectories {
+            site.de = {
+              "list:user:notice:goodbye.txt" = builtins.toFile "goodbye" ''
+                Du erhältst nun keine E-Mails mehr über diese Mailingliste.
 
-              Bei Fragen oder wenn Du doch E-Mails von dieser Liste bekommen möchtest wende Dich an ${admin}.
-            '';
-            "list:member:generic:footer.txt" = builtins.toFile "footer" ''
-              ---
-              Du erhältst diese E-Mail über die Mailingliste "$display_name".
-              Bei Fragen oder wenn Du diese E-Mails nicht mehr bekommen möchtest wende Dich an ${admin}.
-            '';
+                Bei Fragen oder wenn Du doch E-Mails von dieser Liste bekommen möchtest wende Dich an ${admin}.
+              '';
+              "list:member:generic:footer.txt" = builtins.toFile "footer" ''
+                ---
+                Du erhältst diese E-Mail über die Mailingliste "$display_name".
+                Bei Fragen oder wenn Du diese E-Mails nicht mehr bekommen möchtest wende Dich an ${admin}.
+              '';
 
-            "list:user:notice:welcome.txt" = builtins.toFile "welcome" ''
-              Herzlich Willkommen auf der Mailingliste "$display_name".
+              "list:user:notice:welcome.txt" = builtins.toFile "welcome" ''
+                Herzlich Willkommen auf der Mailingliste "$display_name".
 
-              Bei Fragen und wenn Du keine E-Mails von dieser Liste mehr bekommen möchtest wende Dich an ${admin}.
-            '';
-          };
-        }).outPath;
+                Bei Fragen und wenn Du keine E-Mails von dieser Liste mehr bekommen möchtest wende Dich an ${admin}.
+              '';
+            };
+          }
+        ).outPath;
       };
     };
     postfix = {
