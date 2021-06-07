8c04d47ed527f34dc1bb8cd2952cf0eb01e6cef4
Malte Brandy <malte.brandy@maralorn.de>
2021-06-08 00:08:46 +0200

    Comment firefox

--- home-manager/roles/desktop/firefox.nix
+++ home-manager/roles/desktop/firefox.nix
@@ -2,7 +2,11 @@
   programs.firefox.enable = true;
   programs.browserpass.enable = true;
   home.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = 1;
+    MOZ_ENABLE_WAYLAND = 1; # So that firefox uses wayland.
+
+    # So that electron can open firefox links. See
+    # Issue: https://github.com/electron/electron/issues/28436
+    # Solution from: https://wiki.archlinux.org/title/Firefox#Applications_on_Wayland_can_not_launch_Firefox
     MOZ_DBUS_REMOTE = 1;
   };
 }
