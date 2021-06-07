77c5fcab9c13fd9aed9dc9e4e047ca9abcdb6096
Malte Brandy <malte.brandy@maralorn.de>
2021-06-07 02:40:38 +0200

    Fix element links

--- home-manager/roles/desktop/firefox.nix
+++ home-manager/roles/desktop/firefox.nix
@@ -1,5 +1,8 @@
 { ... }: {
   programs.firefox.enable = true;
   programs.browserpass.enable = true;
-  home.sessionVariables.MOZ_ENABLE_WAYLAND = 1;
+  home.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = 1;
+    MOZ_DBUS_REMOTE = 1;
+  };
 }
