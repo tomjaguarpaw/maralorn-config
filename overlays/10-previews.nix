a9cbbd14c2fb59272c3ef0d54943e0bbe406b169
Malte Brandy <malte.brandy@maralorn.de>
2021-06-13 03:17:58 +0200

    Preview cabal2nix

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -3,5 +3,5 @@ let
   unstable = import super.sources.nixos-unstable { };
 in
 {
-  inherit (unstable) haskellPackages ghc cachix nix-output-monitor;
+  inherit (unstable) haskellPackages ghc cachix nix-output-monitor cabal2nix;
 }
