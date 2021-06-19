6917cd8b8fd3e476f39cf843efface5858370866
Malte Brandy <malte.brandy@maralorn.de>
2021-06-19 06:01:26 +0200

    Install cgit

--- nixos/roles/git.nix
+++ nixos/roles/git.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 let
+  gitoliteCfg = config.services.gitolite;
   me = config.m-0.private.me;
   update-command = [
     "${pkgs.systemd}/bin/systemctl"
@@ -41,17 +42,112 @@ let
         bracket (checkout path) (rm "-rf") $ \repoDir -> withCurrentDirectory repoDir $ nix_build "-o" ([i|/var/www/#{deploy}|] :: String) target
         say "Done"
   '';
+  cgitrc = ''
+    enable-git-config=1
+    remove-suffix=1
+    section-from-path=1
+    cache-size=1000
+
+    source-filter=${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py
+    about-filter=${pkgs.cgit}/lib/cgit/filters/about-formatting.sh
+    root-title=Maralorns Projects
+    root-desc=All my public projects.
+
+    snapshots=tar.gz zip
+
+    # Make packages cloneable
+    clone-url=https://git.maralorn.de/$CGIT_REPO_URL
+
+    # hide my name in index
+    enable-index-owner=0
+
+    # for look and feel
+    enable-index-links=1
+    enable-blame=1
+    enable-commit-graph=1
+    enable-follow-links=1
+    enable-log-filecount=1
+    enable-log-linecount=1
+    branch-sort=age
+    noplainemail=1
+    side-by-side-diffs=1
+
+    ##
+    ## List of common mimetypes
+    ##
+
+    mimetype.gif=image/gif
+    mimetype.html=text/html
+    mimetype.jpg=image/jpeg
+    mimetype.jpeg=image/jpeg
+    mimetype.pdf=application/pdf
+    mimetype.png=image/png
+    mimetype.svg=image/svg+xml
+
+    ## Search for these files in the root of the default branch of repositories
+    ## for coming up with the about page:
+
+    readme=:README.md
+    readme=:readme.md
+    readme=:README
+    readme=:readme
+
+    project-list=/var/lib/gitolite/projects.list
+    scan-path=/var/lib/gitolite/repositories
+  '';
 in
 {
   systemd.tmpfiles.rules =
-    let cfg = config.services.gitolite;
-    in
     lib.mkAfter
-      [ "z ${cfg.dataDir}/.ssh/id_ed25519 0600 ${cfg.user} ${cfg.group} - -" ];
-  services.gitolite = {
-    enable = true;
-    user = "git";
-    adminPubkey = builtins.elemAt (pkgs.privateValue [ "" ] "ssh-keys") 0;
-    commonHooks = [ "${post-update}/bin/post-update" ];
+      [
+        "z ${gitoliteCfg.dataDir}/.ssh/id_ed25519 0600 ${gitoliteCfg.user} ${gitoliteCfg.group} - -"
+        "v /var/cache/cgit 0700 cgit ${gitoliteCfg.group} - -"
+      ];
+  users.users.cgit = {
+    isSystemUser = true;
+    group = gitoliteCfg.group;
   };
+  services = {
+    gitolite = {
+      enable = true;
+      user = "git";
+      adminPubkey = builtins.elemAt (pkgs.privateValue [ "" ] "ssh-keys") 0;
+      commonHooks = [ "${post-update}/bin/post-update" ];
+      extraGitoliteRc = ''
+        $RC{UMASK} = 0027;
+        $RC{GIT_CONFIG_KEYS} = 'gitweb\..*';
+        push( @{$RC{ENABLE}}, 'cgit' );
+      '';
+    };
+    fcgiwrap = {
+      enable = true;
+      user = "cgit";
+      group = gitoliteCfg.group;
+    };
+
+    nginx.virtualHosts."git.maralorn.de" = {
+      forceSSL = true;
+      enableACME = true;
+
+      locations = {
+        "~* ^.+\.(css|png|ico)$" = {
+          root = "${pkgs.cgit}/cgit";
+        };
+
+        "/" = {
+          extraConfig = ''
+            include ${pkgs.nginx}/conf/fastcgi_params;
+            fastcgi_param CGIT_CONFIG ${pkgs.writeText "cgitrc" cgitrc};
+            fastcgi_param SCRIPT_FILENAME ${pkgs.cgit}/cgit/cgit.cgi;
+            fastcgi_split_path_info ^(/?)(.+)$;
+            fastcgi_param PATH_INFO $fastcgi_path_info;
+            fastcgi_param QUERY_STRING $args;
+            fastcgi_param HTTP_HOST $server_name;
+            fastcgi_pass unix:${config.services.fcgiwrap.socketAddress};
+          '';
+        };
+      };
+    };
+  };
+
 }
