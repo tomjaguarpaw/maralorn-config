547b044e022a261d278473124028f3cc983db34d
Malte Brandy <malte.brandy@maralorn.de>
2021-07-05 00:52:40 +0200

    Fix wallpapers

--- home-manager/roles/state.nix
+++ home-manager/roles/state.nix
@@ -3,7 +3,7 @@ let
   # Persistent means that files get snapshoted and kept for a month
   # Volatile means that files just lay on the disk
   # Backups are organized independently on this system
-  persistentStateDirs = [ "git" "media" "Maildir" ".ssh" ".task" ".gnupg" ".calendars" ".contacts" ];
+  persistentStateDirs = [ "git" "media" "Maildir" ".ssh" ".task" ".gnupg" ".calendars" ".contacts" ".wallpapers" ];
   persistentStateFiles = [ ".chpwd-recent-dirs" ".zsh_history" ];
   volatileStateFiles = [ ];
   volatileStateDirs = [ ".steam" ".local/share/Steam" ];
@@ -23,5 +23,8 @@ in
         map (mkEntry "d" "persist") persistentStateDirs
     ) ++ [
     (mkLine "L+" "/home/maralorn/.password-store" "git/password-store")
+    (mkLine "f" "/home/maralorn/documents/orga/listen/checklisten/orga-pre.md" "")
+    (mkLine "f" "/home/maralorn/tmp/today.md" "")
+    (mkLine "f" "/home/maralorn/volatile/mode" "default")
   ];
 }
