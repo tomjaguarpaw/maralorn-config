602601ff9b18e0d5cf71777b4409f7e62ccee333
Malte Brandy <malte.brandy@maralorn.de>
2021-07-06 14:15:57 +0200

    Update gnome config

--- home-manager/roles/desktop/gnome.nix
+++ home-manager/roles/desktop/gnome.nix
@@ -1,9 +1,72 @@
-{ pkgs, lib, ... }: {
-  imports = [ ./gnome-generated.nix ];
+{ pkgs, lib, ... }:
+let
+  mkTuple = lib.hm.gvariant.mkTuple;
+in
+{
   services.gpg-agent.pinentryFlavor = "gnome3";
   dconf.settings = {
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-input-source = [ ];
+      switch-input-source-backward = [ ];
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>F5";
+      command = "systemctl suspend";
+      name = "Standby";
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      gtk-im-module = "gtk-im-context-simple";
+      gtk-theme = "Arc";
+      icon-theme = "Arc";
+    };
+
+    "org/gnome/shell/extensions/gtile" = {
+      show-toggle-tiling-alt = [ "<Super>t" ];
+    };
+
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [ "gTile@vibou" "clipboard-indicator@tudmotu.com" "appindicatorsupport@rgcjonas.gmail.com" "nothing-to-say@extensions.gnome.wouter.bolsterl.ee" "notification-position@drugo.dev" "drive-menu@gnome-shell-extensions.gcampax.github.com" "sound-output-device-chooser@kgshank.net" "system-monitor@paradoxxx.zero.gmail.com" "user-theme@gnome-shell-extensions.gcampax.github.com" ];
+      welcome-dialog-last-shown-version = "40.1";
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Arc-Lighter";
+    };
+
+    "system/locale" = {
+      region = "en_DK.UTF-8";
+    };
+
+    "org/gnome/shell/extensions/system-monitor" = {
+      center-display = true;
+      compact-display = true;
+      cpu-show-menu = false;
+      cpu-show-text = false;
+      cpu-style = "graph";
+      icon-display = false;
+      memory-show-text = false;
+      memory-style = "graph";
+      move-clock = false;
+      net-show-menu = true;
+      net-show-text = false;
+      net-speed-in-bits = true;
+      net-style = "both";
+      show-tooltip = true;
+    };
+
     "org/gnome/desktop/input-sources" = {
-      sources = [ (lib.hm.gvariant.mkTuple [ "xkb" "de+neo" ]) ]; # use neo
+      sources = [ (mkTuple [ "xkb" "de+neo" ]) ]; # use neo
       xkb-options = [
         "altwin:swap_lalt_lwin" # swap alt and win
         "lv3:menu_switch" # So that gnome-settings does not set it to ralt
@@ -28,6 +91,7 @@
         name = "Clear Notifications";
       };
     "org/gnome/shell/extensions/nothing-to-say" = {
+      icon-visibility = "always";
       keybinding-toggle-mute = [ "<Primary><Shift>U+2113" ]; # Mouse key side middle
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
