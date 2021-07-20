87dc899860fab724f39f534d67fb6b2678a52953
Malte Brandy <malte.brandy@maralorn.de>
2021-07-20 23:34:24 +0200

    Add startpage

--- home-manager/machines.nix
+++ home-manager/machines.nix
@@ -47,10 +47,15 @@ let
     { pkgs, lib, ... }: {
       systemd.user.services.blockserver = {
         Unit.Description = "Serve a blocklist";
-        Service = {
-          ExecStart = "${pkgs.python3}/bin/python -m http.server 8842 -d ${pkgs.writeTextDir "blocklist" (lib.concatStringsSep "\r\n" list)}";
-          Restart = "always";
-        };
+        Service =
+          let
+            blocklist = pkgs.writeTextDir "blocklist" (lib.concatStringsSep "\r\n" list);
+            startpage = pkgs.writeTextDir "index.html" (builtins.readFile ./startpage.html);
+          in
+          {
+            ExecStart = "${pkgs.python3}/bin/python -m http.server 8842 -d ${pkgs.symlinkJoin { name = "blockserver-dir"; paths = [ blocklist startpage ]; }}";
+            Restart = "always";
+          };
         Install.WantedBy = [ "default.target" ];
       };
     };
@@ -137,7 +142,7 @@ in
         ./roles/update_tasks.nix
         ./roles/vdirsyncer.nix
         (import ./roles/mode-switching.nix { modeDir = ".volatile/modes"; modeFile = ".mode"; })
-        (setStartpage "https://stats.maralorn.de")
+        (setStartpage "http://localhost:8842")
         (makeAutostart "kassandra2")
         (makeAutostart "unlock-ssh")
       ];
