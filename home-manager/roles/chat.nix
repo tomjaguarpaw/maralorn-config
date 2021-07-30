3cb0b72ee96693171755b8cb5b1546d2e945e0ed
Malte Brandy <malte.brandy@maralorn.de>
2021-07-30 20:56:32 +0200

    Fix weechat script

--- home-manager/roles/chat.nix
+++ home-manager/roles/chat.nix
@@ -2,13 +2,6 @@
 {
   home.packages = builtins.attrValues rec {
     inherit (pkgs) discord signal-desktop tdesktop dino element-desktop;
-    weechat = pkgs.writeShellScriptBin "weechat" "ssh -t hera 'tmux -L weechat attach'";
-    chat = pkgs.writeHaskellScript
-      {
-        name = "chat";
-        bins = [ element-desktop signal-desktop weechat discord tdesktop dino pkgs.kitty ];
-      } ''
-      main = mapConcurrently_ Relude.id [ element_desktop, signal_desktop, _Discord, telegram_desktop, kitty "weechat", dino ]
-    '';
+    weechat = pkgs.writeShellScriptBin "weechat" "ssh -t hera 'TMUX_TMPDIR=/run/user/1000 tmux -L weechat attach'";
   };
 }
