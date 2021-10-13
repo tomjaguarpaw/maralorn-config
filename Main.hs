ec9a33de9e00965f460c1a9388393d0c1616615c
Malte Brandy <malte.brandy@maralorn.de>
2021-10-13 18:20:14 +0200

    Support lists of keys

--- Main.hs
+++ Main.hs
@@ -4,34 +4,35 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-import Data.Aeson (
-   FromJSON (..),
-   withObject,
- )
+import Relude
+import Witch
+
+import Data.Aeson (FromJSON (..), Value (Array, String), withObject)
 import qualified Data.Aeson.Key as Key
 import qualified Data.Aeson.KeyMap as Aeson
-import Data.Aeson.Types (Value (String))
-import Dialog
-import Relude
-import System.Process (callCommand)
-import Witch
+import qualified Data.Foldable as Foldable
 import qualified Data.Text as Text
-import System.Posix.Daemon (runDetached, Redirection (DevNull))
 import Data.Yaml (decodeFileEither)
+
+import System.Posix.Daemon (Redirection (DevNull), runDetached)
+import System.Process (callCommand)
+
+import Dialog (Menu (..), MenuEntry (Option, SubMenu), menu, runClearingHaskeline)
 
 data Command = Run Text | Fork Text deriving (Show)
 
 instance FromJSON (Menu Command) where
    parseJSON = parseMenu "Hotkeys"
      where
-      parseMenu name = withObject "mapping to menu entries" $ \object ->
-         fmap (Menu name) $
-            forM (reverse $ Aeson.toList object) $ \(Key.toText -> key, val) ->
-               case val of
-                  String cmd -> pure $ Dialog.Option key (text2cmd cmd)
-                  innerObj -> Dialog.SubMenu <$> parseMenu key innerObj
+      parseList name = (fmap (Menu name) .) $
+         mapM $ \(Key.toText -> key, val) ->
+            case val of
+               String cmd -> pure $ Dialog.Option key (text2cmd cmd)
+               innerObj -> Dialog.SubMenu <$> parseMenu key innerObj
+      unpackMenu (Array arr) = join <$> mapM unpackMenu (reverse $ Foldable.toList arr)
+      unpackMenu value = withObject "mapping to menu entries" (pure . reverse . Aeson.toList) value
+      parseMenu name = parseList name <=< unpackMenu
       text2cmd t = if Text.isPrefixOf "fork " t then Fork (Text.drop 5 t) else Run t
-
 
 main :: IO ()
 main = do
