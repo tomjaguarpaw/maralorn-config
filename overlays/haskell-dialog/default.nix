d02f2454d05958e3a9e1cf589fd83d17abc9d5ff
Malte Brandy <malte.brandy@maralorn.de>
2021-11-15 10:59:03 +0100

    Fix haskell-dialog deps

--- overlays/haskell-dialog/default.nix
+++ overlays/haskell-dialog/default.nix
@@ -1,24 +1,12 @@
 final: prev:
 let
   override = hfinal: hprev: {
-    relude = hfinal.relude_1_0_0_1;
-    semialign = hfinal.semialign_1_2;
-    time-compat = hfinal.time-compat_1_9_6_1;
-    hashable = hfinal.hashable_1_3_4_1;
-    yaml = hfinal.callHackageDirect
-      {
-        ver = "0.11.7.0";
-        pkg = "yaml";
-        sha256 = "0mn08dh5h49np5rd8ym7wl3i8nfdj9vgawprcdgrmvz5j59nsp0n";
-      }
-      { };
-    aeson = hfinal.callHackageDirect
-      {
-        ver = "2.0.1.0";
-        pkg = "aeson";
-        sha256 = "0nhzbnygj17m4x39mmf8r13xisc0hnkijnrwyqskf8gk276x9dpz";
-      }
-      { };
+    relude = hfinal.callHackage "relude" "1.0.0.1" { };
+    semialign = hfinal.callHackage "semialign" "1.2" { };
+    time-compat = hfinal.callHackage "time-compat" "1.9.6.1" { };
+    aeson = hfinal.callHackage "aeson" "2.0.1.0" { };
+    hashable = hfinal.callHackage "hashable" "1.3.4.1" { };
+    yaml = hfinal.callHackage "yaml" "0.11.7.0" { };
   };
 in
 {
