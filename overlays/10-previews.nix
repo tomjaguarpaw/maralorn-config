e917fb1e5a70c914ab47dfa675d94fe77fdb0796
Malte Brandy <malte.brandy@maralorn.de>
2021-11-19 12:48:36 +0100

    Try bumping to nix 2.4

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -5,6 +5,7 @@ in
 {
   inherit unstable;
   inherit (unstable) cachix nix-output-monitor cabal2nix;
+  nix = unstable.nix;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
   mautrix-signal = unstable.mautrix-signal;
