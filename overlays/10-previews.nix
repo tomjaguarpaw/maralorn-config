0dad4dd29a2ae05bc6cedcbd745e014b6ec7421a
Malte Brandy <malte.brandy@maralorn.de>
2021-11-19 14:20:12 +0100

    Use patched nix

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -5,7 +5,15 @@ in
 {
   inherit unstable;
   inherit (unstable) cachix nix-output-monitor cabal2nix;
-  nix = unstable.nix;
+  nix = super.nix.overrideAttrs (o: {
+    patches = (o.patches or [ ]) ++ [
+      (super.fetchpatch {
+        # Overridable cache priorities
+        url = "https://github.com/NixOS/nix/commit/f8abbdd4565542464f31f4dc203a9c3e091b3536.patch";
+        sha256 = "0vbxiab0wd55ccnxkbi86lzmyrvjw946r9scxgq59xiyyss420rl";
+      })
+    ];
+  });
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
   mautrix-signal = unstable.mautrix-signal;
