1cce2c12f2b907e1a8d91698e111d79959b4c8b7
Malte Brandy <malte.brandy@maralorn.de>
2021-11-20 02:05:07 +0100

    Deinstall unused hub tool

--- home-manager/roles/tinkering.nix
+++ home-manager/roles/tinkering.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
   home.packages = builtins.attrValues {
     inherit (pkgs.xorg) xev;
-    inherit (pkgs.gitAndTools) hub;
     inherit (pkgs)
       meld icedtea8_web octave filezilla nix-review gparted
       grafana-devel;
