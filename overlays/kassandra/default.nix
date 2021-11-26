b5c0f07b41a0b6d733e96545ef03a5e48643d02a
Malte Brandy <malte.brandy@maralorn.de>
2021-11-27 00:31:17 +0100

    Bump taskwarrior

--- overlays/kassandra/default.nix
+++ overlays/kassandra/default.nix
@@ -3,7 +3,7 @@ let
   haskellPackages = prev.haskellPackages.extend (self: super: {
     kassandra = self.callCabal2nix "kassandra" (prev.sources.kassandra + "/kassandra") { };
     standalone = self.callCabal2nix "standalone" (prev.sources.kassandra + "/standalone") { };
-    taskwarrior = self.callHackageDirect { pkg = "taskwarrior"; ver = "0.3.1.0"; sha256 = "sha256-XUoa+xWUHfr080za07/4Xxcic6jgfljrTIXbLaXzoqQ="; } { };
+    taskwarrior = self.callHackageDirect { pkg = "taskwarrior"; ver = "0.5.0.0"; sha256 = "sha256-elDUtz0NSG4WHxkyCQ1CunYXWIVRj6EqkKSchPy+c3E="; } { };
   });
 in
 {
