9c5664170e72fd5892097e6a1df8e9d290c4c862
Malte Brandy <malte.brandy@maralorn.de>
2021-11-26 19:27:42 +0100

    Update kassandra for taskwarrior compat

--- overlays/kassandra2/default.nix
+++
@@ -1,8 +0,0 @@
-final: prev:
-let
-  kassandra = prev.haskellPackages.callCabal2nix "kassandra" (prev.sources.kassandra2 + "/kassandra") { };
-  standalone = prev.haskellPackages.callCabal2nix "standalone" (prev.sources.kassandra2 + "/standalone") { inherit kassandra; };
-in
-{
-  kassandra2 = standalone;
-}
