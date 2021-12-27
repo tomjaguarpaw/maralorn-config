14670f5918d02964e9318cb572f8f8437d20c122
Malte Brandy <malte.brandy@maralorn.de>
2021-12-27 16:48:39 +0100

    Use nightly nom

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -4,9 +4,13 @@ let
 in
 {
   inherit unstable;
-  inherit (unstable) cachix nix-output-monitor cabal2nix;
+  inherit (unstable) cachix cabal2nix;
   nix = self.nix_2_4;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
   home-assistant = unstable.home-assistant;
+  nix-output-monitor = unstable.nix-output-monitor.overrideAttrs (old: {
+    src = super.sources.nix-output-monitor;
+    buildInputs = old.buildInputs ++ [ super.haskellPackages.streamly ];
+  });
 }
