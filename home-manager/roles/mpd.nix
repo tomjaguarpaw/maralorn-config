06795d0481c55a13db88bee3ee24081bc456259e
Malte Brandy <malte.brandy@maralorn.de>
2021-12-27 16:56:33 +0100

    Add radio swiss jazz

--- home-manager/roles/mpd.nix
+++ home-manager/roles/mpd.nix
@@ -5,6 +5,7 @@
       musicDirectory = "${config.home.homeDirectory}/media/audio";
       playlistDirectory = pkgs.setToDirectories (lib.mapAttrs' (name: content: lib.nameValuePair "${name}.m3u" (builtins.toFile "${name}.m3u" content)) {
         "radio-swiss-classic" = "https://stream.srg-ssr.ch/m/rsc_de/aacp_96";
+        "radio-swiss-jazz" = "https://stream.srg-ssr.ch/rsj/aacp_96";
         "br-klassik" = "http://dispatcher.rndfnk.com/br/brklassik/live/mp3/high";
       });
       extraConfig = ''
