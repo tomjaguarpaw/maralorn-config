9faea8a63c555d4354563e52c8de38e0f24b0498
Malte Brandy <malte.brandy@maralorn.de>
2022-01-05 20:46:34 +0100

    Disable commit signing by default

--- home-manager/roles/git-sign.nix
+++ home-manager/roles/git-sign.nix
@@ -1,6 +1,3 @@
 { ... }: {
-  programs.git.signing = {
-    signByDefault = true;
-    key = "6C3D12CD88CDF46C5EAF4D12226A2D41EF5378C9";
-  };
+  programs.git.signing.key = "6C3D12CD88CDF46C5EAF4D12226A2D41EF5378C9";
 }
