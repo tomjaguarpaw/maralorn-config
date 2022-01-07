9d38e18c3e39a1128a108625df844ebbb95ad120
Malte Brandy <malte.brandy@maralorn.de>
2022-01-07 12:14:25 +0100

    Add new radio streams

--- home-manager/roles/mpd.nix
+++ home-manager/roles/mpd.nix
@@ -7,6 +7,10 @@
         "radio-swiss-classic" = "https://stream.srg-ssr.ch/m/rsc_de/aacp_96";
         "radio-swiss-jazz" = "https://stream.srg-ssr.ch/m/rsj/aacp_96";
         "br-klassik" = "http://dispatcher.rndfnk.com/br/brklassik/live/mp3/high";
+        "klassik-radio-games" = "https://klassikr.streamabc.net/klr-games-mp3-128-1540253";
+        "klassik-radio-movie" = "https://klassikr.streamabc.net/klr-movie-mp3-128-5213277";
+        "radio-caprice-power-metal" = "http://79.120.77.11:8000/powermetal";
+        "metal-hammer" = "https://metal-hammer.stream.laut.fm/metal-hammer";
       });
       extraConfig = ''
         audio_output {
