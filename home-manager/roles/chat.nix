3550d1113f8c3b92ada534de7835db2643ee60a9
Malte Brandy <malte.brandy@maralorn.de>
2022-01-17 14:42:16 +0100

    Enable Element Labs

--- home-manager/roles/chat.nix
+++ home-manager/roles/chat.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
-  home.packages = builtins.attrValues rec {
-    inherit (pkgs) discord signal-desktop tdesktop dino element-desktop;
-    weechat = pkgs.writeShellScriptBin "weechat" "ssh -t hera 'TMUX_TMPDIR=/run/user/1000 tmux -L weechat attach'";
-  };
+  home.packages = builtins.attrValues
+    {
+      inherit (pkgs) discord signal-desktop tdesktop element-desktop;
+      weechat = pkgs.writeShellScriptBin "weechat" "ssh -t hera 'TMUX_TMPDIR=/run/user/1000 tmux -L weechat attach'";
+    };
+  xdg.configFile."Element/config.json".text = builtins.toJSON { showLabsSettings = true; };
 }
