3f3478d66a0f5aad6ae45fdc4a5168c271933277
Malte Brandy <malte.brandy@maralorn.de>
2022-01-18 23:54:46 +0100

    Add Element Theme

--- home-manager/roles/chat.nix
+++ home-manager/roles/chat.nix
@@ -5,5 +5,16 @@
       inherit (pkgs) discord signal-desktop tdesktop element-desktop;
       weechat = pkgs.writeShellScriptBin "weechat" "ssh -t hera 'TMUX_TMPDIR=/run/user/1000 tmux -L weechat attach'";
     };
-  xdg.configFile."Element/config.json".text = builtins.toJSON { showLabsSettings = true; };
+  xdg.configFile."Element/config.json".text = builtins.toJSON {
+    settingsDefaults.custom_themes = [
+      {
+        name = "My Theme";
+        colors = {
+          primary-color = "#000040";
+          accent-color = "#4040ff";
+        };
+      }
+    ];
+    showLabsSettings = true;
+  };
 }
