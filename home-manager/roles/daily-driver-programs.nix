f62c3c37b57903b6083af041a202d4a9eaca03b9
Malte Brandy <malte.brandy@maralorn.de>
2022-01-20 15:23:57 +0100

    Add feed für Serien

--- home-manager/roles/daily-driver-programs.nix
+++ home-manager/roles/daily-driver-programs.nix
@@ -1,4 +1,21 @@
 { pkgs, lib, config, ... }:
+let
+  serien = pkgs.writeShellScript "serien.rss" ''
+    cat <<EOF
+    <?xml version="1.0"?>
+    <rss version="2.0">
+       <channel>
+          <title>Neue Serien Folgen</title>
+          <link>https://www.pogdesign.co.uk/cat</link>
+          <description>Neue Serien Folgen via TV Calendar</description>
+    EOF
+    khal list now 14d -a Serien -df "" -f '<item><title>{title}</title><description>{description}</description><guid>{uid}</guid><pubDate>{start-date}</pubDate></item>'
+    cat <<EOF
+       </channel>
+    </rss>
+    EOF
+  '';
+in
 {
   home = {
     file = {
@@ -21,6 +38,7 @@
           "https://www.youtube.com/feeds/videos.xml?channel_id=UC2C_jShtL725hvbm1arSV9w" # GCP Grey
 
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCpXBGqwsBkpvcYjsJBQ7LEQ" # Critical Role
+          "exec:${serien}"
         ];
       };
     };
