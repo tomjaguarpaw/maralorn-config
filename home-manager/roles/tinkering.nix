c1bb844026a2d4eb257cbd34823a15ce5e67d344
Malte Brandy <malte.brandy@maralorn.de>
2022-02-15 07:30:25 +0100

    Configure vscodium

--- home-manager/roles/tinkering.nix
+++ home-manager/roles/tinkering.nix
@@ -15,5 +15,41 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      eamodio.gitlens
+      editorconfig.editorconfig
+      haskell.haskell
+      bbenoist.nix
+      vscodevim.vim
+      svsool.markdown-memo
+      tabnine.tabnine-vscode
+      gruntfuggly.todo-tree
+      arrterian.nix-env-selector
+      jnoortheen.nix-ide
+      pkief.material-icon-theme
+      yzhang.markdown-all-in-one
+      davidlday.languagetool-linter
+      justusadam.language-haskell
+      naumovs.color-highlight
+      timonwong.shellcheck
+      usernamehw.errorlens
+      valentjn.vscode-ltex
+      xyz.local-history
+      alefragnani.project-manager
+    ];
+    userSettings = {
+      "editor.fontFamily" = "JetBrainsMono Nerd Font, monospace";
+      "files.autoSave" = "onFocusChange";
+      "editor.tabSize" = 3;
+      "editor.fontLigatures" = true;
+      "workbench.colorTheme" = "Default Light+";
+      "gitlens.hovers.currentLine.over" = "line";
+      "workbench.iconTheme" = "material-icon-theme";
+      "update.mode" = "none";
+      "update.showReleaseNotes" = false;
+      "extensions.autoUpdate" = false;
+      "extensions.autoCheckUpdates" = false;
+      "local-history.path" = "~/.volatile/vscode-local-history/";
+    };
   };
 }
--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -9,6 +9,8 @@ in
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
   home-assistant = unstable.home-assistant;
+  vscode-extensions = unstable.vscode-extensions;
+  vscode = unstable.vscode;
   nix-output-monitor = unstable.nix-output-monitor.overrideAttrs (old: {
     src = super.sources.nix-output-monitor;
     buildInputs = old.buildInputs ++ (with unstable.haskellPackages; [ streamly optics generic-optics ]);
