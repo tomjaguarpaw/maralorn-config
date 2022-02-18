4467d759dd99bf6933315d368a2e1539fbcb552c
Malte Brandy <malte.brandy@maralorn.de>
2022-02-18 12:38:18 +0100

    Adapt emanote config

--- home-manager/roles/zettelkasten.nix
+++ home-manager/roles/zettelkasten.nix
@@ -9,7 +9,7 @@ in
     # host = "127.0.0.1"; # default listen address is 127.0.0.1
     # port = 7000;        # default http port is 7000
     notes = [
-      "${config.home.homeDirectory}/git/notes/content" # add as many layers as you like
+      "${config.home.homeDirectory}/git/notes" # add as many layers as you like
     ];
     package = emanote.defaultPackage.${builtins.currentSystem};
   };
