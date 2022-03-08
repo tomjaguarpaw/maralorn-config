5eecae185811518281da59d5206ea055681ef52e
Malte Brandy <malte.brandy@maralorn.de>
2022-03-08 03:48:19 +0100

    Drop mumble overlay

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -2,11 +2,10 @@ self: super: let
   unstable = import super.sources.nixos-unstable {};
 in {
   inherit unstable;
-  inherit (unstable) cachix cabal2nix;
   nix = self.nix_2_4;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
-  inherit (unstable) home-assistant vscode-extensions vscode;
+  inherit (unstable) home-assistant vscode-extensions vscode mumble cachix cabal2nix mumble;
   nix-output-monitor = unstable.nix-output-monitor.overrideAttrs (old: {
     src = super.sources.nix-output-monitor;
     buildInputs = old.buildInputs ++ (with unstable.haskellPackages; [streamly optics generic-optics extra safe MemoTrie]);
