d37ff3743a57fe8114a04791898a0b5e820ff8e6
Malte Brandy <malte.brandy@maralorn.de>
2022-03-08 03:50:14 +0100

    Fix stupid double

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -5,7 +5,7 @@ in {
   nix = self.nix_2_4;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
-  inherit (unstable) home-assistant vscode-extensions vscode mumble cachix cabal2nix mumble;
+  inherit (unstable) home-assistant vscode-extensions vscode mumble cachix cabal2nix;
   nix-output-monitor = unstable.nix-output-monitor.overrideAttrs (old: {
     src = super.sources.nix-output-monitor;
     buildInputs = old.buildInputs ++ (with unstable.haskellPackages; [streamly optics generic-optics extra safe MemoTrie]);
