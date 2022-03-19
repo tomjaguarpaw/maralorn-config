4d39148357185dbb20ab487c03b8c74cda3dd6dc
Malte Brandy <malte.brandy@maralorn.de>
2022-03-19 14:28:08 +0100

    Update 5e tools

--- nixos/roles/5etools-url.nix
+++ nixos/roles/5etools-url.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://github.com/5etools-mirror-1/5etools-mirror-1.github.io/releases/download/v1.151.1/5etools-v1.151.1.zip";
-  sha256 = "03q2y0wi3il9z9kxyd8nmx02lsykwl7ndn96ckf27h4varcixyl9";
+  url = "https://github.com/5etools-mirror-1/5etools-mirror-1.github.io/releases/download/v1.152.0/5etools-v1.152.0.zip";
+  sha256 = "0jk0vc2v7pc4ws30r9phv9qwyddkn3mwiaxqj1fwq3lin4qwrj2h";
 }
