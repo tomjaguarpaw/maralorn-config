27ce76dfe0ecd19020b297b4b4a3f31c09a513fe
Malte Brandy <malte.brandy@maralorn.de>
2022-03-21 20:42:36 +0100

    Bump nix

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -2,10 +2,9 @@ self: super: let
   unstable = import super.sources.nixos-unstable {};
 in {
   inherit unstable;
-  nix = self.nix_2_4;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
-  inherit (unstable) home-assistant vscode-extensions vscode mumble cachix cabal2nix alejandra;
+  inherit (unstable) nix home-assistant vscode-extensions vscodium mumble cachix cabal2nix alejandra;
   nix-output-monitor = super.haskell.lib.overrideCabal unstable.nix-output-monitor {
     src = super.sources.nix-output-monitor;
   };
