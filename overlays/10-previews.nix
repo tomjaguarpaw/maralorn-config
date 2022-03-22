28e623587d8f42f2c773de02b74d1cbeaff476c1
Malte Brandy <malte.brandy@maralorn.de>
2022-03-22 12:25:59 +0100

    Don’ override nom src

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -4,8 +4,5 @@ in {
   inherit unstable;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
-  inherit (unstable) nix home-assistant vscode-extensions vscodium mumble cachix cabal2nix alejandra;
-  nix-output-monitor = super.haskell.lib.overrideCabal unstable.nix-output-monitor {
-    src = super.sources.nix-output-monitor;
-  };
+  inherit (unstable) nix home-assistant vscode-extensions vscodium mumble cachix cabal2nix alejandra nix-output-monitor;
 }
