7837d209800869a191a72c4c17b0b0f0fa83d73a
Malte Brandy <malte.brandy@maralorn.de>
2022-03-24 01:07:33 +0100

    Bump 5etools

--- nixos/roles/5etools-url.nix
+++ nixos/roles/5etools-url.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://github.com/5etools-mirror-1/5etools-mirror-1.github.io/releases/download/v1.152.0/5etools-v1.152.0.zip";
-  sha256 = "0jk0vc2v7pc4ws30r9phv9qwyddkn3mwiaxqj1fwq3lin4qwrj2h";
+  url = "https://github.com/5etools-mirror-1/5etools-mirror-1.github.io/releases/download/v1.152.1/5etools-v1.152.1.zip";
+  sha256 = "138xh4nbnpjfqrp1257yfv86gi8i8sm5vrq9lffxpncl96ld108n";
 }
