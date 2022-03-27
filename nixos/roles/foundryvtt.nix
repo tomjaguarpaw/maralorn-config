d9a9c7c50ff91d9d2d2310965568337f81656158
Malte Brandy <malte.brandy@maralorn.de>
2022-03-27 21:05:20 +0200

    Better logins

--- nixos/roles/foundryvtt.nix
+++ nixos/roles/foundryvtt.nix
@@ -52,23 +52,30 @@ in {
             forceSSL = true;
             enableACME = true;
             locations = {
-              #"/rules/" = {
-              #  alias = "${pkgs.fetchzip (import ./5etools-url.nix // {stripRoot = false;})}/";
-              #  index = "index.html";
-              #};
               "/" = {
                 proxyPass = "http://[::1]:${toString config.port}";
                 proxyWebsockets = true;
-                extraConfig =
-                  #if ($request_uri ~ ^/rules$) {
-                  #   return 301 /rules/;
-                  #}
-                  ''
-                    proxy_set_header Host $host;
-                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                    proxy_set_header X-Forwarded-Proto $scheme;
-                  '';
+                extraConfig = ''
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                  if ($query_string ~ "pw=([A-Za-z]*)") {
+                     add_header Set-Cookie "password=$1; path=/; Expires=${toString (365 * 24 * 60 * 60)}; Secure";
+                     return 303 /;
+                  }
+                  if ($http_cookie !~ "password=${pkgs.privateValue "" "foundry-pw"}") {
+                     return 303 /logout;
+                  }
+                '';
               };
+              "/login".extraConfig = ''
+                add_header Content-Type text/html;
+                return 200 '<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body><form style="text-align: center; margin: 250px auto; width: 500px;" action="/" method="get"><label for="pw">Passwort: </label><input type="password" name="pw"><input type="submit" value="login"></form></html></body>';
+              '';
+              "/logout".extraConfig = ''
+                add_header Set-Cookie 'password=""; Max-Age=0';
+                return 303 /login;
+              '';
             };
           };
         };
--- nixos/roles/mathechor.de.nix
+++ nixos/roles/mathechor.de.nix
@@ -22,12 +22,28 @@
       virtualHosts."intern.mathechor.de" = {
         forceSSL = true;
         enableACME = true;
-        basicAuthFile = pkgs.privateFile "basic-auth/mathechor.de";
         locations = {
           "/" = {
             root = "/var/www/mathechor/intern";
             index = "index.html";
+            extraConfig = ''
+              if ($query_string ~ "pw=([A-Za-z]*)") {
+                 add_header Set-Cookie "password=$1; path=/; Expires=${toString (365 * 24 * 60 * 60)}; Secure";
+                 return 303 /;
+              }
+              if ($http_cookie !~ "password=${pkgs.privateValue "" "mathechor.de-pw"}") {
+                 return 303 /logout;
+              }
+            '';
           };
+          "/login".extraConfig = ''
+            add_header Content-Type text/html;
+            return 200 '<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body><form style="text-align: center; margin: 250px auto; width: 500px;" action="/" method="get"><label for="pw">Passwort: </label><input type="password" name="pw"><input type="submit" value="login"></form></html></body>';
+          '';
+          "/logout".extraConfig = ''
+            add_header Set-Cookie 'password=""; Max-Age=0';
+            return 303 /login;
+          '';
           "/mathechor.ics" = {
             proxyPass = pkgs.privateValue "" "mathechor-ics";
             extraConfig = ''
