bc20d6e89ecd85c7efe019d411a297388d63dc49
Malte Brandy <malte.brandy@maralorn.de>
2022-04-09 19:05:25 +0200

    Remove unnecessary unbound

--- nixos/machines/apollo/configuration.nix
+++ nixos/machines/apollo/configuration.nix
@@ -84,7 +84,6 @@ in {
       ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
       ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
     '';
-    unbound.enable = true;
     fstrim.enable = true;
     snapper = {
       configs.home = {
