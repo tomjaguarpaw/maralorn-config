3cd9926d31cece16b16660bb7a1a6922af789a33
Malte Brandy <malte.brandy@maralorn.de>
2022-04-18 13:02:04 +0200

    remove 5etools

--- nixos/roles/5etools-url.nix
+++
@@ -1,4 +0,0 @@
-{
-  url = "https://github.com/5etools-mirror-1/5etools-mirror-1.github.io/releases/download/v1.152.4/5etools-v1.152.4.zip";
-  sha256 = "0pv48aqshslx6325mpj6fd5zpns5v5xg6ij8h4z80c5mfnfd7c09";
-}
