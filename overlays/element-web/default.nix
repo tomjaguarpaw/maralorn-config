1ec470cd49a45e8c36aba0589c8864445a8c183e
Malte Brandy <malte.brandy@maralorn.de>
2022-05-21 20:31:29 +0200

    override element message sound

--- overlays/element-web/default.nix
+++ overlays/element-web/default.nix
@@ -1,8 +1,14 @@
-final: prev: {
+final: prev: let
+  chime = final.fetchurl {
+    url = "https://www.mediacollege.com/downloads/sound-effects/star-trek/tng/tng-doorbell.ogg";
+    sha256 = "sha256-tpDJ8lnmSwG5Puq6dJhiW3w+Id0EiPoqlim6N3BPz7c=";
+  };
+in {
   element-web = prev.element-web.overrideAttrs (old: {
     preInstall = ''
       bundle=$(find . -name 'bundle.css')
       cat ${./user.css} >> $bundle
+      cp ${chime} media/message.ogg
     '';
   });
 }
