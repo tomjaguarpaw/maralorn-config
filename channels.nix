787e6b1487b44500ee1a97975757c504ccd282e7
Malte Brandy <malte.brandy@maralorn.de>
2022-05-31 23:51:15 +0200

    update apollo to 22.05

--- channels.nix
+++ channels.nix
@@ -3,9 +3,13 @@ let
     nixpkgs-channel = "nixos-21.11";
     home-manager-channel = "home-manager-21.11";
   };
+  nixos-22-05 = {
+    nixpkgs-channel = "nixos-22.05";
+    home-manager-channel = "home-manager-22.05";
+  };
 in rec {
   hera = nixos-21-11;
-  apollo = nixos-21-11;
+  apollo = nixos-22-05;
   zeus = nixos-21-11;
   fluffy = nixos-21-11;
   chor-cloud = hera;
