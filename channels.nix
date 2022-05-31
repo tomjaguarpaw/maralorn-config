3814a28f79778135fd23fc6a0e09de8c827c43ce
Malte Brandy <malte.brandy@maralorn.de>
2022-06-01 01:51:30 +0200

    fake channels

--- channels.nix
+++ channels.nix
@@ -1,16 +1,12 @@
 let
-  nixos-21-11 = {
-    nixpkgs-channel = "nixos-21.11";
-    home-manager-channel = "home-manager-21.11";
-  };
-  nixos-22-05 = {
-    nixpkgs-channel = "nixos-22.05";
-    home-manager-channel = "home-manager-22.05";
+  nixos-stable = {
+    nixpkgs-channel = "nixos-stable";
+    home-manager-channel = "home-manager-stable";
   };
 in rec {
-  hera = nixos-21-11;
-  apollo = nixos-22-05;
-  zeus = nixos-22-05;
-  fluffy = nixos-21-11;
+  hera = nixos-stable;
+  apollo = nixos-stable;
+  zeus = nixos-stable;
+  fluffy = nixos-stable;
   chor-cloud = hera;
 }
