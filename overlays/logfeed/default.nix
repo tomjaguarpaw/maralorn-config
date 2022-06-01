32b4e2970c96f49e3e60032aae7e7387ce6a8225
Malte Brandy <malte.brandy@maralorn.de>
2022-06-02 01:37:53 +0200

    fix purebred-email

--- overlays/logfeed/default.nix
+++ overlays/logfeed/default.nix
@@ -2,6 +2,6 @@ final: prev: {
   logfeed =
     prev.haskellPackages.callCabal2nix "logfeed" prev.sources.logfeed
     {
-      purebred-email = prev.haskell.lib.unmarkBroken prev.haskellPackages.purebred-email;
+      purebred-email = prev.haskell.lib.dontCheck (prev.haskell.lib.doJailbreak (prev.haskell.lib.unmarkBroken prev.haskellPackages.purebred-email));
     };
 }
