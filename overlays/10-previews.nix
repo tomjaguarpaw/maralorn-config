43472f5e59bf15d23f943cce6012f102ee107ade
Malte Brandy <malte.brandy@maralorn.de>
2022-06-07 12:12:48 +0200

    fix nom reference

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -1,7 +1,7 @@
 self: super: let
   unstable = import super.sources.nixos-unstable {};
   nom_commit = (builtins.fromJSON (builtins.readFile ../nix/sources.json)).nix-output-monitor.rev;
-  nom = builtins.getFlake "git+ssh://git@hera.m-0.eu/nix-output-monitor?rev=${nom_commit}";
+  nom = builtins.getFlake "git+ssh://git@hera.m-0.eu/nix-output-monitor?rev=${nom_commit}&ref=main";
 in {
   inherit unstable;
   unstableHaskellPackages = unstable.haskellPackages;
