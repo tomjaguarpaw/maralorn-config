4b3b0328385fe7e70b07105fc27168264417dd7c
Malte Brandy <malte.brandy@maralorn.de>
2022-06-07 12:06:31 +0200

    update nom

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -1,8 +1,11 @@
 self: super: let
   unstable = import super.sources.nixos-unstable {};
+  nom_commit = (builtins.fromJSON (builtins.readFile ../nix/sources.json)).nix-output-monitor.rev;
+  nom = builtins.getFlake "git+ssh://git@hera.m-0.eu/nix-output-monitor?rev=${nom_commit}";
 in {
   inherit unstable;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
-  inherit (unstable) nix home-assistant vscode-extensions vscodium mumble cachix cabal2nix alejandra nix-output-monitor;
+  inherit (unstable) nix home-assistant vscode-extensions vscodium mumble cachix cabal2nix alejandra;
+  nix-output-monitor = nom.packages.x86_64-linux.default;
 }
