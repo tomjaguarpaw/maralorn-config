2acd5a6d4f566561aa2b18d4048517845208a4c9
maralorn <mail@maralorn.de>
2022-09-09 18:24:53 +0200

    Change element notification sound

--- overlays/element-web/default.nix
+++ overlays/element-web/default.nix
@@ -1,16 +1,11 @@
-final: prev: let
-  chime = final.fetchurl {
-    url = "https://www.mediacollege.com/downloads/sound-effects/star-trek/tng/tng-doorbell.ogg";
-    sha256 = "sha256-tpDJ8lnmSwG5Puq6dJhiW3w+Id0EiPoqlim6N3BPz7c=";
-  };
-in {
+final: prev: {
   element-web = prev.element-web.overrideAttrs (old: {
     preInstall = ''
       bundlecss=$(find . -name 'bundle.css')
       cat ${./user.css} >> $bundlecss
       bundlejs=$(find . -name 'vendors~init.js')
       sed -i 's/return n.room.roomId===e||n.isUnread}/return n.room.roomId===e||n.hasUnreadCount}/' $bundlejs
-      cp ${chime} media/message.ogg
+      cp ${./orville_communicator.opus} media/message.ogg
     '';
   });
 }
