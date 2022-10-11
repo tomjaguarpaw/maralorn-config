cab3e51e1c9b14f1c9bd982952b40c75d13faa87
maralorn <mail@maralorn.de>
2022-10-11 19:57:23 +0200

    Remve a preview

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -2,12 +2,10 @@ self: super: let
   unstable = import super.sources.nixos-unstable {};
   nom_commit = (builtins.fromJSON (builtins.readFile ../nix/sources.json)).nix-output-monitor.rev;
   nom = builtins.getFlake "git+ssh://git@hera.m-0.eu/nix-output-monitor?rev=${nom_commit}&ref=main";
-  chrysalis-pkgs = builtins.getFlake "github:maralorn/nixpkgs/5169d354b34311aac607dbd8c8fd84962e2b1d75";
 in {
   inherit unstable;
   unstableHaskellPackages = unstable.haskellPackages;
   unstableGhc = unstable.ghc;
-  inherit (unstable) nix home-assistant vscode-extensions vscodium cachix cabal2nix;
+  inherit (unstable) nix home-assistant vscode-extensions vscodium cachix cabal2nix chrysalis;
   nix-output-monitor = nom.packages.x86_64-linux.default;
-  inherit (chrysalis-pkgs.legacyPackages.x86_64-linux) chrysalis;
 }
