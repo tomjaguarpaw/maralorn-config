2dcc02818433b49016db54902a0238e0b65c97a9
maralorn <mail@maralorn.de>
2022-11-10 14:47:04 +0100

    Better markup for unknown command

--- exe/Main.hs
+++ exe/Main.hs
@@ -538,7 +538,7 @@ resultHandler syncResult@Matrix.SyncResult{Matrix.srNextBatch, Matrix.srRooms} =
           , m "I have been programmed and am being hosted by " <> mention "@maralorn:maralorn.de" <> m ". Feel free to reach out to him, if you have any problems or suggestions."
           , m "My code is written in Haskell, is open source under the AGPL license and can be found at " <> link "https://git.maralorn.de/nixpkgs-bot" "git.maralorn.de/nixpkgs-bot" <> m "."
           ]
-    MkCommand{command, author} -> sendMessageToUser author (unlinesMsg [m $ "Sorry, I don‘t know what you want from me, when your command starts with: " <> command, m "I‘ll tell you all commands I know, when you use the " <> codeHTML "help" <> m " command."])
+    MkCommand{command, author} -> sendMessageToUser author (unlinesMsg [m "Sorry, I don‘t know what you want from me, when your command starts with: " <> codeHTML command, m "I‘ll tell you all commands I know, when you use the " <> codeHTML "help" <> m " command."])
   last_watch_ref <- getEnv lastWatch
   last_watch <- readIORef last_watch_ref
   now <- liftIO $ Clock.getTime Clock.Monotonic
