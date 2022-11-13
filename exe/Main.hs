2803cf2ba341f521985fe01ca14134f057151c93
maralorn <mail@maralorn.de>
2022-11-13 17:51:06 +0100

    Add temporary todolist

--- exe/Main.hs
+++ exe/Main.hs
@@ -31,6 +31,12 @@ import qualified System.Environment as System
 import qualified System.Process.Typed as Process
 import qualified System.Random as Random
 
+-- TODO:
+--  help on invite
+--  retry invites
+--  subscribe to user
+--  use fragments
+
 data Repo = MkRepo
   { localPath :: FilePath
   , owner :: Text
