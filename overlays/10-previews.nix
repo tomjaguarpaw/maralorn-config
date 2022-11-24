6eee11b0eaec42475b60325c8094ee9bb799af64
maralorn <mail@maralorn.de>
2022-11-24 03:41:00 +0100

    Preview nom again

--- overlays/10-previews.nix
+++ overlays/10-previews.nix
@@ -1,6 +1,8 @@
 self: super: let
   unstable = import super.sources.nixos-unstable {};
   bot_commit = (builtins.fromJSON (builtins.readFile ../nix/sources.json)).nixpkgs-bot.rev;
+  nom_commit = (builtins.fromJSON (builtins.readFile ../nix/sources.json)).nix-output-monitor.rev;
+  nom = builtins.getFlake "git+ssh://git@hera.m-0.eu/nix-output-monitor?rev=${nom_commit}&ref=main";
   bot = builtins.getFlake "git+ssh://git@hera.m-0.eu/nixpkgs-bot?rev=${bot_commit}&ref=main";
 in {
   inherit unstable;
@@ -19,8 +21,8 @@ in {
     taplo
     tut
     emanote
-    nix-output-monitor
     helix
     ;
   nixpkgs-bot = bot.packages.x86_64-linux.default;
+  nix-output-monitor = nom.packages.x86_64-linux.default;
 }
