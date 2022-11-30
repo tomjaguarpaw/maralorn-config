e0cb5982296affd62b4ecb081afd803c9105b55d
maralorn <mail@maralorn.de>
2022-12-01 00:55:38 +0100

    firefox is on wayland anyway

--- home-manager/roles/firefox.nix
+++ home-manager/roles/firefox.nix
@@ -25,8 +25,6 @@ startpage: {pkgs, ...}: {
     enable = true;
   };
   home.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = 1; # So that firefox uses wayland.
-
     # So that electron can open firefox links. See
     # Issue: https://github.com/electron/electron/issues/28436
     # Solution from: https://wiki.archlinux.org/title/Firefox#Applications_on_Wayland_can_not_launch_Firefox
