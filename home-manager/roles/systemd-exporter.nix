c728f7eb2df1eeaaad29939423656fffe584416d
maralorn <mail@maralorn.de>
2022-12-02 03:09:23 +0100

    Fix systemd

--- home-manager/roles/systemd-exporter.nix
+++ home-manager/roles/systemd-exporter.nix
@@ -3,7 +3,7 @@
     services.prometheus-systemd-exporter = {
       Unit.Description = "Prometheus systemd exporter";
       Service = {
-        ExecStart = "${pkgs.prometheus-systemd-exporter}/bin/systemd_exporter --collector.user";
+        ExecStart = "${pkgs.prometheus-systemd-exporter}/bin/systemd_exporter --systemd.collector.user";
       };
       Install.WantedBy = ["default.target"];
     };
