1b7fa65915a26c9695996d2c0bfb41050076eed1
maralorn <mail@maralorn.de>
2023-01-14 01:43:56 +0100

    Simplify logfeed config

--- overlays/logfeed/default.nix
+++ overlays/logfeed/default.nix
@@ -1,7 +1,3 @@
 final: prev: {
-  logfeed =
-    prev.haskellPackages.callCabal2nix "logfeed" prev.sources.logfeed
-    {
-      purebred-email = prev.haskell.lib.dontCheck (prev.haskell.lib.doJailbreak (prev.haskell.lib.unmarkBroken prev.haskellPackages.purebred-email));
-    };
+  logfeed = prev.haskellPackages.callCabal2nix "logfeed" prev.sources.logfeed {};
 }
