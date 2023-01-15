c5c4d4aba7583036a4caf9f87cc1f1442186a243
maralorn <mail@maralorn.de>
2023-01-16 00:30:25 +0100

    Flakify

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "maralorns configuration";
+
+  inputs = {
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.pre-commit-hooks-nix.flakeModule
+      ];
+      systems = ["x86_64-linux"];
+      perSystem = {
+        self',
+        pkgs,
+        config,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          shellHook = config.pre-commit.installationScript;
+        };
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = {
+            hlint.enable = true;
+            alejandra.enable = true;
+            nix-linter.enable = false; # Too many false positives for now
+            statix.enable = true;
+            fourmolu.enable = true;
+            shellcheck.enable = true;
+          };
+        };
+      };
+    };
+}
--- shell.nix
+++
@@ -1,5 +0,0 @@
-(import (import ./nix/sources.nix).nixos-stable {}).mkShell {
-  shellHook = ''
-    ${(import ./test.nix).pre-commit-check.shellHook}
-  '';
-}
