e96922aefd3d95f969fed0f1bf1a65ab2c7f4559
maralorn <mail@maralorn.de>
2023-01-16 00:56:24 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,13 +3,10 @@
 
   inputs = {
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    nixos-unstable.follows = "nixpkgs";
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-22.11";
     pre-commit-hooks-nix = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixos-stable";
       };
     };
   };
