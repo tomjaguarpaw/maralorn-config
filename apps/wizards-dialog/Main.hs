9b4c7e1d28eab62169400ee52c8fe09fd079a801
maralorn <mail@maralorn.de>
2023-01-16 01:11:54 +0100

    Cleanup wizards-dialog

--- apps/wizards-dialog/Main.hs
+++ apps/wizards-dialog/Main.hs
@@ -22,27 +22,27 @@ import Dialog (Menu (..), MenuEntry (Option, SubMenu), menu, runClearingHaskelin
 data Command = Run Text | Fork Text deriving (Show)
 
 instance FromJSON (Menu Command) where
-   parseJSON = parseMenu "Hotkeys"
-     where
-      parseList name = (fmap (Menu name) .) $
-         mapM $ \(Key.toText -> key, val) ->
-            case val of
-               String cmd -> pure $ Dialog.Option key (text2cmd cmd)
-               innerObj -> Dialog.SubMenu <$> parseMenu key innerObj
-      unpackMenu (Array arr) = join <$> mapM unpackMenu (reverse $ Foldable.toList arr)
-      unpackMenu value = withObject "mapping to menu entries" (pure . reverse . Aeson.toList) value
-      parseMenu name = parseList name <=< unpackMenu
-      text2cmd t = if Text.isPrefixOf "fork " t then Fork (Text.drop 5 t) else Run t
+  parseJSON = parseMenu "Hotkeys"
+   where
+    parseList name = (fmap (Menu name) .) $
+      mapM $ \(Key.toText -> key, val) ->
+        case val of
+          String cmd -> pure $ Dialog.Option key (text2cmd cmd)
+          innerObj -> Dialog.SubMenu <$> parseMenu key innerObj
+    unpackMenu (Array arr) = join <$> mapM unpackMenu (reverse $ Foldable.toList arr)
+    unpackMenu value = withObject "mapping to menu entries" (pure . reverse . Aeson.toList) value
+    parseMenu name = parseList name <=< unpackMenu
+    text2cmd t = if Text.isPrefixOf "fork " t then Fork (Text.drop 5 t) else Run t
 
 main :: IO ()
 main = do
-   [menuFileName] <- getArgs
-   menuCommand <- decodeFileEither menuFileName
-   case menuCommand of
-      Left err -> print err
-      Right a -> do
-         cmd <- runClearingHaskeline $ menu Nothing a
-         maybe pass runCommand cmd
+  [menuFileName] <- getArgs
+  menuCommand <- decodeFileEither menuFileName
+  case menuCommand of
+    Left err -> print err
+    Right a -> do
+      cmd <- runClearingHaskeline $ menu Nothing a
+      maybe pass runCommand cmd
 
 runCommand :: Command -> IO ()
 runCommand (Run cmd) = callCommand $ into cmd
