2fe6825d1d37a424967cd240ccf61eecae059cd5
maralorn <mail@maralorn.de>
2023-01-30 02:24:15 +0100

    Deploy miniflux config

--- nixos/roles/miniflux.nix
+++ nixos/roles/miniflux.nix
@@ -7,10 +7,14 @@
     enable = true;
     adminCredentialsFile = pkgs.privatePath "miniflux-admin-credentials";
     config = {
-      DEBUG = "1";
       SCHEDULER_SERVICE = "0";
       BATCH_SIZE = "1000";
       LISTEN_ADDR = "[${config.m-0.hosts.vpn.hera}]:8100";
     };
+  };
+  systemd.services."refresh-miniflux" = {
+    script = "${pkgs.curl}/bin/curl -X PUT -H \"X-AUTH-TOKEN: $(cat $CREDENTIALS_DIRECTORY/miniflux_token)\" hera.vpn.m-0.eu:8100/v1/feeds/refresh";
+    startAt = "20:00:00";
+    serviceConfig.LoadCredential = ["miniflux_token:${pkgs.privatePath "miniflux-refresh-token"}"];
   };
 }
