2bfabe89f2d1e06c52a7baa5db6f6fe35992652b
maralorn <mail@maralorn.de>
2023-03-03 21:39:34 +0100

    Fix tailscale connection

--- nixos/roles/vpn.nix
+++ nixos/roles/vpn.nix
@@ -1,4 +1,9 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   services.tailscale.enable = true;
   networking = {
     firewall = {
@@ -9,5 +14,34 @@
       checkReversePath = "loose";
       allowedUDPPorts = [config.services.tailscale.port];
     };
+  };
+
+  # 2023-03-03: https://tailscale.com/blog/nixos-minecraft/
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = ["network-pre.target" "tailscale.service"];
+    wants = ["network-pre.target" "tailscale.service"];
+    wantedBy = ["multi-user.target"];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${lib.getExe pkgs.tailscale} status -json | ${lib.getExe pkgs.jq} -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${lib.getExe pkgs.tailscale} up -authkey file:${config.age.secrets."tailscale-preauthkey".path} -login-server https://vpn.m-0.eu
+    '';
   };
 }
