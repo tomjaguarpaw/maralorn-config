8a2bbe3362089da547339331a6016146f19eb139
maralorn <mail@maralorn.de>
2023-03-06 15:45:26 +0100

    Nice nix-version

--- nixos/roles/default.nix
+++ nixos/roles/default.nix
@@ -7,6 +7,8 @@
   inherit (config.m-0) hosts;
   inherit (config.networking) hostName;
   inherit (pkgs.flake-inputs.self) sourceInfo;
+  s = builtins.substring;
+  formatDate = date: "${s 0 4 date}-${s 4 2 date}-${s 6 2 date}-${s 8 2 date}:${s 10 2 date}";
 in {
   imports = [
     ../../common
@@ -16,8 +18,8 @@ in {
     configurationRevision = sourceInfo.rev or null;
     nixos.label =
       if sourceInfo ? shortRev
-      then "${sourceInfo.lastModifiedDate}.${sourceInfo.shortRev}"
-      else "${sourceInfo.lastModifiedDate}.dirty";
+      then "${formatDate sourceInfo.lastModifiedDate}-${sourceInfo.shortRev}"
+      else "${formatDate sourceInfo.lastModifiedDate}-dirty";
     systemBuilderCommands = lib.mkIf (sourceInfo ? rev) ''
       echo ${sourceInfo.rev} > $out/config-commit
     '';
