e8fe35a44f29f0d0b10947a01e899f38a8815e5f
maralorn <mail@maralorn.de>
2023-03-06 17:26:46 +0100

    Write config to commit

--- home-manager/flake-module.nix
+++ home-manager/flake-module.nix
@@ -29,10 +29,19 @@
       ];
     });
     buildModesForHost = host: modes:
-      pkgs.recursiveLinkFarm "${host}-modes"
-      (lib.mapAttrs
-        (_: config: (buildHomeManager config).activationPackage)
-        modes);
+      (pkgs.recursiveLinkFarm "${host}-modes"
+        (lib.mapAttrs
+          (_: config: (buildHomeManager config).activationPackage)
+          modes))
+      .overrideAttrs (old: {
+        buildCommand =
+          if inputs.self.sourceInfo ? rev
+          then ''
+            ${old.buildCommand}
+            echo ${inputs.self.sourceInfo.rev} > $out/config-commit;
+          ''
+          else old.buildCommand;
+      });
   in {
     homeConfigurations = lib.mapAttrs (_: buildHomeManager) (flattenAttrs machines);
     homeModes = lib.mapAttrs buildModesForHost {inherit (machines) zeus apollo;};
