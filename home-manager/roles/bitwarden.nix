3cde3709da1a140748d96549e9e426d4a853cc76
maralorn <mail@maralorn.de>
2023-03-09 14:08:05 +0100

    Add rbw-totp script

--- home-manager/roles/bitwarden.nix
+++ home-manager/roles/bitwarden.nix
@@ -1,10 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  package = pkgs.rbw.override {
+    withFzf = true;
+    withPass = true;
+  };
+  rbw-totp-fzf = pkgs.runCommand "rbw-totp-fzf" {} ''
+    mkdir -p $out/bin
+    cp ${package}/bin/rbw-fzf $out/bin/rbw-totp-fzf
+    ${lib.getExe pkgs.sd} "rbw get" "rbw code" $out/bin/rbw-totp-fzf
+  '';
+in {
+  home.packages = [rbw-totp-fzf];
   programs.rbw = {
     enable = true;
-    package = pkgs.rbw.override {
-      withFzf = true;
-      withPass = true;
-    };
+    inherit package;
     settings = {
       email = "bitwarden@maralorn.de";
       base_url = "https://bitwarden.darmstadt.ccc.de";
