162b52f5fa31a9c467162cbe0bd892dabf6c5fa5
maralorn <mail@maralorn.de>
2023-03-12 01:47:00 +0100

    Improve syncthing config

--- common/common.nix
+++ common/common.nix
@@ -5,14 +5,11 @@
         path = "${path}/${name}";
         devices = hosts;
       };
-      devices = {
+      devices = pkgs.lib.mapAttrs (name: conf: conf // {addresses = "tcp6://${name}.vpn.m-0.eu";}) {
         apollo.id = "BOTTTGS-QQUHWAK-IFBT3T2-HGHHUZ7-QHRZXC7-JC42VT7-67ZOJBE-WHDWEQX";
         zeus.id = "5BUZIS5-ESTYAJO-IQQD7EA-O3VGONJ-E74OHUJ-ZSLF4JK-6HS3UHG-4CQ5OAO";
         pegasus.id = "BISYPNZ-54VKBKS-LBND4AS-JNWVOW7-BTW2UMV-QHYM5TZ-GE3AK3E-PGSXPQE";
-        hera = {
-          addresses = ["tcp6://hera.m-0.eu"];
-          id = "TJHVUM6-RTB6V3D-JF4GIB2-TVDF2ST-5MTN6N2-ZDIWGF7-XZUCCFG-EQG5WA6";
-        };
+        hera.id = "TJHVUM6-RTB6V3D-JF4GIB2-TVDF2ST-5MTN6N2-ZDIWGF7-XZUCCFG-EQG5WA6";
       };
     in {
       devices = pkgs.lib.getAttrs hosts devices;
