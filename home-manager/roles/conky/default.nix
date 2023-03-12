43fe2423b59322864538ed6ca4f10d3efdb68a4c
maralorn <mail@maralorn.de>
2023-03-12 13:03:36 +0100

    Conky try harder!

--- home-manager/roles/conky/default.nix
+++ home-manager/roles/conky/default.nix
@@ -8,6 +8,7 @@
     Service = {
       ExecStart = "${lib.getExe pkgs.conky} -c ${./conky.conf}";
       Restart = "always";
+      RestartSec = "10s";
     };
     Install.WantedBy = ["default.target"];
   };
