37b05b30eb27c0cf45cb7c02c13ff0f41eeb8001
maralorn <mail@maralorn.de>
2023-03-13 05:43:53 +0100

    Feeds

--- home-manager/roles/leisure.nix
+++ home-manager/roles/leisure.nix
@@ -37,11 +37,17 @@
       feeds = [
         # Haskell
         "https://www.youtube.com/feeds/videos.xml?channel_id=UCI1Z201n-8OelkSg0DVOsng" # Tweag
+
         "https://www.youtube.com/feeds/videos.xml?channel_id=UCxVE_2I_fsOe3Mgn_QlXqbQ" # Nomeata
 
-        "http://www.zdf.de/rss/podcast/video/zdf/comedy/die-anstalt"
+        "https://www.zdf.de/rss/podcast/video/zdf/comedy/die-anstalt"
+
+        "https://www.zdf.de/rss/podcast/video/zdf/comedy/neo-magazin-mit-jan-boehmermann"
+
+        "https://www.zdf.de/rss/zdf/comedy/zdf-magazin-royale"
 
         "https://www.zdf.de/rss/zdf/show/mai-think-x-die-show"
+
         "https://www.youtube.com/feeds/videos.xml?channel_id=UCyHDQ5C6z1NDmJ4g6SerW8g" # Mailab
 
         "https://www.youtube.com/feeds/videos.xml?channel_id=UC3vIimi9q4AT8EgxYp_dWIw" # NixOS
