9b25c3dba1a9a1d830ff2b71db1b37ce7389b5f4
maralorn <mail@maralorn.de>
2023-03-22 01:06:16 +0100

    New newsboat conf

--- home-manager/roles/leisure.nix
+++ home-manager/roles/leisure.nix
@@ -4,76 +4,34 @@
   config,
   ...
 }: let
-  serien = pkgs.writeShellScript "serien.rss" ''
-    cat <<EOF
-    <?xml version="1.0"?>
-    <rss version="2.0">
-       <channel>
-          <title>Neue Serien Folgen</title>
-          <link>https://www.pogdesign.co.uk/cat</link>
-          <description>Neue Serien Folgen via TV Calendar</description>
-    EOF
-    ${pkgs.khal}/bin/khal list now 14d -a Serien -df "" -f '<item><title>{title}</title><description>{description}</description><guid>{uid}</guid><pubDate>{start-date}</pubDate></item>'
-    cat <<EOF
-       </channel>
-    </rss>
-    EOF
-  '';
+  rbw = config.programs.rbw.package;
   commands = builtins.mapAttrs (name: {
     config ? "",
-    feeds ? [],
+    user,
   }: let
-    configFile = builtins.toFile "${name}-config" ''
+    configFile = pkgs.writeText "${name}-config" ''
       show-read-feeds no
       show-read-articles no
       datetime-format "%Y-%m-%d"
+      urls-source "miniflux"
+      miniflux-url "http://rss.maralorn.de/"
+      miniflux-login "${user}"
+      miniflux-passwordeval "${lib.getExe rbw} get rss.maralorn.de ${user}"
       ${config}
     '';
-    urlFile = pkgs.writeText "${name}-urls" (lib.concatStringsSep "\n" feeds);
   in
-    pkgs.writeShellScriptBin name "${pkgs.newsboat}/bin/newsboat -r -C ${configFile} -u ${urlFile} -c ~/.local/share/newsboat/${name}-cache.db \"$@\"") {
-    watchfeeds = {
-      config = ''browser "mpv %u"'';
-      feeds = [
-        # Haskell
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCI1Z201n-8OelkSg0DVOsng" # Tweag
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCxVE_2I_fsOe3Mgn_QlXqbQ" # Nomeata
-
-        "https://www.zdf.de/rss/podcast/video/zdf/comedy/die-anstalt"
-
-        "https://www.zdf.de/rss/podcast/video/zdf/comedy/neo-magazin-mit-jan-boehmermann"
-
-        "https://www.zdf.de/rss/zdf/comedy/zdf-magazin-royale"
-
-        "https://www.zdf.de/rss/zdf/show/mai-think-x-die-show"
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCyHDQ5C6z1NDmJ4g6SerW8g" # Mailab
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UC3vIimi9q4AT8EgxYp_dWIw" # NixOS
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UC2C_jShtL725hvbm1arSV9w" # GCP Grey
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCpXBGqwsBkpvcYjsJBQ7LEQ" # Critical Role
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UC2PA-AKmVpU6NKCGtZq_rKQ" # Philosophy Tube
-
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCsXVk37bltHxD1rDPwtNM8Q" # Kurzgesagt
-
-        "exec:${serien}"
-      ];
+    pkgs.writeShellScriptBin name "${lib.getExe pkgs.newsboat} -r -C ${configFile} -c ~/.local/share/newsboat/cache.db \"$@\"") {
+    news = {
+      user = "maralorn";
     };
     software-updates = {
-      feeds =
-        [
-          "http://packdeps.haskellers.com/feed/maralorn"
-          "https://repology.org/maintainer/mail%40maralorn.de/feed-for-repo/nix_unstable/atom"
-        ]
-        # GitHub releases of things I need to manually update
-        ++ map (name: "https://github.com/${name}/releases.atom") [
-          "devture/email2matrix"
-          "keyboardio/Chrysalis"
-        ];
+      user = "maralorn-softwareupdates";
+    };
+    watchfeeds = {
+      user = "maralorn-watchfeeds";
+      config = ''
+        browser "mpv %u"
+      '';
     };
   };
 in {
