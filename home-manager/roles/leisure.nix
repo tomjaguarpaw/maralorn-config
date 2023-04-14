d9d9551665fa30ed5b1eb9e199ca92bd126d4d3b
maralorn <mail@maralorn.de>
2023-04-14 13:14:19 +0200

    watch-and-download script

--- home-manager/roles/leisure.nix
+++ home-manager/roles/leisure.nix
@@ -5,35 +5,52 @@
   ...
 }: let
   rbw = config.programs.rbw.package;
-  commands = builtins.mapAttrs (name: {
-    config ? "",
-    user,
-  }: let
-    configFile = pkgs.writeText "${name}-newsboat-config" ''
-      show-read-feeds no
-      show-read-articles no
-      datetime-format "%Y-%m-%d"
-      urls-source "miniflux"
-      miniflux-url "http://rss.maralorn.de/"
-      miniflux-login "${user}"
-      miniflux-passwordeval "${lib.getExe rbw} get rss.maralorn.de ${user}"
-      ${config}
-    '';
-  in
-    pkgs.writeShellScriptBin name "${lib.getExe pkgs.newsboat} -r -C ${configFile} -c ~/.local/share/newsboat/${name}-cache.db \"$@\"") {
-    news = {
-      user = "maralorn";
-    };
-    software-updates = {
-      user = "maralorn-softwareupdates";
-    };
-    watchfeeds = {
-      user = "maralorn-watchfeeds";
-      config = ''
-        browser "mpv %u"
+  download-and-watch = pkgs.writeShellScriptBin "download-and-watch" ''
+    set -euo pipefail
+    cd ~/.volatile/video-downloads
+
+    link="''${1:-`${lib.getBin pkgs.wl-clipboard}/bin/wl-paste`}"
+
+    title="`${lib.getExe pkgs.yt-dlp} -j $link | ${lib.getExe pkgs.jq} -r .title`"
+    filename="`${lib.getExe pkgs.yt-dlp} -j $link | ${lib.getExe pkgs.jq} -r .filename`"
+    if [[ -f "$filename" ]]; then
+      ${lib.getExe pkgs.mpv} "$filename"
+    else
+      foot -derror ${lib.getExe pkgs.yt-dlp} $1 &
+      ${lib.getExe pkgs.mpv} "$title"*.part
+    fi
+  '';
+  commands =
+    builtins.mapAttrs (name: {
+      config ? "",
+      user,
+    }: let
+      configFile = pkgs.writeText "${name}-newsboat-config" ''
+        show-read-feeds no
+        show-read-articles no
+        datetime-format "%Y-%m-%d"
+        urls-source "miniflux"
+        miniflux-url "http://rss.maralorn.de/"
+        miniflux-login "${user}"
+        miniflux-passwordeval "${lib.getExe rbw} get rss.maralorn.de ${user}"
+        ${config}
       '';
-    };
-  };
+    in
+      pkgs.writeShellScriptBin name "${lib.getExe pkgs.newsboat} -r -C ${configFile} -c ~/.local/share/newsboat/${name}-cache.db \"$@\"") {
+      news = {
+        user = "maralorn";
+      };
+      software-updates = {
+        user = "maralorn-softwareupdates";
+      };
+      watchfeeds = {
+        user = "maralorn-watchfeeds";
+        config = ''
+          browser "${lib.getExe download-and-watch} %u"
+        '';
+      };
+    }
+    // {inherit download-and-watch;};
 in {
   systemd.user = {
     services.update-software-feeds = {
