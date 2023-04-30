0d749c5f4c9a615581bd23bff4570664970afc9a
maralorn <mail@maralorn.de>
2023-04-30 03:11:13 +0200

    Improve disk-config for fluffy

--- nixos/machines/fluffy/disk-config.nix
+++ nixos/machines/fluffy/disk-config.nix
@@ -1,62 +1,69 @@
 {device, ...}: {
-  disko.devices.disk.system = {
-    type = "disk";
-    inherit device;
-    content = {
-      type = "table";
-      format = "gpt";
-      partitions = [
-        {
-          name = "efi-system-partition";
-          start = "1MiB";
-          end = "1GiB";
-          fs-type = "fat32";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/efi";
-          };
-        }
-        {
-          name = "swap";
-          start = "1GiB";
-          end = "17GiB";
-          content = {
-            type = "swap";
-            randomEncryption = true;
-          };
-        }
-        {
-          name = "data-disk";
-          start = "17GiB";
-          end = "100%";
-          content = {
-            type = "luks";
-            name = "crypted-nixos";
-            extraOpenArgs = ["--allow-discards"];
+  disko.devices = {
+    disk.system = {
+      type = "disk";
+      inherit device;
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "ESP";
+            end = "1GiB";
+            fs-type = "fat32";
+            bootable = true;
             content = {
-              type = "btrfs";
-              extraArgs = ["-f"]; # Override existing partition
-              subvolumes = {
-                # Mountpoints inferred from subvolume name
-                "/disk" = {
-                  mountOptions = ["compress=zstd"];
-                };
-                "/disk/persist" = {
-                  mountOptions = ["compress=zstd"];
-                };
-                "/disk/volatile" = {
-                  mountOptions = ["compress=zstd"];
-                };
-                "/nix" = {
-                  mountOptions = ["compress=zstd" "noatime"];
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/efi";
+            };
+          }
+          {
+            name = "swap";
+            start = "1GiB";
+            end = "17GiB";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+            };
+          }
+          {
+            name = "system-vault";
+            start = "17GiB";
+            content = {
+              type = "luks";
+              name = "system";
+              extraOpenArgs = ["--allow-discards"];
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                subvolumes = {
+                  # Mountpoints inferred from subvolume name
+                  "/disk" = {
+                    mountOptions = ["compress=zstd"];
+                  };
+                  "/disk/persist" = {
+                    mountOptions = ["compress=zstd"];
+                  };
+                  "/disk/volatile" = {
+                    mountOptions = ["compress=zstd"];
+                  };
+                  "/nix" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
                 };
               };
             };
-          };
-        }
-      ];
+          }
+        ];
+      };
+      nodev."/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "defaults"
+          "mode=755"
+        ];
+      };
     };
   };
 }
