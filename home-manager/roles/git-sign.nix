765f8a6bb3922cee7edfcd63dd40bbe8803b19b4
maralorn <mail@maralorn.de>
2023-06-01 03:01:07 +0200

    Configure ssh commit signing

--- home-manager/roles/git-sign.nix
+++ home-manager/roles/git-sign.nix
@@ -1,1 +1,10 @@
-_: { programs.git.signing.key = "6C3D12CD88CDF46C5EAF4D12226A2D41EF5378C9"; }
+{
+  programs.git = {
+    signing = {
+      signByDefault = true;
+      key =
+        "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVmGAML5Ap+5RNPqVnWumAVMoY6xbebvS/KfDeqp8lk signing-key";
+    };
+    extraConfig.gpg.format = "ssh";
+  };
+}
