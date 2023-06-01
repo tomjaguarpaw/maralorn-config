e802c6ede2fb4c6500519bffa5665505df87ef04
maralorn <mail@maralorn.de>
2023-06-01 03:13:05 +0200

    Fix commit signing

--- home-manager/roles/git-sign.nix
+++ home-manager/roles/git-sign.nix
@@ -1,10 +1,18 @@
-{
+let
+  signing-key =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVmGAML5Ap+5RNPqVnWumAVMoY6xbebvS/KfDeqp8lk";
+in {
   programs.git = {
     signing = {
       signByDefault = true;
-      key =
-        "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVmGAML5Ap+5RNPqVnWumAVMoY6xbebvS/KfDeqp8lk signing-key";
+      key = "key::${signing-key}";
     };
-    extraConfig.gpg.format = "ssh";
+    extraConfig = {
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile =
+        builtins.toFile "git-ssh-allowedSignersFile" ''
+          mail@maralorn.de ${signing-key}
+        '';
+    };
   };
 }
