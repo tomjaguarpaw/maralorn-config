c3e7bedc864adf423ac9743de915ee7eb92d5a4f
maralorn <mail@maralorn.de>
2023-06-14 01:32:59 +0200

    Fix element-web patch

--- overlays/element-web/default.nix
+++ overlays/element-web/default.nix
@@ -1,12 +1,17 @@
-final: prev: {
-  element-web = final.runCommand "maralorns-element-web" { } ''
-    cp -r ${prev.element-web} $out
-    cd $out
-    chmod u+w -R $out
-    bundlecss=$(find . -name 'bundle.css')
-    cat ${./user.css} >> $bundlecss
-    bundlejs=$(find . -name 'vendors~init.js')
-    sed -i 's/return n.room.roomId===e||n.isUnread}/return n.room.roomId===e||n.hasUnreadCount}/' $bundlejs
-    cp ${./orville_communicator.opus} media/message.ogg
-  '';
+_final: prev: {
+  element-web = prev.element-web.overrideAttrs (_: {
+    postConfigure = ''
+      patch node_modules/matrix-react-sdk/lib/components/views/rooms/RoomList.js ${
+        ./RoomList.js.patch
+      }
+      cp ${
+        ./orville_communicator.opus
+      } node_modules/matrix-react-sdk/res/media/message.ogg
+    '';
+    preInstall = ''
+      find . -name 'bundle.css'
+      bundlecss=$(find . -name 'bundle.css')
+      cat ${./user.css} >> $bundlecss
+    '';
+  });
 }
