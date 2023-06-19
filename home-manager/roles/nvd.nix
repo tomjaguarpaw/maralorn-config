d316bc0cde644da45dd19fa86e130839d414c829
maralorn <mail@maralorn.de>
2023-06-19 11:59:43 +0200

    Fix nvd script

--- home-manager/roles/nvd.nix
+++ home-manager/roles/nvd.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let inherit (config.lib) dag;
 in {
   home.activation.report-changes = dag.entryAnywhere ''
-     if [[ -n "$oldGenPath" && "$oldGenPath" != "$newGenPath" ]]; then
-    ${pkgs.nvd}/bin/nvd diff $oldGenPath $newGenPath
-     fi
+    if [[ -v oldGenPath && "$oldGenPath" != "$newGenPath" ]]; then
+      ${lib.getExe pkgs.nvd} diff $oldGenPath $newGenPath
+    fi
   '';
 }
