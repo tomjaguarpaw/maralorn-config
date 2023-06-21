ed368ca97f19ce03524a9c7bd07da333dd38066b
maralorn <mail@maralorn.de>
2023-06-21 05:20:35 +0200

    Override nix-serve-ng to use working version

--- nixos/machines/fluffy/configuration.nix
+++ nixos/machines/fluffy/configuration.nix
@@ -52,7 +52,14 @@ in {
   };
 
   services.nix-serve = {
-    package = pkgs.nix-serve-ng;
+    package = pkgs.haskell.lib.overrideCabal pkgs.nix-serve-ng {
+      src = pkgs.fetchFromGitHub {
+        repo = "nix-serve-ng";
+        owner = "aristanetworks";
+        rev = "dabf46d65d8e3be80fa2eacd229eb3e621add4bd";
+        hash = "sha256-SoJJ3rMtDMfUzBSzuGMY538HDIj/s8bPf8CjIkpqY2w=";
+      };
+    };
     enable = true;
     bindAddress = "localhost";
     secretKeyFile = config.age.secrets.nix-serve-secret-key.path;
