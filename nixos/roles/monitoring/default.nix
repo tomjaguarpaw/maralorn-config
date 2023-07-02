47edf12fcffa31d63e177e80ab1cff4ba7ff1156
maralorn <mail@maralorn.de>
2023-07-03 00:42:28 +0200

    Fix monitoring hosts

--- nixos/roles/monitoring/default.nix
+++ nixos/roles/monitoring/default.nix
@@ -1,25 +1,19 @@
 { config, ... }:
-let
-  commonOptions = {
-    enableACME = true;
-    forceSSL = true;
-    basicAuthFile = config.age.secrets."basic-auth/monitoring".path;
-  };
+let inherit (config.m-0) virtualHosts;
 in {
   imports = [ ./alertmanager.nix ./grafana.nix ./prometheus.nix ./probes.nix ];
 
   services = {
     nginx = {
       enable = true;
-      virtualHosts."alerts.maralorn.de" = {
-        locations."/".proxyPass = "http://localhost:9093";
-      } // commonOptions;
-      virtualHosts."stats.maralorn.de" = {
-        locations."/".proxyPass = "http://localhost:3000/";
-      } // commonOptions;
-      virtualHosts."monitoring.maralorn.de" = {
-        locations."/".proxyPass = "http://localhost:9090";
-      } // commonOptions;
+      virtualHosts = {
+        ${virtualHosts.alerts}.locations."/".proxyPass =
+          "http://localhost:9093";
+        ${virtualHosts.stats}.locations."/".proxyPass =
+          "http://localhost:3000/";
+        ${virtualHosts.monitoring}.locations."/".proxyPass =
+          "http://localhost:9090";
+      };
     };
   };
 }
