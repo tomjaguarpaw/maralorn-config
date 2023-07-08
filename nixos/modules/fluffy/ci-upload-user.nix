601c2cec784b73498d6ac0a8605a233e6e81b713
maralorn <mail@maralorn.de>
2023-07-09 01:44:34 +0200

    Add ci-uploda-user

---
+++ nixos/modules/fluffy/ci-upload-user.nix
@@ -0,0 +1,8 @@
+{
+  users.users.ci-upload-user = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEbgJOIJrr09SPboM468PphhiGEfMnARVLqNWmjN5+Tv forgejo-runner@fluffy.maralorn.de"
+    ];
+  };
+}
