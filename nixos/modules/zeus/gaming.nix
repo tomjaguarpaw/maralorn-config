704a79ed47448779df3d427e79dd960035c40fb6
maralorn <mail@maralorn.de>
2023-07-24 21:42:54 +0200

    Deactive amdvlk

--- nixos/modules/zeus/gaming.nix
+++ nixos/modules/zeus/gaming.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
 {
 
   hardware.opengl = {
-    extraPackages = [ pkgs.amdvlk ];
-    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+    #extraPackages = [ pkgs.amdvlk ];
+    #extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     enable = true;
     driSupport = true;
     driSupport32Bit = true; # for gw2
