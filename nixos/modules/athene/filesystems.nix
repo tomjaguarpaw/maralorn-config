6d05f632e0333d17fad54110fe58a8128fbaf096
maralorn <mail@maralorn.de>
2023-07-26 12:29:13 +0200

    Fix athene filesystem config

--- nixos/modules/athene/filesystems.nix
+++ nixos/modules/athene/filesystems.nix
@@ -4,5 +4,8 @@
       device = "/dev/disk/by-id/ata-Samsung_SSD_870_QVO_4TB_S5STNJ0W103706V";
     })
   ];
-  fileSystems."/disk/persist".neededForBoot = true;
+  fileSystems = {
+    "/disk/persist".neededForBoot = true;
+    "/disk/volatile".neededForBoot = true;
+  };
 }
