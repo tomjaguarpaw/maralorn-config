8fd7220aebc605977dc6d704bbfe7fb04a80699f
maralorn <mail@maralorn.de>
2023-07-28 00:50:08 +0200

    Use Warning type for stale warnings

--- packages/status-script/exe/Main.hs
+++ packages/status-script/exe/Main.hs
@@ -485,54 +485,60 @@ main = Notify.withManager \watch_manager -> do
                   then tryCmd (git "--no-optional-locks" "-C" (git_dir </> "config") "log" "--oneline" "origin/main" "^main")
                   else pure ""
               when' (not $ LBS.null behind) $ withColor yellow [i|Config #{show (length (LBSC.lines behind))} commits behind.|]
-          , OldModule \var -> do
-              commit_var <- newTVarIO ""
-              system_var <- newTVarIO ""
-              modes_var <- newTVarIO ""
-              system_dirty_var <- newTVarIO False
-              modes_dirty_var <- newTVarIO False
-              host_name <- ByteStringChar.strip <$> readFileBS "/etc/hostname"
-              let scan = do
-                    current_commit <- readFileBS (git_dir </> "config/.git/refs/heads/main")
-                    system_commit <- Exception.try @Exception.IOException do readFileBS "/run/current-system/config-commit"
-                    modes_commit <- Exception.try do readFileBS (modes_dir </> "config-commit")
-                    current_system <- readlink "/run/current-system" |> captureTrim
-                    current_modes <- readlink modes_dir |> captureTrim
-                    let stale_config = \case
-                          (Right commit) | commit == current_commit -> False
-                          _ -> True
-                        system_stale = stale_config system_commit
-                        modes_stale = stale_config modes_commit
-                    (commit_change, system_change, modes_change) <-
-                      atomically $
-                        (,,)
-                          <$> (STM.stateTVar commit_var \previous_commit -> (previous_commit /= current_commit, current_commit))
-                          <*> (STM.stateTVar system_var \previous_system -> (previous_system /= current_system, current_system))
-                          <*> (STM.stateTVar modes_var \previous_modes -> (previous_modes /= current_modes, current_modes))
-                    if system_stale
-                      then when (commit_change || system_change) do
-                        sayErr "Eval system config …"
-                        next_system <- nix "eval" "--raw" ([i|#{home}/git/config\#nixosConfigurations.#{host_name}.config.system.build.toplevel.drvPath|] :: String) |> captureTrim
-                        sayErr "System eval finished."
-                        diff_is_small <- diffIsSmall next_system current_system
-                        atomically $ writeTVar system_dirty_var (not diff_is_small)
-                      else atomically do writeTVar system_dirty_var False
-                    if modes_stale
-                      then when (commit_change || modes_change) do
-                        sayErr "Eval home config …"
-                        next_modes <- nix "eval" "--raw" ([i|#{home}/git/config\#homeModes.#{host_name}.drvPath|] :: String) |> captureTrim
-                        sayErr "Home eval finished."
-                        diff_is_small <- diffIsSmall next_modes current_modes
-                        atomically $ writeTVar modes_dirty_var (not diff_is_small)
-                      else atomically do writeTVar modes_dirty_var False
-                    system_dirty <- readTVarIO system_dirty_var
-                    modes_dirty <- readTVarIO modes_dirty_var
-                    when' (system_dirty || modes_dirty) $ withColor yellow [i|Current #{case (system_dirty,modes_dirty) of (True, True) -> "home and system"; (True, _) -> "system"; _ -> "home"} stale|]
-              forever do
-                Concurrent.threadDelay (4 * oneSecond)
-                dirty <- elem "config" <$> readTVarIO dirty_var
-                var =<< if dirty then pure Nothing else scan
           ]
+    commit_var <- newTVarIO ""
+    system_var <- newTVarIO ""
+    modes_var <- newTVarIO ""
+    system_dirty_var <- newTVarIO False
+    modes_dirty_var <- newTVarIO False
+    host_name <- ByteStringChar.strip <$> readFileBS "/etc/hostname"
+    let scan = do
+          current_commit <- readFileBS (git_dir </> "config/.git/refs/heads/main")
+          system_commit <- Exception.try @Exception.IOException do readFileBS "/run/current-system/config-commit"
+          modes_commit <- Exception.try do readFileBS (modes_dir </> "config-commit")
+          current_system <- readlink "/run/current-system" |> captureTrim
+          current_modes <- readlink modes_dir |> captureTrim
+          let stale_config = \case
+                (Right commit) | commit == current_commit -> False
+                _ -> True
+              system_stale = stale_config system_commit
+              modes_stale = stale_config modes_commit
+          (commit_change, system_change, modes_change) <-
+            atomically $
+              (,,)
+                <$> (STM.stateTVar commit_var \previous_commit -> (previous_commit /= current_commit, current_commit))
+                <*> (STM.stateTVar system_var \previous_system -> (previous_system /= current_system, current_system))
+                <*> (STM.stateTVar modes_var \previous_modes -> (previous_modes /= current_modes, current_modes))
+          if system_stale
+            then when (commit_change || system_change) do
+              sayErr "Eval system config …"
+              next_system <- nix "eval" "--raw" ([i|#{home}/git/config\#nixosConfigurations.#{host_name}.config.system.build.toplevel.drvPath|] :: String) |> captureTrim
+              sayErr "System eval finished."
+              diff_is_small <- diffIsSmall next_system current_system
+              atomically $ writeTVar system_dirty_var (not diff_is_small)
+            else atomically do writeTVar system_dirty_var False
+          if modes_stale
+            then when (commit_change || modes_change) do
+              sayErr "Eval home config …"
+              next_modes <- nix "eval" "--raw" ([i|#{home}/git/config\#homeModes.#{host_name}.drvPath|] :: String) |> captureTrim
+              sayErr "Home eval finished."
+              diff_is_small <- diffIsSmall next_modes current_modes
+              atomically $ writeTVar modes_dirty_var (not diff_is_small)
+            else atomically do writeTVar modes_dirty_var False
+          system_dirty <- readTVarIO system_dirty_var
+          modes_dirty <- readTVarIO modes_dirty_var
+          let stale_warn = \scope ->
+                MkWarning
+                  { description = [i|Current #{scope} stale|]
+                  , group = "warning"
+                  , subgroup = Nothing
+                  }
+          pure $ [stale_warn "system" | system_dirty] <> [stale_warn "home" | modes_dirty]
+    (stale_warning, stale_trigger) <- R.newTriggerEvent
+    void $ liftIO $ Conc.forkIO $ forever do
+      Concurrent.threadDelay (4 * oneSecond)
+      dirty <- elem "config" <$> readTVarIO dirty_var
+      stale_trigger =<< if dirty then pure [] else scan
     dirty_event <- do
       dirty_updates <- performEventThreaded git_dir_events \dirs -> do
         now_dirty <- Set.fromList . fmap toText <$> filterM (isDirty . (git_dir </>)) dirs
@@ -560,7 +566,7 @@ main = Notify.withManager \watch_manager -> do
                 , group = "git"
                 , subgroup = Just "unpushed"
                 }
-    warnings <- concatEvents [dirty_event, unpushed_event]
+    warnings <- concatEvents [dirty_event, unpushed_event, stale_warning]
     broadcastToSocket "warnings" (warnings <&> Aeson.encode % toStrict)
     broadcastToSocket
       "warninggroups"
