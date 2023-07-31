42bc4ef16fb6f9c1285c7ceacd24c252a2dd2140
maralorn <mail@maralorn.de>
2023-07-31 20:14:25 +0200

    Fix refresh-config

--- home-manager/roles/refresh-config.nix
+++ home-manager/roles/refresh-config.nix
@@ -1,13 +1,26 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   configPath = "${config.home.homeDirectory}/git/config";
-  configGit = "${pkgs.git}/bin/git -C ${configPath}";
+  configGit = "${pkgs.gitMinimal}/bin/git -C ${configPath}";
 in
 {
   systemd.user = {
     services.refresh-config = {
       Unit.Description = "Fetch config repo";
       Service = {
+        Environment = "PATH=${
+            lib.makeBinPath [
+              pkgs.coreutils
+              pkgs.bash
+              pkgs.openssh
+              pkgs.gitMinimal
+            ]
+          }";
         Type = "oneshot";
         ExecStart = "${configGit} fetch origin main";
       };
