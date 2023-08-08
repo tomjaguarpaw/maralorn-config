19f2b92e5e10bf5edbe58bd3a910789a9fee8511
maralorn <mail@maralorn.de>
2023-08-08 03:17:00 +0200

    Nicer debug output

--- packages/status-script/lib/StatusScript/CommandUtil.hs
+++ packages/status-script/lib/StatusScript/CommandUtil.hs
@@ -1,6 +1,7 @@
 module StatusScript.CommandUtil (tryCmd, reportMissing) where
 
 import Data.ByteString.Lazy qualified as LBS
+import Data.Text qualified as Text
 import Maralorn.Prelude
 import Shh ((|>))
 import Shh qualified
@@ -9,4 +10,4 @@ tryCmd :: Shh.Proc a -> IO LBS.ByteString
 tryCmd x = Shh.ignoreFailure x |> Shh.captureTrim
 
 reportMissing :: MonadIO m => IO [FilePath] -> m ()
-reportMissing missing = whenJustM (nonEmpty <$> liftIO missing) \missing' -> sayErr [i|missing executables #{missing'}|]
+reportMissing missing = whenJustM (nonEmpty <$> liftIO missing) \missing' -> sayErr [i|Missing executables: #{Text.intercalate "," (toList missing' <&> toText)}|]
