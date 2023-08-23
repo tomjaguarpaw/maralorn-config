4e933e6d19c3cf3c05fad9e802d17de2610c3036
maralorn <mail@maralorn.de>
2023-08-23 11:27:19 +0200

    Clean up stale detection

--- packages/status-script/lib/StatusScript/Modules/ConfigStale.hs
+++ packages/status-script/lib/StatusScript/Modules/ConfigStale.hs
@@ -1,97 +1,43 @@
 module StatusScript.Modules.ConfigStale (configStale) where
 
-import Control.Concurrent.STM qualified as STM
 import Control.Exception qualified as Exception
 import Data.ByteString.Char8 qualified as ByteStringChar
-import Data.ByteString.Lazy qualified as LBS
 import Data.Set qualified as Set
 import Maralorn.Prelude
 import Reflex qualified as R
 import Reflex.Host.Headless qualified as R
-import Shh ((|>))
-import Shh qualified
-import StatusScript.CommandUtil qualified as CommandUtil
 import StatusScript.Env (Env (..))
+import StatusScript.FileWatch qualified as FileWatch
 import StatusScript.Mode (Mode (..))
 import StatusScript.ReflexUtil qualified as ReflexUtil
 import StatusScript.Warnings (Warning (..))
 import System.FilePath ((</>))
 
-Shh.load Shh.Absolute ["readlink", "nix", "nix-diff", "jq"]
-missingExecutables :: IO [FilePath]
-diffIsSmall :: LBS.ByteString -> LBS.ByteString -> IO Bool
-diffIsSmall = \pathA pathB ->
-  CommandUtil.tryCmd
-    (nix_diff "--json" [pathA, pathB] |> jq ".inputsDiff.inputDerivationDiffs")
-    <&> (== "[]")
-
 configStale :: (R.MonadHeadlessApp t m) => Env -> R.Dynamic t Mode -> R.Dynamic t (Set Text) -> m (R.Event t [Warning])
 configStale env mode dirties = do
-  CommandUtil.reportMissing missingExecutables
-  commit_var <- newTVarIO ""
-  system_var <- newTVarIO ""
-  modes_var <- newTVarIO ""
-  system_dirty_var <- newTVarIO False
-  modes_dirty_var <- newTVarIO False
-  host_name <- ByteStringChar.strip <$> readFileBS "/etc/hostname"
   let home = env.homeDir
       git_dir = home </> "git"
       modes_dir = home </> ".volatile" </> "modes"
-      scan = do
-        current_commit <- readFileBS (git_dir </> "config/.git/refs/heads/main")
-        system_commit <- Exception.try @Exception.IOException do readFileBS "/run/current-system/config-commit"
-        modes_commit <- Exception.try do readFileBS (modes_dir </> "config-commit")
-        current_system <- CommandUtil.tryCmd (readlink "/run/current-system")
-        current_modes <- CommandUtil.tryCmd (readlink modes_dir)
-        let stale_config = \case
-              (Right commit) | commit == current_commit -> False
-              _ -> True
-            system_stale = stale_config system_commit
-            modes_stale = stale_config modes_commit
-        (commit_change, system_change, modes_change) <-
-          atomically $
-            (,,)
-              <$> (STM.stateTVar commit_var \previous_commit -> (previous_commit /= current_commit, current_commit))
-              <*> (STM.stateTVar system_var \previous_system -> (previous_system /= current_system, current_system))
-              <*> (STM.stateTVar modes_var \previous_modes -> (previous_modes /= current_modes, current_modes))
-        if system_stale
-          then when (commit_change || system_change) do
-            sayErr "Eval system config …"
-            next_system <-
-              CommandUtil.tryCmd $
-                nix
-                  "eval"
-                  "--raw"
-                  [s|#{home}/git/config\#nixosConfigurations.#{host_name}.config.system.build.toplevel.drvPath|]
-            sayErr "System eval finished."
-            diff_is_small <- diffIsSmall next_system current_system
-            atomically $ writeTVar system_dirty_var (not diff_is_small)
-          else atomically do writeTVar system_dirty_var False
-        if modes_stale
-          then when (commit_change || modes_change) do
-            sayErr "Eval home config …"
-            next_modes <-
-              CommandUtil.tryCmd $
-                nix
-                  "eval"
-                  "--raw"
-                  [s|#{home}/git/config\#homeModes.#{host_name}.drvPath|]
-            sayErr "Home eval finished."
-            diff_is_small <- diffIsSmall next_modes current_modes
-            atomically $ writeTVar modes_dirty_var (not diff_is_small)
-          else atomically do writeTVar modes_dirty_var False
-        system_dirty <- readTVarIO system_dirty_var
-        modes_dirty <- readTVarIO modes_dirty_var
-        let stale_warn = \scope ->
-              MkWarning
-                { description = Just [i|Current #{scope} stale|]
-                , group = "warning"
-                , subgroup = Nothing
-                }
-        pure $ [stale_warn "system" | system_dirty] <> [stale_warn "home" | modes_dirty]
+      config_dirty = dirties <&> Set.member "config"
+  commit_event <- FileWatch.watchFile env (git_dir </> "config/.git/refs/heads") "main"
+  system_commit_event <- FileWatch.watchFile env "/run/current-system" "config-commit"
+  modes_commit_event <- FileWatch.watchFile env modes_dir "config-commit"
   tick <- ReflexUtil.tickEvent 30
-  let config_dirty = dirties <&> Set.member "config"
-  ReflexUtil.performEventThreaded env (ReflexUtil.taggedAndUpdated ((,) <$> mode <*> config_dirty) tick) \case
+  ReflexUtil.performEventThreaded env (ReflexUtil.taggedAndUpdated ((,) <$> mode <*> config_dirty) (R.leftmost [commit_event, system_commit_event, modes_commit_event, tick])) \case
     (Klausur, _) -> pure []
     (_, True) -> pure []
-    _ -> scan
+    _ -> do
+      commit <- readFileBS (git_dir </> "config/.git/refs/heads/main") & Exception.try @Exception.IOException %> hush
+      system_commit <- readFileBS "/run/current-system/config-commit" & Exception.try @Exception.IOException %> hush
+      modes_commit <- readFileBS (modes_dir </> "config-commit") & Exception.try @Exception.IOException %> hush
+      let is_dirty = \installed_commit -> case (commit, installed_commit) of
+            (Nothing, _) -> False
+            (Just a, Just b) | ByteStringChar.strip a == ByteStringChar.strip b -> False
+            _ -> True
+      let stale_warn = \scope ->
+            MkWarning
+              { description = Just [i|Current #{scope} stale|]
+              , group = "warning"
+              , subgroup = Nothing
+              }
+      pure $ [stale_warn "system" | is_dirty system_commit] <> [stale_warn "home" | is_dirty modes_commit]
