d49474351a33848fe00568d1885da356c1a29dc4
maralorn <mail@maralorn.de>
2023-08-28 12:00:03 +0200

    No nmcli on zeus

--- packages/status-script/lib/StatusScript/Modules/Network.hs
+++ packages/status-script/lib/StatusScript/Modules/Network.hs
@@ -8,13 +8,16 @@ import StatusScript.CommandUtil qualified as CommandUtil
 import StatusScript.Env (Env (..))
 import StatusScript.ReflexUtil qualified as ReflexUtil
 
-Shh.load Shh.Absolute ["nmcli"]
-missingExecutables :: IO [FilePath]
 networkState :: (R.MonadHeadlessApp t m) => Env -> m (R.Event t [Text])
-networkState = \env -> do
-  monitor_event <- ReflexUtil.processLines env (nmcli "monitor")
-  ReflexUtil.performEventThreaded env monitor_event \_ -> do
-    CommandUtil.tryCmd (nmcli "-g" "name" "connection" "show" "--active")
-      <&> decodeUtf8
-        % lines
-        % filter (/= "lo")
+networkState = \env ->
+  liftIO Shh.pathBins <&> (elem "nmcli") >>= \case
+    False -> do
+      sayErr "No nmcli in PATH, disabling networkmanager support."
+      pure R.never
+    True -> do
+      monitor_event <- ReflexUtil.processLines env (Shh.exe "nmcli" "monitor")
+      ReflexUtil.performEventThreaded env monitor_event \_ -> do
+        CommandUtil.tryCmd (Shh.exe "nmcli" "-g" "name" "connection" "show" "--active")
+          <&> decodeUtf8
+            % lines
+            % filter (/= "lo")
