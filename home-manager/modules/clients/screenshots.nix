37e7061e915f659e1fbd0a60ba9dc0482710e72c
maralorn <mail@maralorn.de>
2023-08-29 00:35:55 +0200

    Fix screenshoting

--- home-manager/modules/clients/screenshots.nix
+++ home-manager/modules/clients/screenshots.nix
@@ -3,8 +3,6 @@
   home.packages = [
     pkgs.slurp
     pkgs.grim
-    (pkgs.writeShellScriptBin "screenshot"
-      "GRIM_DEFAULT_DIR=$XDG_PICTURES_DIR/screenshots grim -g $(slurp)"
-    )
+    (pkgs.writeShellScriptBin "screenshot" ''grim -g "$(slurp)" - | wl-copy'')
   ];
 }
