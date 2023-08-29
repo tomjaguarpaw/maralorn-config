92b05cac89b19288c40f0adca7d7ed12621ea644
maralorn <mail@maralorn.de>
2023-08-29 13:05:22 +0200

    Do total mute on notebook sleep

--- home-manager/modules/apollo/idle-config.nix
+++ home-manager/modules/apollo/idle-config.nix
@@ -10,6 +10,24 @@ in
         inherit command;
       }
       {
+        event = "before-sleep";
+        command =
+          (pkgs.writeShellScript "total-mute" ''
+            echo "Running total mute";
+            ${lib.getExe pkgs.mpc-cli} pause
+            for ((index=0; index < $(${pkgs.pulseaudio}/bin/pactl list short sinks | ${
+              lib.getBin pkgs.coreutils
+            }/bin/wc -l); index++)); do
+              ${pkgs.pulseaudio}/bin/pactl set-sink-mute $index 1;
+            done
+            for ((index=0; index < $(${pkgs.pulseaudio}/bin/pactl list short sources | ${
+              lib.getBin pkgs.coreutils
+            }/bin/wc -l); index++)); do
+              ${pkgs.pulseaudio}/bin/pactl set-source-mute $index 1;
+            done
+          '').outPath;
+      }
+      {
         event = "lock";
         inherit command;
       }
