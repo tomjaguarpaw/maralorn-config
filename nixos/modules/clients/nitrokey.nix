98bc361d31a9a777682f0dea4488be119e58d777
maralorn <mail@maralorn.de>
2023-09-06 16:10:41 +0200

    Lock screen on key removal.

--- nixos/modules/clients/nitrokey.nix
+++ nixos/modules/clients/nitrokey.nix
@@ -1,5 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   hardware.nitrokey.enable = true;
   environment.systemPackages = [ pkgs.pynitrokey ];
+  services.udev.extraRules = ''
+    ACTION=="remove", ENV{ID_BUS}=="usb", ENV{ID_MODEL_ID}=="42b2", ENV{ID_VENDOR_ID}=="20a0", RUN+="${
+      lib.getBin pkgs.systemd
+    }/bin/loginctl lock-sessions"
+  '';
 }
