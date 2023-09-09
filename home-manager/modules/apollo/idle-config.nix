e0386fe3d2071d95af18925a4d42fb610bbb2607
maralorn <mail@maralorn.de>
2023-09-09 14:20:39 +0200

    Fix lock

--- home-manager/modules/apollo/idle-config.nix
+++ home-manager/modules/apollo/idle-config.nix
@@ -1,7 +1,9 @@
 { pkgs, lib, ... }:
 {
   services.swayidle = {
-    events = [ {
+    # Here we override the before-sleep eveent
+    # swayidle only runs the last before-sleep command given on the commandline
+    events = lib.mkAfter [ {
       event = "before-sleep";
       command =
         (pkgs.writeShellScript "total-mute" ''
@@ -17,6 +19,7 @@
           }/bin/wc -l); index++)); do
             ${pkgs.pulseaudio}/bin/pactl set-source-mute $index 1;
           done
+          ${lib.getExe pkgs.swaylock} -f
         '').outPath;
     } ];
     timeouts = [ {
