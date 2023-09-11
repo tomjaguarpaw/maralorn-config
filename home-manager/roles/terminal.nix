c2c521feff97689a77c718f4f9a7ceb431324689
maralorn <mail@maralorn.de>
2023-09-11 12:41:33 +0200

    Spleen config

--- home-manager/roles/terminal.nix
+++ home-manager/roles/terminal.nix
@@ -4,11 +4,11 @@
   programs.foot = {
     settings = {
       main = {
-        font = "Symbols Nerd Font Mono:pixelsize=12,CozetteVector:pixelsize=12";
+        font = "Symbols Nerd Font Mono:pixelsize=12,Spleen:pixelsize=12";
         include =
           (pkgs.runCommandLocal "foot-theme" { } ''
             cat ${pkgs.foot.themes}/share/foot/themes/catppuccin > $out
-            echo -e "background=000000\nalpha=0.9" >> $out
+            echo -e "background=000040\nalpha=0.9" >> $out
           '').outPath;
       };
       mouse = {
