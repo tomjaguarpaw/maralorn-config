a33c1e5f878b4e34b37f89e07ef6ac53c5061da6
maralorn <mail@maralorn.de>
2023-09-18 14:51:35 +0200

    Add iso.nix

---
+++ iso.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix>
+  ];
+
+  # In 23.05 zfs conflicts with bcachefs to prevent this we do this override.
+  nixpkgs.overlays = [
+    (_: super: { zfs = super.zfs.overrideAttrs (_: { meta.platforms = [ ]; }); })
+  ];
+
+  environment.systemPackages = [
+    pkgs.helix
+    pkgs.fd
+    pkgs.htop
+    pkgs.tree
+    pkgs.lazygit
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+    "repl-flake"
+  ];
+
+  boot.supportedFilesystems = [ "bcachefs" ];
+  console = {
+    earlySetup = true;
+    font = "${pkgs.spleen}/share/consolefonts/spleen-6x12.psfu";
+    keyMap = "neo";
+  };
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+}
