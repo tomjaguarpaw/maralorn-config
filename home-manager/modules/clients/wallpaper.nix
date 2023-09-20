700a98df17c0500c3416ee697c4f3fb54e58bb58
maralorn <mail@maralorn.de>
2023-09-20 04:57:16 +0200

    Fix wallpaper

--- home-manager/modules/clients/wallpaper.nix
+++ home-manager/modules/clients/wallpaper.nix
@@ -9,11 +9,18 @@ let
   modeFile = "${homeDirectory}/.mode";
   wallPapers = "${homeDirectory}/media/images/wallpapers";
   hyprpaperConfig = "${homeDirectory}/.config/hypr/hyprpaper.conf";
+  fallback = pkgs.fetchurl {
+    url = "https://wallpapers.com/images/hd/pink-space-nebula-shp7fs58j0or29qo.jpg";
+    hash = "sha256-gahXC/kx8SL6XaZBli1tNdIFJn8M3SES3IWkPUp1O8k=";
+  };
   wallpaper-daemon =
     pkgs.writeHaskellScript
       {
         name = "wallpaper-daemon";
-        imports = [ "System.Random" ];
+        imports = [
+          "System.Random"
+          "Control.Exception (handle)"
+        ];
         bins = [
           pkgs.coreutils
           pkgs.hyprpaper
@@ -21,21 +28,18 @@ let
       }
       ''
         main = do
-           mode <- cat "${modeFile}" |> captureTrim
-           (lines . decodeUtf8 -> files) <- ls ([i|${wallPapers}/#{mode}|] :: String) |> captureTrim
-           ((files Unsafe.!!) -> file) <- getStdRandom $ randomR (0, length files - 1)
-           let new = [i|${wallPapers}/#{mode}/#{file}|] :: Text
-           writeFile "${hyprpaperConfig}" [i|preload = #{new}\nwallpaper = ,#{new}|];
+           file <- handle (\(_ :: SomeException) -> pure "${fallback}") do
+             mode <- cat "${modeFile}" |> captureTrim
+             (lines . decodeUtf8 -> files) <- ls ([i|${wallPapers}/#{mode}|] :: String) |> captureTrim
+             ((files Unsafe.!!) -> file) <- getStdRandom $ randomR (0, length files - 1)
+             pure ([i|${wallPapers}/#{mode}/#{file}|] :: Text)
+           writeFile "${hyprpaperConfig}" [i|preload = #{file}\nwallpaper = ,#{file}|];
            hyprpaper
       '';
 in
 {
   systemd.user.services.wallpaper = {
-    Unit = {
-      Description = "Wallpaper Daemon";
-    };
-    Service = {
-      ExecStart = lib.getExe wallpaper-daemon;
-    };
+    Unit.Description = "Wallpaper Daemon";
+    Service.ExecStart = lib.getExe wallpaper-daemon;
   };
 }
