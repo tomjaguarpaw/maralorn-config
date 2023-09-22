1b5f96d703ba37ef8f3c59362473b29787b9b845
maralorn <mail@maralorn.de>
2023-09-22 15:08:25 +0200

    Change CouchDB timeout

--- nixos/modules/hephaistos/couchdb.nix
+++ nixos/modules/hephaistos/couchdb.nix
@@ -3,6 +3,10 @@
     enable = true;
     adminUser = "admin";
     adminPass = "admin";
+    extraConfig = ''
+      [chttpd_auth]
+      timeout = 7200
+    '';
   };
   environment.persistence.snapshoted.directories = [ "/var/lib/couchdb" ];
 }
