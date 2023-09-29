bc9b5e15c5afac985cfcbbb0e79ac95b576424a6
maralorn <mail@maralorn.de>
2023-09-29 16:18:25 +0200

    Move and improve git config

---
+++ home-manager/modules/all/git.nix
@@ -0,0 +1,46 @@
+{
+  programs.git = {
+    aliases = {
+      sync = "!git pull -r && git push";
+      cpr = "!f() { git fetch origin refs/pull/$1/head && git checkout FETCH_HEAD; }; f";
+    };
+
+    extraConfig = {
+      merge.conflictStyle = "diff3";
+      pull.ff = "only";
+      core.editor = "hx";
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+    };
+    enable = true;
+    ignores = [
+      ".direnv"
+      ".syncthing*.tmp"
+      ".helix"
+      "*.swp"
+      "*.autosave~"
+      "*.aux"
+      "*.bbl"
+      "*.fls"
+      "*.idx"
+      "*.ilg"
+      "*.ind"
+      "*.log"
+      "*.out"
+      "*.toc"
+      "*.bcf"
+      "*.blg"
+      "*.fdb*"
+      "*.thm"
+      "*.run.xml"
+      "*.slnc"
+      "*.glade~"
+      "__pycache__"
+      ".hledger-web_client_session_key.aes"
+      ".nix-gc-roots"
+      "result"
+    ];
+    userEmail = "mail@maralorn.de";
+    userName = "maralorn";
+  };
+}
--- home-manager/roles/default.nix
+++ home-manager/roles/default.nix
@@ -46,49 +46,6 @@
         set -g allow-rename on
       '';
     };
-    git = {
-      aliases = {
-        sync = "!git pull -r && git push";
-        cpr = "!f() { git fetch origin refs/pull/$1/head && git checkout FETCH_HEAD; }; f";
-      };
-
-      extraConfig = {
-        merge.conflictStyle = "diff3";
-        pull.ff = "only";
-        core.editor = "hx";
-        init.defaultBranch = "main";
-      };
-      enable = true;
-      ignores = [
-        ".direnv"
-        ".syncthing*.tmp"
-        ".helix"
-        "*.swp"
-        "*.autosave~"
-        "*.aux"
-        "*.bbl"
-        "*.fls"
-        "*.idx"
-        "*.ilg"
-        "*.ind"
-        "*.log"
-        "*.out"
-        "*.toc"
-        "*.bcf"
-        "*.blg"
-        "*.fdb*"
-        "*.thm"
-        "*.run.xml"
-        "*.slnc"
-        "*.glade~"
-        "__pycache__"
-        ".hledger-web_client_session_key.aes"
-        ".nix-gc-roots"
-        "result"
-      ];
-      userEmail = "mail@maralorn.de";
-      userName = "maralorn";
-    };
     htop = {
       enable = true;
       settings = {
