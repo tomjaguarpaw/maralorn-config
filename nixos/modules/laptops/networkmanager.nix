543773bdb2636121c24f82948a5db6a57341137c
maralorn <mail@maralorn.de>
2023-10-18 10:55:05 +0200

    Reformat networkmanager

--- nixos/modules/laptops/networkmanager.nix
+++ nixos/modules/laptops/networkmanager.nix
@@ -4,13 +4,10 @@
     pkgs.networkmanagerapplet # For when the gnome dialog sucks in asking for a wifi password.
   ];
 
-  networking = {
-    networkmanager.enable = true;
-  };
+  networking.networkmanager.enable = true;
+
   systemd = {
     network.wait-online.enable = false;
-    services = {
-      NetworkManager-wait-online.enable = false;
-    };
+    services.NetworkManager-wait-online.enable = false;
   };
 }
