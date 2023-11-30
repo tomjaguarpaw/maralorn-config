cee9e7b4f9202fdc514cdc3f053e85d5f8aefebc
maralorn <mail@maralorn.de>
2023-11-30 13:55:03 +0100

    Improve unread reporting

--- packages/status-script/lib/StatusScript/Modules/Mail.hs
+++ packages/status-script/lib/StatusScript/Modules/Mail.hs
@@ -43,7 +43,7 @@ mail env mode = do
   notmuch_update <-
     FileWatch.watchFile env (env.homeDir </> "Maildir/.notmuch/xapian") "flintlock"
   events <-
-    [ (const True, ["tag:unread"], "e-mail")
+    [ (const True, ["tag:unread", "not", "folder:/Archiv/"], "e-mail")
     -- , ((== Orga), ["folder:hera/Inbox", "not", "tag:unread"], "e-mail-open")
     -- , ((== Code), ["folder:hera/Code"], "Code")
       ]
