ad90ccb4a7b866a5c1fed004cebf2b2507de7075
maralorn <mail@maralorn.de>
2023-12-03 12:17:28 +0100

    Tmp disable signing

--- home-manager/roles/clients/git-sign.nix
+++ home-manager/roles/clients/git-sign.nix
@@ -1,18 +1,18 @@
-let
-  # signing-key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIIwv46KpfLQp1w5GwXCUUKtG4oKmFU04FLvkxp6DDnE+AAAABHNzaDo="; # maralorn@solo-key-1
-  signing-key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDl0e4VqFH93mCNwbuz5CcbURbHj+b9DdW3RbzRoKPYLAAAADHNzaDptYXJhbG9ybg=="; # maralorn@nitro-key-1
-in
+#let
+#  # signing-key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIIwv46KpfLQp1w5GwXCUUKtG4oKmFU04FLvkxp6DDnE+AAAABHNzaDo="; # maralorn@solo-key-1
+#  signing-key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDl0e4VqFH93mCNwbuz5CcbURbHj+b9DdW3RbzRoKPYLAAAADHNzaDptYXJhbG9ybg=="; # maralorn@nitro-key-1
+#in
 {
-  programs.git = {
-    signing = {
-      signByDefault = false;
-      key = "key::${signing-key}";
-    };
-    extraConfig = {
-      gpg.format = "ssh";
-      gpg.ssh.allowedSignersFile = builtins.toFile "git-ssh-allowedSignersFile" ''
-        mail@maralorn.de ${signing-key}
-      '';
-    };
-  };
+  # programs.git = {
+  #   signing = {
+  #     signByDefault = false;
+  #     key = "key::${signing-key}";
+  #   };
+  #   extraConfig = {
+  #     gpg.format = "ssh";
+  #     gpg.ssh.allowedSignersFile = builtins.toFile "git-ssh-allowedSignersFile" ''
+  #       mail@maralorn.de ${signing-key}
+  #     '';
+  #   };
+  # };
 }
