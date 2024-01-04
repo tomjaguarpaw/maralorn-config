27ef750e3260ca4f72eae9f5243ac1cc999c3d85
maralorn <mail@maralorn.de>
2024-01-04 10:56:16 +0100

    Configure wallpaper

--- home-manager/roles/clients/wallpaper.nix
+++ home-manager/roles/clients/wallpaper.nix
@@ -1,46 +1,8 @@
+{ pkgs, ... }:
+
 {
-  pkgs,
-  lib,
-  config,
-  ...
-}:
-let
-  inherit (config.home) homeDirectory;
-  modeFile = "${homeDirectory}/.mode";
-  wallPapers = "${homeDirectory}/media/images/wallpapers";
-  hyprpaperConfig = "${homeDirectory}/.config/hypr/hyprpaper.conf";
-  fallback = pkgs.fetchurl {
-    url = "https://wallpapers.com/images/hd/pink-space-nebula-shp7fs58j0or29qo.jpg";
-    hash = "sha256-gahXC/kx8SL6XaZBli1tNdIFJn8M3SES3IWkPUp1O8k=";
-  };
-  wallpaper-daemon =
-    pkgs.writeHaskellScript
-      {
-        name = "wallpaper-daemon";
-        imports = [
-          "System.Random"
-          "Control.Exception (handle)"
-        ];
-        bins = [
-          pkgs.coreutils
-          pkgs.hyprpaper
-        ];
-      }
-      ''
-        main = do
-           file <- handle (\(_ :: SomeException) -> pure "${fallback}") do
-             mode <- cat "${modeFile}" |> captureTrim
-             (lines . decodeUtf8 -> files) <- ls ([i|${wallPapers}/#{mode}|] :: String) |> captureTrim
-             ((files Unsafe.!!) -> file) <- getStdRandom $ randomR (0, length files - 1)
-             pure ([i|${wallPapers}/#{mode}/#{file}|] :: Text)
-           writeFile "${hyprpaperConfig}" [i|preload = #{file}\nwallpaper = ,#{file}|];
-           hyprpaper
-      '';
-in
-{
-  xdg.configFile.wallpaper.source = fallback;
-  systemd.user.services.wallpaper = {
-    Unit.Description = "Wallpaper Daemon";
-    Service.ExecStart = lib.getExe wallpaper-daemon;
+  xdg.configFile.wallpaper.source = pkgs.fetchurl {
+    url = "https://images-assets.nasa.gov/image/PIA12832/PIA12832~orig.jpg";
+    hash = "sha256-3yhQlzspkpJL5aoeC7kuiJ5I5SJqv2eixwxI8PQMeVU=";
   };
 }
