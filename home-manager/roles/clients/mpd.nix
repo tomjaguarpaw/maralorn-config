ca4cce342cd045688eeea4e505e8eb4de8162f76
maralorn <mail@maralorn.de>
2024-01-07 22:53:41 +0100

    Fix mpd update

--- home-manager/roles/clients/mpd.nix
+++ home-manager/roles/clients/mpd.nix
@@ -13,12 +13,17 @@ in
     mpd.host = "::";
   };
   home.packages = [
-    (pkgs.writeShellScriptBin "switch-mpd" ''
-      mkdir -p ${config.xdg.configHome}/mpDris2
-      rm -f $HOME/${mprisCfg.target}
-      cp ${mprisCfg.source} $HOME/${mprisCfg.target}
-      ${lib.getExe pkgs.sd} "host = ::" "host = $1" $HOME/${mprisCfg.target}
-      ${pkgs.systemd}/bin/systemctl --user restart mpdris2.service
-    '')
+    (pkgs.writeShellScriptBin "switch-mpd" (
+      let
+        target = "$HOME/${mprisCfg.target}";
+      in
+      ''
+        mkdir -p ${config.xdg.configHome}/mpDris2
+        rm -f ${target} ${target}.home-manager-backup
+        cp ${mprisCfg.source} ${target}
+        ${lib.getExe pkgs.sd} "host = ::" "host = $1" ${target}
+        ${pkgs.systemd}/bin/systemctl --user restart mpdris2.service
+      ''
+    ))
   ];
 }
