06859c457917982aaa290e551db8ed8ed20f14d9
maralorn <mail@maralorn.de>
2024-01-15 15:15:23 +0100

    Fix nix-serve-ng

--- nixpkgs/previews.nix
+++ nixpkgs/previews.nix
@@ -4,7 +4,8 @@ final: _prev: {
   nix-output-monitor = final.flake-inputs'.nix-output-monitor.packages.default;
   inherit (final.unstable)
     nix # For testing with nix-output-monitor
-    nil # Because old nil with new nix does not get cached.
+    nil # lings against nix
+    nix-serve-ng # links against nix
     forgejo # I am apparently stuck here, because forgejo frequently receives
     # updates and when switching back to stable I run into database version
     # conflicts.
