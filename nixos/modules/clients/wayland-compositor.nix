e4cc99098f22cf6e22b6eb6888650d0c99b8c0ca
maralorn <mail@maralorn.de>
2024-01-17 19:35:43 +0100

    One less newline

--- nixos/modules/clients/wayland-compositor.nix
+++ nixos/modules/clients/wayland-compositor.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "launch-wayland" ''
-      XDG_CURRENT_DESKTOP=river exec river &>> /run/user/$UID/river.log
-    '')
+    (pkgs.writeShellScriptBin "launch-wayland" "XDG_CURRENT_DESKTOP=river exec river &>> /run/user/$UID/river.log"
+    )
   ];
   services.greetd.enable = true;
   programs.sway.enable = true; # For swaylock pam files …
