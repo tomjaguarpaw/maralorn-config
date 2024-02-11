bec73da9e165bea39823277cae96809aa86dcd43
maralorn <mail@maralorn.de>
2024-02-11 19:34:23 +0100

    Remove cgit

--- nixos/roles/hera/git.nix
+++ nixos/roles/hera/git.nix
@@ -42,69 +42,11 @@ let
             bracket (checkout path) (rm "-rf") $ \repoDir -> withCurrentDirectory repoDir $ nix_build "-o" ([i|/var/www/#{deploy}|] :: String) target
             say "Done"
       '';
-  cgitrc = ''
-    enable-git-config=1
-    remove-suffix=1
-    section-from-path=1
-    cache-size=1000
-
-    source-filter=${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py
-    about-filter=${pkgs.cgit}/lib/cgit/filters/about-formatting.sh
-    root-title=Maralorns Projects
-    root-desc=All my public projects.
-
-    snapshots=tar.gz zip
-
-    # Make packages cloneable
-    clone-url=https://git.maralorn.de/$CGIT_REPO_URL
-
-    # hide my name in index
-    enable-index-owner=0
-
-    # for look and feel
-    enable-index-links=1
-    enable-blame=1
-    enable-commit-graph=1
-    enable-follow-links=1
-    enable-log-filecount=1
-    enable-log-linecount=1
-    branch-sort=age
-    noplainemail=1
-    side-by-side-diffs=1
-
-    ##
-    ## List of common mimetypes
-    ##
-
-    mimetype.gif=image/gif
-    mimetype.html=text/html
-    mimetype.jpg=image/jpeg
-    mimetype.jpeg=image/jpeg
-    mimetype.pdf=application/pdf
-    mimetype.png=image/png
-    mimetype.svg=image/svg+xml
-
-    ## Search for these files in the root of the default branch of repositories
-    ## for coming up with the about page:
-
-    readme=:README.md
-    readme=:readme.md
-    readme=:README
-    readme=:readme
-
-    project-list=/var/lib/gitolite/projects.list
-    scan-path=/var/lib/gitolite/repositories
-  '';
 in
 {
   systemd.tmpfiles.rules = lib.mkAfter [
     "z ${gitoliteCfg.dataDir}/.ssh/id_ed25519 0600 ${gitoliteCfg.user} ${gitoliteCfg.group} - -"
-    "v /var/cache/cgit 0700 cgit ${gitoliteCfg.group} - -"
   ];
-  users.users.cgit = {
-    isSystemUser = true;
-    inherit (gitoliteCfg) group;
-  };
   systemd.services.gitolite-init.postStart = ''
     export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
     dir=$(mktemp -d)
@@ -129,37 +71,7 @@ in
       extraGitoliteRc = ''
         $RC{UMASK} = 0027;
         $RC{GIT_CONFIG_KEYS} = 'gitweb\..*';
-        push( @{$RC{ENABLE}}, 'cgit' );
       '';
-    };
-    fcgiwrap = {
-      enable = true;
-      user = "cgit";
-      inherit (gitoliteCfg) group;
-    };
-
-    nginx.virtualHosts."git.maralorn.de" = {
-      forceSSL = true;
-      enableACME = true;
-
-      locations = {
-        "~* ^.+.(css|png|ico)$" = {
-          root = "${pkgs.cgit}/cgit";
-        };
-
-        "/" = {
-          extraConfig = ''
-            include ${pkgs.nginx}/conf/fastcgi_params;
-            fastcgi_param CGIT_CONFIG ${pkgs.writeText "cgitrc" cgitrc};
-            fastcgi_param SCRIPT_FILENAME ${pkgs.cgit}/cgit/cgit.cgi;
-            fastcgi_split_path_info ^(/?)(.+)$;
-            fastcgi_param PATH_INFO $fastcgi_path_info;
-            fastcgi_param QUERY_STRING $args;
-            fastcgi_param HTTP_HOST $server_name;
-            fastcgi_pass unix:${config.services.fcgiwrap.socketAddress};
-          '';
-        };
-      };
     };
   };
 }
