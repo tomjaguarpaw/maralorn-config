9cb44719215149630d8f842bc1ec72309e74474b
maralorn <mail@maralorn.de>
2024-02-11 18:17:57 +0100

    Move chat to athene

--- home-manager/roles/clients/chat.nix
+++ home-manager/roles/clients/chat.nix
@@ -8,7 +8,7 @@
       element-desktop
       rocketchat-desktop
       ;
-    weechat = pkgs.writeShellScriptBin "weechat" "ssh -t hera 'TMUX_TMPDIR=/run/user/1000 tmux -L weechat attach'";
+    weechat = pkgs.writeShellScriptBin "weechat" "ssh -t athene 'TMUX_TMPDIR=/run/user/1000 tmux -L weechat attach'";
   };
   xdg.configFile."Element/config.json".text = builtins.toJSON { showLabsSettings = true; };
 }
