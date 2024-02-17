57cc4bb8dbeb9e02f259c1421d3d8ddd64aaea8c
maralorn <mail@maralorn.de>
2024-02-17 21:31:46 +0100

    Another attempt at the rollback script

--- nixos/roles/zeus/sysroot-rollback.hs
+++ nixos/roles/zeus/sysroot-rollback.hs
@@ -4,7 +4,6 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 
 import Control.Exception.Safe (catchAny)
-import GHC.IO.Encoding as Encoding
 import Relude
 import Say (say)
 import System.Directory (listDirectory, removePathForcibly)
@@ -15,8 +14,7 @@ dirsToKeep = ["nix", "disk"]
 
 main :: IO ()
 main = do
-  Encoding.setLocaleEncoding Encoding.utf8
-  say "Rolling back / by clearing everything but /nix and /disk …"
+  say "Rolling back / by clearing everything but /nix and /disk ..."
   entries <- listDirectory "/sysroot"
   let toDelete = filter (`notElem` dirsToKeep) entries
   forM_ toDelete \entry -> do
