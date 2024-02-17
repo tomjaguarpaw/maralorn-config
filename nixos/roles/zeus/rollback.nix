63a5a7a12e8c02b455338cfc5e0806656cf4da36
maralorn <mail@maralorn.de>
2024-02-17 20:29:10 +0100

    Try fixing zeus

--- nixos/roles/zeus/rollback.nix
+++ nixos/roles/zeus/rollback.nix
@@ -1,15 +1,22 @@
 { pkgs, ... }:
+let
+  script = pkgs.writers.writeHaskell "rollback" {
+    libraries = builtins.attrValues pkgs.myHaskellScriptPackages;
+  } (builtins.readFile ./sysroot-rollback.hs);
+in
 {
-  boot.initrd.systemd.services.rollback = {
-    description = "Delete everything but /nix and /disk on the root filesystem to get a fresh nixos install.";
-    wantedBy = [ "initrd-root-fs.target" ];
-    after = [ "sysroot.mount" ];
-    unitConfig.DefaultDependencies = "no";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writers.writeHaskell "rollback" {
-        libraries = builtins.attrValues pkgs.myHaskellScriptPackages;
-      } (builtins.readFile ./sysroot-rollback.hs);
+
+  boot.initrd.systemd = {
+    storePaths = [ script ];
+    services.rollback = {
+      description = "Delete everything but /nix and /disk on the root filesystem to get a fresh nixos install.";
+      wantedBy = [ "initrd-root-fs.target" ];
+      after = [ "sysroot.mount" ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = script;
+      };
     };
   };
 }
