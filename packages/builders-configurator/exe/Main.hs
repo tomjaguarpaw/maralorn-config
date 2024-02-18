5343f5208f1ad2a347f0ff596b4d06bb66bdb414
maralorn <mail@maralorn.de>
2024-02-18 17:12:17 +0100

    new builders

--- packages/builders-configurator/exe/Main.hs
+++ packages/builders-configurator/exe/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DerivingStrategies #-}
+
 module Main where
 
 import Control.Exception qualified as Exception
@@ -32,36 +34,76 @@ Hera         729
 
 -}
 
-remoteBuilder :: Text
+newtype Builder = MkBuilder Text
+  deriving newtype (IsString, Eq, Ord, Show)
+
+builder :: Builder -> Text
+builder = coerce
+
+newtype BuilderGroup = MkBuilderGroup (Set Builder)
+  deriving newtype (Eq, Ord, Show)
+
+builderList :: BuilderGroup -> [Builder]
+builderList = toList @Set . coerce
+
+remoteBuilder :: Builder
 remoteBuilder = "remote.builder"
 
-nixbuildDotNet :: Text
+remoteGroup :: BuilderGroup
+remoteGroup = MkBuilderGroup (one remoteBuilder)
+
+nixbuildDotNet :: Builder
 nixbuildDotNet = "nixbuild.net"
 
-zeusBuilder :: Text
-zeusBuilder = "zeus.builder"
+nixbuildGroup :: BuilderGroup
+nixbuildGroup = MkBuilderGroup (one nixbuildDotNet)
 
-atheneBuilder :: Text
-atheneBuilder = "athene.builder"
+zeusBuilder :: Builder
+zeusBuilder = "zeus.vpn.m-0.eu"
 
-builderInfos :: Map.Map Text Natural
+zeusGroup :: BuilderGroup
+zeusGroup = MkBuilderGroup (one zeusBuilder)
+
+cccda1 :: Builder
+cccda1 = "build1.darmstadt.ccc.de"
+
+cccda2 :: Builder
+cccda2 = "build2.darmstadt.ccc.de"
+
+cccda3 :: Builder
+cccda3 = "build3.darmstadt.ccc.de"
+
+cccda4 :: Builder
+cccda4 = "build4.darmstadt.ccc.de"
+
+cccdaGroup :: BuilderGroup
+cccdaGroup = MkBuilderGroup (fromList [cccda1, cccda2, cccda3, cccda4])
+
+builderInfos :: Map.Map Builder Natural
 builderInfos =
   Map.fromList
-    [ (remoteBuilder, 32)
+    [ (cccda1, 4)
+    , (cccda2, 4)
+    , (cccda3, 4)
+    , (cccda4, 2)
+    , (remoteBuilder, 32)
     , (nixbuildDotNet, 100)
     , (zeusBuilder, 12)
-    , (atheneBuilder, 2)
     ]
 
-builderConfigs :: Map.Map Text [(Text, Reachable)]
+mkConfig :: Bool -> [(BuilderGroup, Reachable)]
+mkConfig isZeus =
+  ([(zeusGroup, Check) | not isZeus])
+    <> [(cccdaGroup, Always), (remoteGroup, Always), (nixbuildGroup, Always)]
+
+builderConfigs :: Map.Map Text [(BuilderGroup, Reachable)]
 builderConfigs =
   Map.fromList
-    -- don’t use athene as remote builder, it’s not beefy enough
-    [ ("hera", [(zeusBuilder, Check), (atheneBuilder, Check), (remoteBuilder, Always), (nixbuildDotNet, Always)])
-    , ("apollo", [(zeusBuilder, Check), (atheneBuilder, Check), (remoteBuilder, Always), (nixbuildDotNet, Always)])
-    , ("hephaistos", [(zeusBuilder, Check), (atheneBuilder, Check), (remoteBuilder, Always), (nixbuildDotNet, Always)])
-    , ("athene", [(zeusBuilder, Check), (remoteBuilder, Always), (nixbuildDotNet, Always)])
-    , ("zeus", [(remoteBuilder, Always), (atheneBuilder, Check), (nixbuildDotNet, Always)])
+    [ ("hera", mkConfig False)
+    , ("apollo", mkConfig False)
+    , ("hephaistos", mkConfig False)
+    , ("athene", mkConfig False)
+    , ("zeus", mkConfig True)
     ]
 
 systems :: [Text]
@@ -73,7 +115,7 @@ supportedFeatures = ["benchmark", "big-parallel", "kvm", "nixos-test"]
 data Reachable = Always | Check
 
 data Ping :: Effect where
-  CheckConnectivity :: Text -> Ping m Bool
+  CheckConnectivity :: Builder -> Ping m Bool
 
 makeEffect ''Ping
 
@@ -84,37 +126,36 @@ runWithoutConnectivity = Eff.interpret $ \_ -> \case
 runWithPing :: (Eff.IOE :> es) => Eff (Ping : es) a -> Eff es a
 runWithPing = Eff.interpret $ \_ -> \case
   CheckConnectivity host_name -> liftIO $ Exception.handle (\(_ :: SomeException) -> pure False) do
-    let reqUrl = Req.http (sshHostToDNS host_name)
+    let reqUrl = Req.http (builder host_name)
     response <- (Req.runReq Req.defaultHttpConfig $ Req.req Req.GET reqUrl Req.NoReqBody Req.lbsResponse (Req.responseTimeout 500_000))
     let status = Req.responseStatusCode response
     pure $ status >= 200 && status < 300
 
-sshHostToDNS :: Text -> Text
-sshHostToDNS = \case
-  b | b == zeusBuilder -> "zeus.vpn.m-0.eu"
-  b | b == atheneBuilder -> "athene.vpn.m-0.eu"
-  host -> error [i|No dns name none for ssh host #{host}|]
-
 commaList :: [Text] -> Text
 commaList = Text.intercalate ","
 
-builderLine :: (Text, Natural, Natural) -> Text
-builderLine (hostName, maxJobs, speed_factor) = [i|ssh-ng://#{hostName} #{commaList systems} - #{maxJobs} #{speed_factor} #{commaList supportedFeatures} - -|]
+builderLine :: Builder -> Natural -> Natural -> Text
+builderLine hostName maxJobs speed_factor = [i|ssh-ng://#{builder hostName} #{commaList systems} - #{maxJobs} #{speed_factor} #{commaList supportedFeatures} - -|]
 
-testBuilders :: (Ping :> es) => [(Text, Reachable)] -> Eff es [Text]
+testBuilders :: (Ping :> es) => [(BuilderGroup, Reachable)] -> Eff es [BuilderGroup]
 testBuilders =
   fmap (fmap fst) . filterM \case
     (_, Always) -> pure True
-    (host, Check) -> checkConnectivity host
+    (hosts, Check) -> andM (checkConnectivity <$> builderList hosts)
 
-printBuilders :: [Text] -> Text
-printBuilders = Text.unlines . fmap builderLine . Foldable.foldr' folder []
+printBuilders :: [BuilderGroup] -> Text
+printBuilders = Text.unlines . (uncurry flatten_group <=< Foldable.foldr' folder [])
  where
-  folder :: Text -> [(Text, Natural, Natural)] -> [(Text, Natural, Natural)]
-  folder builder_name collected_lines = next_entry : collected_lines
+  flatten_group :: [(Builder, Natural)] -> Natural -> [Text]
+  flatten_group = \builder_group speed_factor ->
+    builder_group <&> \(host, max_jobs) -> builderLine host max_jobs speed_factor
+  folder :: BuilderGroup -> [([(Builder, Natural)], Natural)] -> [([(Builder, Natural)], Natural)]
+  folder builder_group collected_lines = next_entry : collected_lines
    where
-    next_entry = (builder_name, max_jobs, maybe 1 (\(_, _, x) -> max_jobs * x) (viaNonEmpty head collected_lines))
-    max_jobs = fromMaybe (error [i|#{builder_name} not found in builderInfos.|]) $ Map.lookup builder_name builderInfos
+    next_entry = (group_jobs, maybe 1 (\(_, x) -> max_max_jobs * x) (viaNonEmpty head collected_lines))
+    group_jobs = builderList builder_group <&> \name -> (name, max_jobs name)
+    max_max_jobs = Foldable.maximum (1 : (snd <$> group_jobs))
+    max_jobs name = fromMaybe (error [i|#{name} not found in builderInfos.|]) $ Map.lookup name builderInfos
 
 main :: IO ()
 main = do
@@ -126,7 +167,7 @@ main = do
         [host', "--force"] -> (into host', False, False)
         [host', "--without-connection"] -> (into host', True, False)
         _ -> error [i|Unknown arguments: #{args}|]
-      builder_tries :: (Ping :> es) => Eff es [Text]
+      builder_tries :: (Ping :> es) => Eff es [BuilderGroup]
       builder_tries = testBuilders $ fromMaybe (error [i|#{host} not found in builderConfigs.|]) $ Map.lookup (into host) builderConfigs
   builders <- if allow_empty && host `elem` ["zeus", "hephaistos"] then pure [] else Eff.runEff $ (if withoutConnection then runWithoutConnectivity else runWithPing) builder_tries
   (path, handle) <- IO.openTempFile "/tmp" "machines"
