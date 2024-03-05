187fd2c5ba8db6ee165eb66cca60ba5568368eb4
maralorn <mail@maralorn.de>
2024-03-06 00:32:35 +0100

    Configure kvm-switching

---
+++ nixos/roles/zeus/kvm-switch.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, ... }:
+let
+  mkMonitorline = action: monitor: ''
+    ACTION=="${action}", ENV{DEVTYPE}=="usb_device", ENV{PRODUCT}=="3496/6/100", RUN+="${lib.getExe pkgs.ddcutil} -l \"DELL S2721QS\" setvcp 60 ${monitor}"
+  '';
+in
+{
+
+  services.udev.extraRules = lib.concatStringsSep "\n" [
+    (mkMonitorline "add" "15") # DP
+    (mkMonitorline "remove" "17") # HDM11
+  ];
+  hardware.i2c.enable = true;
+}
