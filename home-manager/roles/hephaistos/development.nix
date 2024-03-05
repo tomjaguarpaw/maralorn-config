e2e253de4103f27a62394b247e4dda3028bfd9e6
maralorn <mail@maralorn.de>
2024-03-05 15:21:54 +0100

    pack/unpack

--- home-manager/roles/hephaistos/development.nix
+++ home-manager/roles/hephaistos/development.nix
@@ -1,6 +1,18 @@
 { pkgs, ... }:
 {
   home.packages = [
+    (pkgs.writeShellScriptBin "unpack" ''
+      ob --no-handoff thunk unpack "$1"
+      cd "$1"
+      git config user.name "Malte Ott"
+      git config user.email "mott@heilmannsoftware.de"
+      sd "https://gitlab.heilmannsoftware.net" "ssh://git@gitlab.heilmannsoftware.net" .git/config
+
+    '')
+    (pkgs.writeShellScriptBin "pack" ''
+      sd "ssh://git@gitlab.heilmannsoftware.net" "https://gitlab.heilmannsoftware.net" "$1/.git/config"
+      ob --no-handoff thunk pack "$1"
+    '')
     pkgs.remmina
     pkgs.mdbtools
     (pkgs.writeShellScriptBin "for-model" ''
