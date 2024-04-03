716cc8ca05ae4e92dccc783f09c6edfb48298f38
maralorn <mail@maralorn.de>
2024-04-03 16:25:59 +0200

    Preview helix

--- nixpkgs/previews.nix
+++ nixpkgs/previews.nix
@@ -5,10 +5,11 @@ final: _prev: {
   inherit (final.unstable)
     eww
     forgejo # I am apparently stuck here, because forgejo frequently receives
-    forgejo-actions-runner
-    nixfmt-rfc-style
     # updates and when switching back to stable I run into database version
     # conflicts.
+    forgejo-actions-runner
+    nixfmt-rfc-style
+    helix
     klog-time-tracker # not in 23.11
     ;
 }
