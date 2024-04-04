723db0c0a8f3bb1a6941053559e889ad28614267
maralorn <mail@maralorn.de>
2024-04-04 17:09:41 +0200

    Udptae cache

--- nixos/roles/hephaistos/hs-substituters.nix
+++ nixos/roles/hephaistos/hs-substituters.nix
@@ -1,12 +1,12 @@
 {
   nix.settings = {
     substituters = [
-      "http://nixcache.hs.local"
       "https://nixcache.reflex-frp.org"
+      "http://nixcache.heilmannsoftware.net"
     ];
     trusted-public-keys = [
-      "nixcache.hs.local-1:NZ7btgoWdlaD6+GRHW7VhQ+gwg5lAVqFmElCQrQHS4w="
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+      "nixcache.heilmannsoftware.net:5kPz/A5gVCs8E96y8PlT10sdAJtAQ5n4n3E0j7UXGQA="
     ];
   };
 }
