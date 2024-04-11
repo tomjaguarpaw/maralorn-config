e5b742a2c88c2c9ef1a23338103fc1a45f038d7b
maralorn <mail@maralorn.de>
2024-04-11 10:07:36 +0200

    Less annoying night-warn

--- home-manager/roles/clients/night-warn.nix
+++ home-manager/roles/clients/night-warn.nix
@@ -5,6 +5,10 @@
       Timer.OnCalendar = "23:00";
       Install.WantedBy = [ "timers.target" ];
     };
-    services.night-warn.Service.ExecStart = ''${lib.getExe pkgs.set-timer} Wecker "tomorrow 8:45"'';
+    services.night-warn.Service.ExecStart = pkgs.writeShellScript "set-timer" ''
+      if [[ "$(date +%H)" == "23" ]]; then
+        ${lib.getExe pkgs.set-timer} Wecker "tomorrow 8:45"
+      fi
+    '';
   };
 }
