9b486885ebbac172d30abe978b80105d41742e99
maralorn <mail@maralorn.de>
2024-05-30 06:22:32 +0200

    Make merge-bot repo independent

--- packages/merge-bot/exe/Main.hs
+++ packages/merge-bot/exe/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import Control.Concurrent (threadDelay)
-import Data.Aeson
+import Data.Aeson hiding (Error, Success)
 import Data.Aeson.Optics
 import Data.String.Interpolate
 import Data.Text qualified as Text
@@ -17,63 +17,142 @@ loadCredential name = do
   credentials_directory <- System.getEnv "CREDENTIALS_DIRECTORY"
   Text.strip . decodeUtf8 <$> readFileBS (credentials_directory <> "/" <> name)
 
+data PR = MkPR
+  { repo :: Text
+  , num :: Integer
+  , base :: Text
+  , head :: Text
+  , headCommit :: Text
+  }
+
+data CIState = Pending | Success | Failure | Error deriving stock (Show)
+
+data Commit = MkCommit
+  { state :: CIState
+  , lastUpdate :: Text
+  }
+
+data Config = MkConfig
+  { wreqOptions :: Wreq.Options
+  , forgejoUrl :: Text
+  }
+
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
   [config_path] <- getArgs
-  config <- readFileBS config_path
+  config_json <- readFileBS config_path
   token <- loadCredential "merge_bot_token"
-  let url = config ^?! key "server" % _String
-      repos = config ^.. key "repos" % values % _String
-      botname = config ^?! key "botname" % filteredBy _String
-      opts = defaults & lensVL auth ?~ oauth2Bearer (encodeUtf8 token)
+  let forgejoUrl = config_json ^?! key "server" % _String
+      config =
+        MkConfig
+          { wreqOptions = defaults & lensVL auth ?~ oauth2Bearer (encodeUtf8 token)
+          , forgejoUrl
+          }
   forever do
-    for_ repos \repo -> do
-      let repo_api = [i|#{url}/api/v1/repos/#{repo}|] :: Text
-      putTextLn [i|Fetching open PRs from #{url}/#{repo}|]
-      assigned_prs_resp <- getWith opts [i|#{repo_api}/pulls?state=open|]
-      let prs = assigned_prs_resp ^.. lensVL responseBody % values % filteredBy (key "assignee" % key "login" % only botname)
-      forM_ prs \pr -> do
-        let num = pr ^?! key "number" % _Integer
-            pr_base = pr ^?! key "base" % key "ref" % _String
-            pr_head = pr ^?! key "head" % key "ref" % _String
-            pr_head_commit = pr ^?! key "head" % key "sha" % _String
-        commit_state_resp <- getWith opts [i|#{repo_api}/commits/#{pr_head_commit}/status|]
-        putText [i|\##{num}: From #{pr_head} into #{pr_base}: |]
-        let commit_state_body = commit_state_resp ^. lensVL responseBody
-        case commit_state_body ^?! key "state" % _String of
-          "success" -> do
-            putText "CI success, "
-            mergePR opts repo_api num pr_head_commit
-          "" -> putTextLn "CI pending."
-          "pending" -> putTextLn "CI pending."
-          x -> do
-            let timestamp =
-                  Text.replace "+" "%2B" $
-                    fromMaybe "updated at timestamp" $
-                      maximumOf (key "statuses" % values % key "updated_at" % _String) commit_state_body
-            putTextLn [i|CI #{x}.|]
-            comments_resp <- getWith opts [i|#{repo_api}/issues/#{num}/comments?since=#{timestamp}|]
-            when (hasn't (lensVL responseBody % values) comments_resp) do
-              putTextLn "Adding comment"
-              void $
-                postWith
-                  opts
-                  [i|#{repo_api}/issues/#{num}/comments|]
-                  (object ["body" .= ("CI failed" :: Text)])
+    putTextLn [i|Fetching open PRs on #{forgejoUrl} assigned to token owner …|]
+    prs <- fetchPRs config
+    forM_ prs \pr -> do
+      let num = pr.num
+          pr_head = pr.head
+          pr_base = pr.base
+      putText [i|\##{num}: From #{pr_head} into #{pr_base}: |]
+      commit <- fetchHeadCommit config pr
+      case commit.state of
+        Success -> do
+          putText "CI success, "
+          mergePR config pr
+        Pending -> putTextLn "CI pending."
+        x -> do
+          let msg = [i|CI #{x}.|]
+          putTextLn msg
+          unlessM (hasRecentComments config pr commit.lastUpdate) $ putComment config pr msg
     threadDelay 60_000_000
 
-mergePR :: Wreq.Options -> Text -> Integer -> Text -> IO ()
-mergePR opts repo_api num head_commit = do
+mergePR :: Config -> PR -> IO ()
+mergePR config pr = do
   _ <-
     postWith
-      opts
-      [i|#{repo_api}/pulls/#{num}/merge|]
+      config.wreqOptions
+      [i|#{prUrl config pr}/merge|]
       ( object
           [ "Do" .= ("fast-forward-only" :: Text)
           , "delete_branch_after_merge" .= True
-          , "head_commit_id" .= head_commit
+          , "head_commit_id" .= pr.headCommit
           ]
       )
   putTextLn "Merged."
+
+putComment :: Config -> PR -> Text -> IO ()
+putComment config pr comment = do
+  putTextLn [i|Adding comment: #{comment}|]
+  void $ postWith config.wreqOptions [i|#{issueUrl config pr}/comments|] (object ["body" .= comment])
+
+hasRecentComments :: Config -> PR -> Text -> IO Bool
+hasRecentComments config pr timestamp = do
+  let timestamp' = Text.replace "+" "%2B" timestamp
+      num = pr.num
+  comments_resp <- getWith config.wreqOptions [i|#{repoUrl config pr }/issues/#{num}/comments?since=#{timestamp'}|]
+  pure $ has (lensVL responseBody % values) comments_resp
+
+fetchHeadCommit :: Config -> PR -> IO Commit
+fetchHeadCommit config pr = do
+  commit_state_resp <- getWith config.wreqOptions [i|#{repoUrl config pr}/commits/#{commit_id}/status|]
+  let commit_state_body = commit_state_resp ^. lensVL responseBody
+  pure $
+    MkCommit
+      { state =
+          commit_state_body
+            ^?! key "state"
+            % _String
+            % to
+              ( \case
+                  "" -> Just Pending
+                  "success" -> Just Success
+                  "pending" -> Just Pending
+                  "error" -> Just Error
+                  "failure" -> Just Failure
+                  _ -> Nothing
+              )
+            % _Just
+      , lastUpdate =
+          fromMaybe "updated at timestamp" $
+            maximumOf (key "statuses" % values % key "updated_at" % _String) commit_state_body
+      }
+ where
+  commit_id = pr.headCommit
+
+fetchPRs :: Config -> IO [PR]
+fetchPRs config =
+  getWith config.wreqOptions [i|#{apiUrl config}/repos/issues/search?state=open&type=pulls&assigned=true|]
+    <&> toListOf
+      ( lensVL responseBody % values % to \pr ->
+          MkPR
+            { repo = pr ^?! key "repository" % key "full_name" % _String
+            , num = pr ^?! key "number" % _Integer
+            , base = pr ^?! key "base" % key "ref" % _String
+            , head = pr ^?! key "head" % key "ref" % _String
+            , headCommit = pr ^?! key "head" % key "sha" % _String
+            }
+      )
+
+apiUrl :: Config -> Text
+apiUrl config = [i|#{url}/api/v1|]
+ where
+  url = config.forgejoUrl
+
+repoUrl :: Config -> PR -> Text
+repoUrl config pr = [i|#{apiUrl config}/#{repo}|]
+ where
+  repo = pr.repo
+
+prUrl :: Config -> PR -> Text
+prUrl config pr = [i|#{prUrl config pr}/pulls/#{num}|]
+ where
+  num = pr.num
+
+issueUrl :: Config -> PR -> Text
+issueUrl config pr = [i|#{prUrl config pr}/issues/#{num}|]
+ where
+  num = pr.num
