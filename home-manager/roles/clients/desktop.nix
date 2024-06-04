185726f99b9082d93a35334be4a00375292523fd
maralorn <mail@maralorn.de>
2024-06-05 01:13:30 +0200

    Nix refactor

--- home-manager/roles/clients/desktop.nix
+++ home-manager/roles/clients/desktop.nix
@@ -1,69 +1,67 @@
 { pkgs, ... }:
 {
-  home = {
-    # This fixes border drawing but makes neo wonky. sessionVariables.NIXOS_OZONE_WL = "1";
-    packages = builtins.attrValues {
-      zoom = pkgs.zoom-us.overrideAttrs (old: {
-        postFixup =
-          old.postFixup
-          + ''
-            wrapProgram $out/bin/zoom-us --unset XDG_SESSION_TYPE
-            wrapProgram $out/bin/zoom --unset XDG_SESSION_TYPE
-          '';
-      });
-      mic-check = pkgs.writeShellScriptBin "mic-check" ''
-        echo "Activating loopback!"
-        ${pkgs.pulseaudio}/bin/pactl load-module module-loopback
-        echo "Can your hear yourself? Fix audio setup! Then press enter …"
-        read
-        echo "Deactivating loopback!"
-        ${pkgs.pulseaudio}/bin/pactl unload-module module-loopback
-        echo "Continuing …"
-      '';
+  # This fixes border drawing but makes neo wonky. sessionVariables.NIXOS_OZONE_WL = "1";
+  home.packages = builtins.attrValues {
+    zoom = pkgs.zoom-us.overrideAttrs (old: {
+      postFixup =
+        old.postFixup
+        + ''
+          wrapProgram $out/bin/zoom-us --unset XDG_SESSION_TYPE
+          wrapProgram $out/bin/zoom --unset XDG_SESSION_TYPE
+        '';
+    });
+    mic-check = pkgs.writeShellScriptBin "mic-check" ''
+      echo "Activating loopback!"
+      ${pkgs.pulseaudio}/bin/pactl load-module module-loopback
+      echo "Can your hear yourself? Fix audio setup! Then press enter …"
+      read
+      echo "Deactivating loopback!"
+      ${pkgs.pulseaudio}/bin/pactl unload-module module-loopback
+      echo "Continuing …"
+    '';
 
-      inherit (pkgs.gnome) nautilus;
-      inherit (pkgs.xorg) xbacklight;
-      inherit (pkgs)
-        # web
-        chromium
-        mumble
-        upower
-        speedtest-cli
-        acpi
+    inherit (pkgs.gnome) nautilus;
+    inherit (pkgs.xorg) xbacklight;
+    inherit (pkgs)
+      # web
+      chromium
+      mumble
+      upower
+      speedtest-cli
+      acpi
 
-        # tools & office
-        klog-time-tracker
-        feh
-        gimp
-        imagemagick
-        libreoffice-fresh
-        xournal
-        musescore
-        handbrake
-        evince
-        abcde
-        zbar
-        graphia
-        pdfpc
+      # tools & office
+      klog-time-tracker
+      feh
+      gimp
+      imagemagick
+      libreoffice-fresh
+      xournal
+      musescore
+      handbrake
+      evince
+      abcde
+      zbar
+      graphia
+      pdfpc
 
-        # media
-        deluge
-        vlc
-        esphome # To flash devices
-        esptool # provides esptool.py
+      # media
+      deluge
+      vlc
+      esphome # To flash devices
+      esptool # provides esptool.py
 
-        lm_sensors
-        xwayland
-        xdg_utils
-        libnotify
-        shotcut
-        audacity
-        wl-clipboard
-        dconf2nix
-        chrysalis
-        ;
-      inherit (pkgs.gnome) dconf-editor;
-    };
+      lm_sensors
+      xwayland
+      xdg_utils
+      libnotify
+      shotcut
+      audacity
+      wl-clipboard
+      dconf2nix
+      chrysalis
+      ;
+    inherit (pkgs.gnome) dconf-editor;
   };
   gtk = {
     enable = true;
