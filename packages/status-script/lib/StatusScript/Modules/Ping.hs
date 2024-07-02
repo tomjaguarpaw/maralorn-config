1086df04af6608b0f8d194a417db78ff3da847a0
maralorn <mail@maralorn.de>
2024-07-02 17:41:56 +0200

    Fix warnings in Ping module

--- packages/status-script/lib/StatusScript/Modules/Ping.hs
+++ packages/status-script/lib/StatusScript/Modules/Ping.hs
@@ -5,19 +5,22 @@ import Reflex qualified as R
 import Reflex.Host.Headless qualified as R
 import Shh ((&>))
 import Shh qualified
-import StatusScript.Env (Env (..))
-import StatusScript.ReflexUtil qualified as ReflexUtil
-import StatusScript.Warnings (Warning (..))
+import StatusScript.CommandUtil
+import StatusScript.Env
+import StatusScript.ReflexUtil
+import StatusScript.Warnings
 
 hosts :: [Text]
 hosts = ["hera", "athene"]
 
 Shh.load Shh.Absolute ["ping"]
 
+missingExecutables :: IO [FilePath]
 ping' :: R.MonadHeadlessApp t m => Env -> m (R.Event t [Warning])
 ping' = \env -> do
-  tick <- ReflexUtil.tickEvent 15
-  ReflexUtil.performEventThreaded env tick \_ -> do
+  reportMissing missingExecutables
+  tick <- tickEvent 15
+  performEventThreaded env tick \_ -> do
     unreachable_hosts <- flip filterM hosts \host -> isLeft <$> (Shh.tryFailure do (ping "-c" "1" (toString host)) &> Shh.devNull)
     pure $
       unreachable_hosts
