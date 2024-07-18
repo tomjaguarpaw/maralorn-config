ca9f493dff2a7f347840338d699bf72f21e176ed
maralorn <mail@maralorn.de>
2024-07-18 11:12:14 +0200

    Use a helix version with code lenses

--- nixpkgs/previews.nix
+++ nixpkgs/previews.nix
@@ -5,7 +5,7 @@ final: _prev: {
   inherit (final.flake-inputs'.nixos-oldstable.legacyPackages) ddcutil;
   inherit (final.unstable) jujutsu;
   helix =
-    # PR https://github.com/helix-editor/helix/pull/6417
-    (builtins.getFlake "github:helix-editor/helix/107cdf3e43fef79d36a0bdc8b59327abe85472c9")
+    # PR https://github.com/helix-editor/helix/pull/5063
+    (builtins.getFlake "github:helix-editor/helix/3e838fff106f9dc5bef37b691ade97fb4cd885b4")
     .packages.x86_64-linux.default;
 }
