16ad382fd0471fafd2dac7fe515cd7e6a398294b
maralorn <mail@maralorn.de>
2024-08-15 22:56:28 +0200

    Drop 5etools update

--- nixos/roles/athene/5etools.nix
+++ nixos/roles/athene/5etools.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, ... }:
 let
   inherit (config.m-0) virtualHosts;
 in
@@ -12,17 +7,17 @@ in
 
   services.nginx.virtualHosts.${virtualHosts."5e"}.locations."/".root = "/var/www/5etools";
 
-  systemd.services.update-5etools = {
-    script = ''
-      cd /var/www/5etools
-      ${lib.getExe pkgs.git} config user.email "mail@maralorn.de"
-      ${lib.getExe pkgs.git} config user.name "maralorn"
-      if [[ -d ".git" ]]; then
-         ${lib.getExe pkgs.git} pull -r
-      else
-         ${lib.getExe pkgs.git} clone https://github.com/5etools-mirror-2/5etools-mirror-2.github.io.git .
-      fi
-    '';
-    startAt = "daily";
-  };
+  # systemd.services.update-5etools = {
+  #   script = ''
+  #     cd /var/www/5etools
+  #     ${lib.getExe pkgs.git} config user.email "mail@maralorn.de"
+  #     ${lib.getExe pkgs.git} config user.name "maralorn"
+  #     if [[ -d ".git" ]]; then
+  #        ${lib.getExe pkgs.git} pull -r
+  #     else
+  #        ${lib.getExe pkgs.git} clone https://github.com/5etools-mirror-2/5etools-mirror-2.github.io.git .
+  #     fi
+  #   '';
+  #   startAt = "daily";
+  # };
 }
