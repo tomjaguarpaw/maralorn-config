4775971dd6e58fce937bf6dd660e99f754aad509
maralorn <mail@maralorn.de>
2024-09-23 15:57:51 +0200

    Dont use Last-Modified because GitHub screws up Cache Invalidation

--- packages/status-script/lib/StatusScript/Modules/GitHub.hs
+++ packages/status-script/lib/StatusScript/Modules/GitHub.hs
@@ -4,8 +4,8 @@ import Control.Concurrent (threadDelay)
 import Data.Aeson (FromJSON, eitherDecode')
 import Data.String.Interpolate (i)
 import Data.Text qualified as Text
-import Data.Time (NominalDiffTime, UTCTime, defaultTimeLocale, formatTime, parseTimeM)
-import Network.Wreq (checkResponse, defaults, header, responseHeader, responseStatus, statusCode)
+import Data.Time (NominalDiffTime)
+import Network.Wreq (checkResponse, defaults, header, responseHeader, responseStatus)
 import Network.Wreq qualified as Wreq
 import Network.Wreq.Lens (responseBody)
 import Optics
@@ -54,11 +54,8 @@ getToken :: IO Text
 getToken =
   Text.strip . decodeUtf8 <$> (Shh.exe "rbw" "get" "github.com" "-f" "kass" Shh.|> Shh.captureTrim)
 
-modifiedFormatString :: String
-modifiedFormatString = "%a, %d %b %Y %X GMT"
-
-getNotifications :: Maybe UTCTime -> Maybe NominalDiffTime -> IO (Maybe UTCTime, NominalDiffTime, Maybe [Notification])
-getNotifications last_modified old_poll_interval = do
+getNotifications :: Maybe NominalDiffTime -> IO (NominalDiffTime, [Notification])
+getNotifications old_poll_interval = do
   whenJust old_poll_interval $ threadDelay . floor . (* 1_000_000)
   token <- getToken
   response <-
@@ -67,35 +64,21 @@ getNotifications last_modified old_poll_interval = do
           & lensVL (header "Authorization") .~ [[i|Bearer #{token}|]]
           --          & lensVL (param "all") .~ ["true"]
           & lensVL checkResponse ?~ (\_ _ -> pure ())
-          & maybe
-            id
-            ( \ts ->
-                lensVL (header "If-Modified-Since")
-                  .~ [encodeUtf8 (formatTime defaultTimeLocale modifiedFormatString ts)]
-            )
-            last_modified
       )
       "https://api.github.com/notifications"
-  let new_last_modified =
-        response
-          ^? pre (foldVL (responseHeader "Last-Modified"))
-          % to decodeUtf8
-          % to (parseTimeM True defaultTimeLocale modifiedFormatString)
-          % _Just
-      poll_interval = response ^? pre (foldVL (responseHeader "X-Poll-Interval")) % to decodeUtf8 % to readMaybe % _Just
-      not_modified = response ^. lensVL (responseStatus . statusCode) == 304
-  pure
-    ( if not_modified then last_modified else new_last_modified
-    , fromMaybe 60 $ (fromInteger <$> poll_interval) <|> old_poll_interval
-    , if not_modified
-        then Nothing
-        else either (error . toText) id $ eitherDecode' $ response ^. lensVL responseBody
-    )
+  let
+    poll_interval = response ^? pre (foldVL (responseHeader "X-Poll-Interval")) % to decodeUtf8 % to readMaybe % _Just
+  let retval =
+        ( fromMaybe 60 $ (fromInteger <$> poll_interval) <|> old_poll_interval
+        , either (error . toText) id $ eitherDecode' $ response ^. lensVL responseBody
+        )
+  print (response ^. lensVL responseStatus, retval)
+  pure retval
 
 watchNotifications :: ([Notification] -> IO ()) -> IO ()
-watchNotifications cb = go Nothing Nothing
+watchNotifications cb = go Nothing
  where
-  go prev_last prev_interv = do
-    (last', interv', res) <- getNotifications prev_last prev_interv
-    whenJust res cb
-    go last' (Just interv')
+  go prev_interv = do
+    (interv', res) <- getNotifications prev_interv
+    cb res
+    go (Just interv')
