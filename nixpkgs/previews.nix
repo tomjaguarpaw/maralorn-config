897dc171ac48536cedd2574b8debce115214400f
maralorn <mail@maralorn.de>
2024-10-22 22:56:59 +0200

    Factorio

--- nixpkgs/previews.nix
+++ nixpkgs/previews.nix
@@ -4,6 +4,11 @@ final: _prev: {
   nix-output-monitor = final.flake-inputs'.nix-output-monitor.packages.default;
   inherit (final.flake-inputs'.nixos-oldstable.legacyPackages) ddcutil;
   inherit (final.unstable) jujutsu;
+  factorio =
+    (import (builtins.getFlake "github:NixOS/nixpkgs/ed88329f5f751972acf4309e084e66ccff62066d") {
+      config.allowUnfree = true;
+      system = "x86_64-linux";
+    }).factorio-space-age;
   helix =
     # PR https://github.com/helix-editor/helix/pull/5063
     (builtins.getFlake "github:helix-editor/helix/3e838fff106f9dc5bef37b691ade97fb4cd885b4")
