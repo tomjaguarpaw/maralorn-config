e0c301bb240bf98313ae12def561bc1a054aaec2
maralorn <mail@maralorn.de>
2024-10-25 15:50:12 +0200

    Fix localhost ip lookup

--- nixos/roles/all/network.nix
+++ nixos/roles/all/network.nix
@@ -13,31 +13,37 @@ in
     nftables.enable = true; # Uses firewall variables since 23.05
     useNetworkd = true;
     useDHCP = false; # enabled per interface
-    hosts = lib.zipAttrs (
-      lib.mapAttrsToList (
-        host: ip:
-        if builtins.typeOf ip == "set" then
+    hosts =
+      let
+        replaceLocalHost =
+          host: ips:
+          if host == hostName then
+            {
+              AAAA = "::1";
+              A = "127.0.0.1";
+            }
+          else
+            ips;
+      in
+      lib.zipAttrs (
+        lib.mapAttrsToList (host: ips: {
+          ${(replaceLocalHost host ips).AAAA or null} = "${host} ${host}.m-0.eu";
+          ${(replaceLocalHost host ips).A or null} = "${host} ${host}.m-0.eu";
+        }) config.m-0.hosts
+        ++ lib.mapAttrsToList (
+          host: ips:
+          let
+            mkHost = name: "${name} ${name}.maralorn.de";
+            name = "${host} ${host}.vpn.m-0.eu ${
+              lib.concatMapStringsSep " " mkHost config.m-0.hosts.aliases.${host} or [ ]
+            }";
+          in
           {
-            ${ip.AAAA or null} = "${host} ${host}.m-0.eu";
-            ${ip.A or null} = "${host} ${host}.m-0.eu";
+            ${(replaceLocalHost host ips).AAAA} = name;
+            ${(replaceLocalHost host ips).A} = name;
           }
-        else
-          { "${ip}" = "${host} ${host}.m-0.eu"; }
-      ) config.m-0.hosts
-      ++ lib.mapAttrsToList (
-        host: ips:
-        let
-          mkHost = name: "${name} ${name}.maralorn.de";
-          name = "${host} ${host}.vpn.m-0.eu ${
-            lib.concatMapStringsSep " " mkHost config.m-0.hosts.aliases.${host} or [ ]
-          }";
-        in
-        {
-          ${ips.AAAA} = name;
-          ${ips.A} = name;
-        }
-      ) config.m-0.hosts.tailscale
-    );
+        ) config.m-0.hosts.tailscale
+      );
   };
   systemd.network.wait-online.anyInterface = true;
   m-0.virtualHosts = lib.genAttrs (hosts.aliases.${hostName} or [ ]) (name: "${name}.maralorn.de");
