d159fdf36d4d843b6d703c5522a994504ef8cfe3
maralorn <mail@maralorn.de>
2024-11-19 23:14:20 +0100

    Disable element patch

--- nixpkgs/element-web/default.nix
+++ nixpkgs/element-web/default.nix
@@ -1,13 +1,13 @@
-_final: prev: {
-  element-web = prev.element-web.overrideAttrs (_: {
-    postConfigure = ''
-      patch node_modules/matrix-react-sdk/src/components/views/rooms/RoomList.tsx ${./RoomList.tsx.patch}
-      cp ${./orville_communicator.opus} node_modules/matrix-react-sdk/res/media/message.ogg
-    '';
-    preInstall = ''
-      find . -name 'bundle.css'
-      bundlecss=$(find . -name 'bundle.css')
-      cat ${./user.css} >> $bundlecss
-    '';
-  });
+_final: _prev: {
+  #element-web = prev.element-web.overrideAttrs (_: {
+  #  postConfigure = ''
+  #    patch node_modules/matrix-react-sdk/src/components/views/rooms/RoomList.tsx ${./RoomList.tsx.patch}
+  #    cp ${./orville_communicator.opus} node_modules/matrix-react-sdk/res/media/message.ogg
+  #  '';
+  #  preInstall = ''
+  #    find . -name 'bundle.css'
+  #    bundlecss=$(find . -name 'bundle.css')
+  #    cat ${./user.css} >> $bundlecss
+  #  '';
+  #});
 }
