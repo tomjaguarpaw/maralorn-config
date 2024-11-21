b429f8af5315484c09a4ad75604dc99952c65b45
maralorn <mail@maralorn.de>
2024-11-21 23:43:18 +0100

    Disable rollback

--- nixos/roles/zeus/rollback.nix
+++ nixos/roles/zeus/rollback.nix
@@ -1,26 +1,28 @@
-{ pkgs, ... }:
-let
-  script = pkgs.writers.writeHaskell "rollback" {
-    libraries = builtins.attrValues pkgs.myHaskellScriptPackages;
-  } (builtins.readFile ./sysroot-rollback.hs);
-in
-{
-
-  boot.initrd.systemd = {
-    storePaths = [ script ];
-    services.rollback = {
-      description = "Rollback File System Root";
-      wantedBy = [ "basic.target" ];
-      before = [
-        "initrd-root-fs.target"
-        "sysroot-var-lib-nixos.mount"
-      ];
-      after = [ "sysroot.mount" ];
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = script;
-      };
-    };
-  };
-}
+_: { }
+#{ pkgs, ... }:
+#let
+#  script = pkgs.writers.writeHaskell "rollback" {
+#    libraries = builtins.attrValues pkgs.myHaskellScriptPackages;
+#  } (builtins.readFile ./sysroot-rollback.hs);
+#in
+#{
+#
+#  boot.initrd.systemd = {
+#    storePaths = [ script ];
+#    services.rollback = {
+#      description = "Rollback File System Root";
+#      wantedBy = [ "basic.target" ];
+#      before = [
+#        "initrd-root-fs.target"
+#        "sysroot-var-lib-nixos.mount"
+#      ];
+#      after = [ "sysroot.mount" ];
+#      unitConfig.DefaultDependencies = "no";
+#      serviceConfig = {
+#        Type = "oneshot";
+#        ExecStart = script;
+#      };
+#    };
+#  };
+#}
+#
