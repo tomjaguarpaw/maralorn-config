9a4a81826437dc217e32b428ee1d4da0621279c6
maralorn <mail@maralorn.de>
2024-11-25 10:22:51 +0100

    Create zfs iso with new kernel

--- iso.nix
+++ iso.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }:
 {
 
-  nixpkgs.overlays = [
-    (_: super: {
-      zfs = super.zfs.overrideAttrs (_: {
-        meta.platforms = [ ];
-      });
-    })
-  ];
-
   environment.systemPackages = [
     pkgs.helix
     pkgs.fd
@@ -20,19 +12,16 @@
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
-    "repl-flake"
   ];
 
   boot = {
-    supportedFilesystems = [ "bcachefs" ];
+    zfs.package = pkgs.zfs_unstable;
     kernelPackages = pkgs.linuxPackages_latest;
   };
 
   #boot.supportedFilesystems = [ "bcachefs" ];
   console = {
     earlySetup = true;
-    font = "${pkgs.spleen}/share/consolefonts/spleen-6x12.psfu";
     keyMap = "neo";
   };
-  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
 }
