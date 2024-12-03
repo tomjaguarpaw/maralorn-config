ed104de5d064153b164a515fd566c8b13646563b
maralorn <mail@maralorn.de>
2024-12-03 19:57:16 +0100

    Add agent forwarding for a host

--- home-manager/roles/all/ssh.nix
+++ home-manager/roles/all/ssh.nix
@@ -4,6 +4,7 @@
     controlPersist = "10m";
     enable = true;
     matchBlocks = {
+      "console.heilmannsoftware.net".forwardAgent = true;
       git-auto.identityFile = "~/.ssh/id_auto_ed25519";
       "door.cccda.de".identityFile = "~/.ssh/door_rsa";
     };
