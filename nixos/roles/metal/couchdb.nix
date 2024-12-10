451cf07d27e0efcdd23fd693b8693c0563083c25
maralorn <mail@maralorn.de>
2024-12-10 12:14:59 +0100

    More robust CouchDB

--- nixos/roles/metal/couchdb.nix
+++ nixos/roles/metal/couchdb.nix
@@ -6,7 +6,11 @@
     adminPass = "admin";
     bindAddress = "::";
     extraConfig = lib.generators.toINI { } {
-      chttpd_auth.timeout = 604800; # one week
+      chttpd_auth = {
+        timeout = 604800; # one week
+        max_http_request_size = "80000000";
+      };
+      couchdb.max_document_size = "80000000";
       log.writer = "journald"; # to prevent redundant timestamps
     };
   };
