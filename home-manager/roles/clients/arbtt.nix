83896d06d056266b556bfc409ee229d5d555c99d
maralorn <mail@maralorn.de>
2025-02-20 11:35:15 +0100

    Better arbtt rules

--- home-manager/roles/clients/arbtt.nix
+++ home-manager/roles/clients/arbtt.nix
@@ -9,11 +9,28 @@ in
     tag Program:$current.program,
     tag Title:$current.title,
     current window $title =~ m!/git/([^/]*)! ==> tag Project:$1,
-    condition work = current window $title =~ m!(heilmann|basecamp|connect|backend|model|console|frontend|Fauxton)!
-
-      in { $work   ==> tag work,
-           ! $work ==> tag notwork
-         }
+    current window $program == "Signal" ==> tag Graph:signal,
+    current window $program == "Element" ==> tag Graph:matrix,
+    current window $program == "Discord" ==> tag Graph:discord,
+    current window $program == "org_telegram_desktop" ==> tag Graph:telegram,
+    current window $title =~ m!eomutt! ==> tag Graph:mail,
+    current window $title =~ m!asecamp! ==> tag Graph:basecamp,
+    current window $title =~ m!connect! ==> tag Graph:connect,
+    current window $title =~ m!heilmann! ==> tag Graph:heilmann,
+    current window $title =~ m!google! ==> tag Graph:google,
+    current window $title =~ [/nixos/,/nixpkgs/] ==> tag Graph:nix,
+    current window $title =~ [/reflex/,/obsidiansystems/] ==> tag Graph:reflex,
+    current window $title =~ [/haskell/,/ghc-proposals/] ==> tag Graph:haskell,
+    current window $title =~ /github/ ==> tag Graph:github,
+    current window $title =~ m!git/config! ==> tag Graph:config,
+    current window $title =~ [/git/,/jj/,/tig/] ==> tag Graph:jj,
+    current window $program == "mpv" ==> tag Graph:video,
+    current window $title =~ m!youtube! ==> tag Graph:video,
+    current window $title =~ m!watchfeeds! ==> tag Graph:video,
+    current window $program == "Chromium" ==> tag Graph:chromium,
+    current window $program == "kitty" ==> tag Graph:term,
+    current window $program == "firefox" ==> tag Graph:browser,
+    $idle < 60 ==> tag Graph:other
   '';
   systemd.user = {
     timers.my-arbtt-capture = {
